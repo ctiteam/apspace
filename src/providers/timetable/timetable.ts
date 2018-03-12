cafd7ffc84c9f13d8ef7d55b4ef426cdd9d59401
Ivan Tham <pickfire@riseup.net>
2018-03-12 14:13:49 +0800

    Update cas flow

--- src/providers/timetable/timetable.ts
+++ src/providers/timetable/timetable.ts
@@ -1,10 +1,8 @@
+import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ToastController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
-
-import { Observable } from 'rxjs/Observable';
-import { tap, catchError } from 'rxjs/operators';
 
 import { Timetable } from '../../interfaces/timetable';
 
@@ -22,20 +20,17 @@ export class TimetableProvider {
   /** GET: timetable */
   getTimetable(refresh: boolean = false): Observable<Timetable[]> {
     const service = 'https://ws.apiit.edu.my/web-services/index.php/open/weektimetable';
+
     if (refresh) {
-      this.timetable$ = this.http.get<Timetable[]>(service).timeout(1000).pipe(
-        tap(cache => this.storage.set('timetable', cache)),
-        catchError(err => {
-          this.toastCtrl.create({ message: err.message, duration: 1000 }).present();
-          return Observable.from(this.storage.get('timetable'));
-        }),
-      ).publishLast().refCount();
-    } else if (!this.timetable$ || !this.timetable$.takeLast(1)) {
+      this.timetable$ = this.http.get<Timetable[]>(service).timeout(1000)
+      .do(cache => this.storage.set('timetable', cache))
+      .catch(err => this.toastCtrl.create({ message: err.message, duration: 1000 }).present()
+        || Observable.from(this.storage.get('timetable')));
+    } else {
       this.timetable$ = Observable.from(this.storage.get('timetable'))
-        .switchMap(v => v ? Observable.of(v) : this.getTimetable(true))
-        .publishLast().refCount();
+        .switchMap(v => v ? Observable.of(v) : this.getTimetable(true));
     }
-    return this.timetable$;
+    return this.timetable$.publishLast().refCount();
   }
 
 }
