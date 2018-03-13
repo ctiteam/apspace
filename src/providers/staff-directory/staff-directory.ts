75037f386b5c46db3f35ad19c1131be6e88ebec5
Ivan Tham <pickfire@riseup.net>
2018-03-13 10:42:30 +0800

    Fix cache timeout issue for api providers

--- src/providers/staff-directory/staff-directory.ts
+++ src/providers/staff-directory/staff-directory.ts
@@ -16,23 +16,25 @@ export class StaffDirectoryProvider {
     public http: HttpClient,
     public storage: Storage,
     public toastCtrl: ToastController,
-    private casService: CasTicketProvider
+    private casService: CasTicketProvider,
   ) { }
 
   /** GET: staff directory */
   getStaffDirectory(refresh: boolean = false): Observable<StaffDirectory[]> {
     const service = 'https://ws.apiit.edu.my/web-services/index.php/staff/listing';
+    const options = { withCredentials: true };
 
     if (refresh) {
-      const options = { withCredentials: true };
-      this.staff$ = this.http.get<StaffDirectory[]>(service, options).timeout(1000)
-        .do(cache => this.storage.set('staffDirectory', cache))
-        .catch(_ => this.casService.getST(service).timeout(1000).switchMap(ticket =>
-          this.http.get<StaffDirectory[]>(`${service}?ticket=${ticket}`, options).timeout(1000)))
-        .catch(err => this.toastCtrl.create({ message: err.message, duration: 3000 }).present()
-          || Observable.from(this.storage.get('staffDirectory')));
+      this.staff$ = this.http.get<StaffDirectory[]>(service, options)
+        .catch(_ => this.casService.getST(service)
+          .switchMap(st => this.http.get<StaffDirectory[]>(`${service}?ticket=${st}`, options)))
+        .do(cache => this.storage.set('staffDirectory', cache)).timeout(2000)
+        .catch(err => {
+          this.toastCtrl.create({ message: err.message, duration: 3000 }).present()
+          return Observable.fromPromise(this.storage.get('staffDirectory'))
+        })
     } else {
-      this.staff$ = Observable.from(this.storage.get('staffDirectory'))
+      this.staff$ = Observable.fromPromise(this.storage.get('staffDirectory'))
         .switchMap(v => v ? Observable.of(v) : this.getStaffDirectory(true));
     }
     return this.staff$.publishLast().refCount();
--- src/providers/timetable/timetable.ts
+++ src/providers/timetable/timetable.ts
@@ -22,12 +22,14 @@ export class TimetableProvider {
     const service = 'https://ws.apiit.edu.my/web-services/index.php/open/weektimetable';
 
     if (refresh) {
-      this.timetable$ = this.http.get<Timetable[]>(service).timeout(1000)
-      .do(cache => this.storage.set('timetable', cache))
-      .catch(err => this.toastCtrl.create({ message: err.message, duration: 1000 }).present()
-        || Observable.from(this.storage.get('timetable')));
+      this.timetable$ = this.http.get<Timetable[]>(service)
+        .do(cache => this.storage.set('timetable', cache)).timeout(6000)
+        .catch(err => {
+          this.toastCtrl.create({ message: err.message, duration: 3000 }).present();
+          return Observable.fromPromise(this.storage.get('timetable'));
+        });
     } else {
-      this.timetable$ = Observable.from(this.storage.get('timetable'))
+      this.timetable$ = Observable.fromPromise(this.storage.get('timetable'))
         .switchMap(v => v ? Observable.of(v) : this.getTimetable(true));
     }
     return this.timetable$.publishLast().refCount();
