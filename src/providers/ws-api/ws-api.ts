83ace42c3ce142f9c1639efd9f3123e192f2afe8
Ivan Tham <pickfire@riseup.net>
2018-03-15 17:26:30 +0800

    Switch wording to endpoint in ws-api

--- src/providers/ws-api/ws-api.ts
+++ src/providers/ws-api/ws-api.ts
@@ -24,30 +24,30 @@ export class WsApiProvider {
   /**
    * GET: Request WS API with cache and error handling.
    *
-   * @param path - <apiUrl>/<path> for service, used for caching
+   * @param endpoint - <apiUrl>/<endpoint> for service, used for caching
    * @param refresh - force refresh (default: false)
    * @param options.timeout - request timeout (default: 5000)
    * @param options.auth - authentication required (default: true)
    * @return shared cached observable
    */
-  get<T>(path: string, refresh?: boolean, options: {
+  get<T>(endpoint: string, refresh?: boolean, options: {
     timeout?: number,
     auth?: boolean,
   } = {}): Observable<T> {
-    const url = this.apiUrl + path;
+    const url = this.apiUrl + endpoint;
     const opt = { withCredentials: Boolean(options.auth !== false) };
 
     return (refresh && (!this.plt.is('cordova') || this.network.type !== 'none')
       ? this.http.get<T>(url, opt)
       .catch(err => options.auth === false ? Observable.throw(err)
         : this.cas.getST(url).switchMap(st => this.http.get<T>(`${url}?ticket=${st}`, opt)))
-      .do(cache => this.storage.set(path, cache)).timeout(options.timeout || 5000)
+      .do(cache => this.storage.set(endpoint, cache)).timeout(options.timeout || 5000)
       .catch(err => {
         this.toastCtrl.create({ message: err.message, duration: 3000 }).present();
-        return Observable.fromPromise(this.storage.get(path));
+        return Observable.fromPromise(this.storage.get(endpoint));
       })
-      : Observable.fromPromise(this.storage.get(path))
-      .switchMap(v => v ? Observable.of(v) : this.get(path, true, options))
+      : Observable.fromPromise(this.storage.get(endpoint))
+      .switchMap(v => v ? Observable.of(v) : this.get(endpoint, true, options))
     ).publishLast().refCount();
   }
 }
