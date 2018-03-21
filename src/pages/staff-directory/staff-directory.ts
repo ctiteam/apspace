97f74b2dad30c8c6258c02b5114f70752e7a14ae
Ivan Tham <pickfire@riseup.net>
2018-03-21 16:12:21 +0800

    Fix staff directory double click to exit
    - Use nav instead of models (core dev frozed)

--- src/pages/staff-directory/staff-directory.ts
+++ src/pages/staff-directory/staff-directory.ts
@@ -1,5 +1,5 @@
 import { Component, HostListener, ViewChild } from '@angular/core';
-import { NavController, ModalController, Searchbar, IonicPage } from 'ionic-angular';
+import { NavController, Searchbar, IonicPage } from 'ionic-angular';
 
 import { Observable } from 'rxjs/Observable';
 
@@ -16,15 +16,11 @@ export class StaffDirectoryPage {
   @ViewChild(Searchbar) searchbar: Searchbar;
 
   term: string = '';
-  department: string = '';
+  dept: string = '';
   staff$: Observable<StaffDirectory[]>;
   staffType$: Observable<string[]>;
 
-  constructor(
-    public navCtrl: NavController,
-    public modalCtrl: ModalController,
-    private ws: WsApiProvider,
-  ) { }
+  constructor(public navCtrl: NavController, private ws: WsApiProvider) { }
 
   @HostListener('keydown', ['$event']) onkeydown(e) {
     if (e.keyCode == 13) {
@@ -33,15 +29,10 @@ export class StaffDirectoryPage {
     }
   }
 
-  info(staff: StaffDirectory) {
-    this.modalCtrl.create('StaffDirectoryInfoPage', { id: staff.CODE }).present();
-  }
-
   selected(staff: StaffDirectory[]): StaffDirectory[] {
-    return staff ? (staff).filter(s =>
-      (!this.department || [s.DEPARTMENT, s.DEPARTMENT2, s.DEPARTMENT3].indexOf(this.department) !== -1)
-      && (s.FULLNAME.toLowerCase().indexOf(this.term.toLowerCase()) !== -1)
-    ) : [] as StaffDirectory[];
+    return (staff || []).filter(s =>
+      (!this.dept || [s.DEPARTMENT, s.DEPARTMENT2, s.DEPARTMENT3].indexOf(this.dept) !== -1)
+      && s.FULLNAME.toLowerCase().indexOf(this.term.toLowerCase()) !== -1);
   }
 
   doRefresh(refresher?) {
