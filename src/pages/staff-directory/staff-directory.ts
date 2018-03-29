39ebb0b86470b9cb2ffd58122696e6aecca5451e
Ivan Tham <pickfire@riseup.net>
2018-03-29 10:28:26 +0800

    Default with filter operator in staff directory

--- src/pages/staff-directory/staff-directory.ts
+++ src/pages/staff-directory/staff-directory.ts
@@ -2,7 +2,7 @@ import { Component, HostListener, ViewChild } from '@angular/core';
 import { NavController, Searchbar, IonicPage } from 'ionic-angular';
 
 import { Observable } from 'rxjs/Observable';
-import { finalize, map } from 'rxjs/operators';
+import { filter, finalize, map } from 'rxjs/operators';
 
 import { StaffDirectory } from '../../interfaces';
 import { WsApiProvider } from '../../providers';
@@ -30,8 +30,8 @@ export class StaffDirectoryPage {
     }
   }
 
-  selected(staff: StaffDirectory[]): StaffDirectory[] {
-    return (staff || []).filter(s =>
+  selected(staffs: StaffDirectory[]): StaffDirectory[] {
+    return (staffs || []).filter(s =>
       (!this.dept || [s.DEPARTMENT, s.DEPARTMENT2, s.DEPARTMENT3].indexOf(this.dept) !== -1)
       && s.FULLNAME.toLowerCase().indexOf(this.term.toLowerCase()) !== -1);
   }
@@ -39,7 +39,8 @@ export class StaffDirectoryPage {
   doRefresh(refresher?) {
     this.staff$ = this.ws.get<StaffDirectory[]>('/staff/listing', Boolean(refresher));
     this.staffType$ = this.staff$.pipe(
-      map(ss => Array.from(new Set((ss || []).map(s => s.DEPARTMENT)))),
+      filter(ss => ss instanceof Array),
+      map(ss => Array.from(new Set(ss.map(s => s.DEPARTMENT)))),
       finalize(() => refresher && refresher.complete()),
     );
   }
