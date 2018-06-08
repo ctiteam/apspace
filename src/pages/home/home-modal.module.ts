d77b755351d3262aac1f53cd5d70b125916f756e
Esen Zheenchoroev <TP034790@mail.apu.edu.my>
2018-06-08 17:12:22 +0800

    feat(home-info): add parallax scroll

--- src/pages/home/home-modal.module.ts
+++ src/pages/home/home-modal.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { HomeModalPage } from './home-modal';
+import { ParallaxHeaderDirective } from '../../directives/parallax-header/parallax-header';
 
 @NgModule({
   declarations: [
     HomeModalPage,
+    ParallaxHeaderDirective
   ],
   imports: [
     IonicPageModule.forChild(HomeModalPage),
