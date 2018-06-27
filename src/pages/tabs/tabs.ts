d8972dcd9ea1a0c08a6120c4b84e6daceb123280
Ivan Tham <pickfire@riseup.net>
2018-06-27 17:52:07 +0800

    refactor(tabs): clean up

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { IonicPage, NavParams } from 'ionic-angular';
+import { IonicPage } from 'ionic-angular';
 import { SuperTabs } from 'ionic2-super-tabs';
 
 @IonicPage()
@@ -11,25 +11,12 @@ export class TabsPage {
 
   @ViewChild(SuperTabs) superTabs: SuperTabs;
 
-  tabs: Array<{
-    icon: any,
-    root: any
-  }>;
+  tabs: Array<{icon: string, root: string}> = [
+    { icon: 'md-home', root: 'HomePage' },
+    { icon: 'md-calendar', root: 'TimetablePage' },
+    { icon: 'md-alarm', root: 'AttendancePage' },
+    { icon: 'md-card', root: 'ApcardPage' },
+    { icon: 'ios-more', root: 'MorePage' },
+  ];
 
-  constructor(public navParams: NavParams) {
-
-    this.tabs = [
-      { icon: 'md-home', root: 'HomePage' },
-      { icon: 'md-calendar', root: 'TimetablePage' },
-      { icon: 'md-alarm', root: 'AttendancePage' },
-      { icon: 'md-card', root: 'ApcardPage' },
-      { icon: 'ios-more', root: 'MorePage' },
-    ]
-  }
-
-  ionViewDidLoad() { }
-
-  slideToIndex(index: number) {
-    this.superTabs.slideTo(index);
-  }
 }
