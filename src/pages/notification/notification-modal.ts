fb5d0a0af931dc52fe915c6c3d05bac77268859c
Esen Zheenchoroev <TP034790@mail.apu.edu.my>
2018-08-10 17:46:31 +0800

    refactor(notification): html support

--- src/pages/notification/notification-modal.ts
+++ src/pages/notification/notification-modal.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 
 @IonicPage()
@@ -14,9 +15,14 @@ export class NotificationModalPage {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
+    private sanitizer: DomSanitizer,
   ) {}
 
   ionViewDidLoad(){
     this.itemDetails = this.navParams.get('itemDetails');
+  }
+
+  sanitize(value: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(value);
   }
 }
