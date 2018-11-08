05fc44ef98927f2a943fda7f37c5a931fabb3403
Esen Zheenchoroev <TP034790@mail.apu.edu.my>
2018-11-08 18:03:25 +0800

    style(events): add time and date icons

--- src/pages/events/events.ts
+++ src/pages/events/events.ts
@@ -86,21 +86,6 @@ export class EventsPage {
   }
 
   getHolidays() {
-    const months = {
-      'January': '01',
-      'February': '02',
-      'March': '03',
-      'April': '04',
-      'May': '05',
-      'June': '06',
-      'July': '07',
-      'August': '08',
-      'September': '09',
-      'October': '10',
-      'November': '11',
-      'December': '12',
-    };
-
     const now = new Date();
     this.nextHoliday$ = this.ws.get<Holidays>('/transix/holidays/filtered/students', true)
       .pipe(
