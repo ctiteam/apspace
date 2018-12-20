e6d5b901fa35ec422322686c9e6b34d9c0887eb1
Ivan Tham <pickfire@riseup.net>
2018-12-20 09:30:08 +0800

    feat(fees): display currency

--- src/pages/fees/fees.ts
+++ src/pages/fees/fees.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage } from 'ionic-angular';
 
 import { Observable } from 'rxjs/Observable';
-import { forkJoin } from 'rxjs/observable/forkJoin';
 
 import {
   FeesBankDraft, FeesDetails, FeesSummary, FeesTotalSummary,
@@ -15,26 +14,24 @@ import { WsApiProvider } from '../../providers';
   templateUrl: 'fees.html',
 })
 export class FeesPage {
-  selectedSegment: string = 'Summary';
+  selectedSegment = 'Summary';
 
   totalSummary$: Observable<FeesTotalSummary[]>;
   bankDraft$: Observable<FeesBankDraft[]>;
   detail$: Observable<FeesDetails[]>;
   summary$: Observable<FeesSummary[]>;
 
-  numOfSkeletons = new Array(5);
-  isLoading: boolean;
-
-  constructor(
-    private ws: WsApiProvider,
-  ) { }
+  constructor(private ws: WsApiProvider) { }
 
   ionViewDidLoad() {
     this.totalSummary$ = this.ws.get('/student/summary_overall_fee', true);
     this.summary$ = this.ws.get('/student/outstanding_fee', true);
     this.bankDraft$ = this.ws.get('/student/bankdraft_amount', true);
     this.detail$ = this.ws.get('/student/overall_fee', true);
-    forkJoin(this.totalSummary$, this.summary$).pipe(
-    ).subscribe();
   }
+
+  isNumber(val: any): boolean {
+    return typeof val === 'number';
+  }
+
 }
