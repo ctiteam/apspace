bc55c76b38dfa780a8c441ef6a5a6d9fc09f3783
Ivan Tham <pickfire@riseup.net>
2019-02-13 10:05:29 +0800

    Revert "feat: happy chinese new year"
    This reverts commit df1134e5d4699a9ea315b46288a47283d05f1e47.

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
 import { IonicModule } from 'ionic-angular';
 
-import { FireworksComponent } from './fireworks/fireworks';
 import { LecturerTimetableComponent } from './lecturer-timetable/lecturer-timetable';
 import { SkeletonItemComponent } from './skeleton-item/skeleton-item';
 
 @NgModule({
   declarations: [
-    FireworksComponent,
     LecturerTimetableComponent,
     SkeletonItemComponent,
   ],
   imports: [IonicModule],
   exports: [
-    FireworksComponent,
     LecturerTimetableComponent,
     SkeletonItemComponent,
   ],
