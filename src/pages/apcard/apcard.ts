5d750c258facb59e03f5e2fb48ec1d114fa4189a
Mohamad Al Ghayeb <mohamad_alghayeb@hotmail.com>
2019-02-14 18:18:29 +0800

    fix(apcard): group transactions by year starting from activation year

--- src/pages/apcard/apcard.ts
+++ src/pages/apcard/apcard.ts
@@ -3,51 +3,53 @@ import {
   state,
   style,
   transition,
-  trigger,
-} from '@angular/animations';
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { IonicPage } from 'ionic-angular';
-import * as _ from 'lodash';
-import { Observable } from 'rxjs/Observable';
-import { finalize, map, tap } from 'rxjs/operators';
-import { Apcard } from '../../interfaces';
-import { AppAnimationProvider, WsApiProvider } from '../../providers';
+  trigger
+} from "@angular/animations";
+import { Component, ElementRef, ViewChild } from "@angular/core";
+import { IonicPage } from "ionic-angular";
+import * as _ from "lodash";
+import { Observable } from "rxjs/Observable";
+import { finalize, map, tap } from "rxjs/operators";
+import { Apcard } from "../../interfaces";
+import { AppAnimationProvider, WsApiProvider } from "../../providers";
 
 @IonicPage()
 @Component({
-  selector: 'page-apcard',
-  templateUrl: 'apcard.html',
+  selector: "page-apcard",
+  templateUrl: "apcard.html",
   animations: [
-    trigger('flyInOut', [
-      state('in', style({ transform: 'translateX(0)' })),
-      transition('void => *', [
-        style({ transform: 'translateX(-30%)' }),
-        animate(700),
+    trigger("flyInOut", [
+      state("in", style({ transform: "translateX(0)" })),
+      transition("void => *", [
+        style({ transform: "translateX(-30%)" }),
+        animate(700)
       ]),
-      transition('* => void', [
-        animate(700, style({ transform: 'translateX(30%)' })),
-      ]),
-    ]),
-  ],
+      transition("* => void", [
+        animate(700, style({ transform: "translateX(30%)" }))
+      ])
+    ])
+  ]
 })
 export class ApcardPage {
-  @ViewChild('apcardBalanceBackground') balanceBackgroundElement: ElementRef;
+  @ViewChild("apcardBalanceBackground") balanceBackgroundElement: ElementRef;
 
   transaction$: Observable<Apcard[]>;
   objectKeys = Object.keys; // USED FOR GROUPING TRANSACTIONS PER MONTH
-  filterEntry: string = '';
+  filterEntry: string = "";
   balance: number;
   monthly: number;
   monthlyTransactions: any;
   monthlyData: any;
   transactionsGroupedByDate: any;
+  transactonsYears: string[] = [];
+  transactionsMonths: string[] = [];
 
   numOfSkeletons = new Array(5);
   isLoading: boolean;
 
   constructor(
     private ws: WsApiProvider,
-    private appAnimationProvider: AppAnimationProvider,
+    private appAnimationProvider: AppAnimationProvider
   ) {}
 
   /** Analyze transactions. */
@@ -62,7 +64,7 @@ export class ApcardPage {
     const a = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     this.monthlyData = transactions.reduce(
       (tt, t) => {
-        const c = t.SpendVal > 0 ? 'dr' : 'cr'; // classify spent type
+        const c = t.SpendVal > 0 ? "dr" : "cr"; // classify spent type
         const d = new Date(t.SpendDate);
         d.getFullYear() in tt[c] || (tt[c][d.getFullYear()] = a.slice());
         tt[c][d.getFullYear()][d.getMonth()] += Math.abs(t.SpendVal);
@@ -72,47 +74,62 @@ export class ApcardPage {
       },
       {
         dr: { [now.getFullYear()]: a.slice() },
-        cr: { [now.getFullYear()]: a.slice() },
-      },
+        cr: { [now.getFullYear()]: a.slice() }
+      }
     );
 
     this.transactionsGroupedByDate = _.mapValues(
       _.groupBy(transactions, item => {
         return (
           new Date(item.SpendDate).getMonth() +
-          ', ' +
+          ", " +
           new Date(item.SpendDate).getFullYear()
         );
-      }),
+      })
     );
     // reverse monthlyData last year
     this.monthly = this.monthlyData.dr[now.getFullYear()][now.getMonth()];
+    console.log(this.monthlyData);
+    
   }
 
   /** Negate spend value for top ups. */
   signTransactions(transactions: Apcard[]): Apcard[] {
     transactions.forEach(transaction => {
-      if (transaction.ItemName === 'Top Up') {
+      if (transaction.ItemName === "Top Up") {
         transaction.SpendVal *= -1;
       }
     });
     return transactions;
   }
 
+  getTransactionsYears(){
+    let firstTransactionYear = this.objectKeys(this.monthlyData.cr)[0];
+    let currentYear = new Date().getFullYear();
+    for (let year=+firstTransactionYear; year<=currentYear;year++){
+      console.log(year.toString());
+      this.transactonsYears.push(year.toString());
+    }
+    for(let month=11; month>=0;month--){
+      this.transactionsMonths.push(month.toString());
+    }
+  }
+
   doRefresh(refresher?) {
     this.isLoading = true;
-    this.transaction$ = this.ws.get<Apcard[]>('/apcard/', true).pipe(
+    this.transaction$ = this.ws.get<Apcard[]>("/apcard/", true).pipe(
       map(t => this.signTransactions(t)),
       tap(t => this.analyzeTransactions(t)),
+      tap(t => this.getTransactionsYears()),
       finalize(() => refresher && refresher.complete()),
-      finalize(() => (this.isLoading = false)),
+      finalize(() => (this.isLoading = false))
     );
   }
 
   ionViewDidLoad() {
     this.doRefresh();
     this.appAnimationProvider.animateBalanceBackground(
-      this.balanceBackgroundElement,
+      this.balanceBackgroundElement
     );
   }
 }
