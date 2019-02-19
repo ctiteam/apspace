5877c4ebd3d4415c45d85f9c47923d2b65176b0a
Ivan Tham <pickfire@riseup.net>
2019-02-19 17:04:19 +0800

    chore: bump version to 1.1.1

--- src/providers/version.ts
+++ src/providers/version.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class VersionProvider {
 
-  readonly version = '1.1.0';
+  readonly version = '1.1.1';
 
   /** Application version name. */
   get name(): string {
