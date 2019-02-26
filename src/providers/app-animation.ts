ed71537122335f747da8139f34296bc7767d7dba
Mohamad Al Ghayeb@Home <mohamad_alghayeb@hotmail.com>
2019-02-26 15:46:08 +0800

    refactor(animation-provider): remove unused code

--- src/providers/app-animation.ts
+++ src/providers/app-animation.ts
@@ -1,32 +1,10 @@
-import { ElementRef, Injectable, Renderer2, RendererFactory2 } from '@angular/core';
+import { ElementRef, Injectable } from '@angular/core';
 
-const colorsSet1 = [
-  '#67809F',
-  '#019875',
-  '#96281B',
-  '#C0392B',
-  '#7f8c8d',
-  '#22313F',
-  '#004790',
-  '#6E248D',
-];
-const colorsSet2 = [
-  '#2E3131',
-  '#2ECC71',
-  '#EC644B',
-  '#40407a',
-  '#33d9b2',
-  '#c23616',
-  '#106B60',
-  '#1297E0',
-];
+
 
 @Injectable()
 export class AppAnimationProvider {
-  private renderer: Renderer2;
-  constructor(rendererFactory: RendererFactory2) {
-    this.renderer = rendererFactory.createRenderer(null, null);
-  }
+  constructor() {}
 
   /*
   USED TO CHECK IF AN ELEMENT IS IN THE VIEWPORT OR NOT.
@@ -52,34 +30,4 @@ export class AppAnimationProvider {
       }
     }
   }
-
-  // CAN BE MERGED WITH 2
-  // giveQuickAccessItemsRandomColors(elRef: ElementRef) {
-  //   let quickAccessItems = elRef.nativeElement.querySelectorAll(
-  //     ".quick-access-item"
-  //   );
-  //   for (let i = 0; i < quickAccessItems.length; i++) {
-  //     let value = Math.floor(Math.random() * colorsSet1.length);
-  //     quickAccessItems[i].style.backgroundColor = colorsSet1[value];
-  //     quickAccessItems[i].style.boxShadow =
-  //       "1px 1px 1px " +
-  //       colorsSet1[value] +
-  //       ", -1px -1px 1px " +
-  //       colorsSet1[value];
-  //     quickAccessItems[i].style.color = "#FFFFFF";
-  //   }
-  // }
-
-  // 2
-  // giveSectionsRandomBorderColors(elRef: ElementRef) {
-  //   let sections = elRef.nativeElement.querySelectorAll(".section");
-  //   let sectionsHeader = elRef.nativeElement.querySelectorAll(
-  //     ".section-header"
-  //   );
-  //   for (let i = 0; i < sections.length; i++) {
-  //     let value = Math.floor(Math.random() * colorsSet2.length);
-  //     sections[i].style.borderColor = colorsSet2[value];
-  //     sectionsHeader[i].style.color = colorsSet2[value];
-  //   }
-  // }
 }
