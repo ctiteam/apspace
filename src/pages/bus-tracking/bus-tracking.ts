594fe663d4256bae7db0c7e1a9d307012b89611e
Mohamad Al Ghayeb <mohamad_alghayeb@hotmail.com>
2019-03-04 13:42:59 +0800

    feat(bus tracking): apply filter without clicking the apply filter button

--- src/pages/bus-tracking/bus-tracking.ts
+++ src/pages/bus-tracking/bus-tracking.ts
@@ -57,86 +57,32 @@ export class BusTrackingPage {
       finalize(() => refresher && refresher.complete()),
     );
 
-    // FILTEREDTRIP$ IS USED IN THE TEMPLATE AFTER GROUPING TRIP$
-    this.filteredTrip$ = this.trip$.pipe(
-      map(trips => {
-          return _.filter(trips, trip => {
-            // FILTER TRIPS TO TODAYS' TRIPS ONLY
-            if (this.tripDay == 'mon-fri') {
-              return trip.trip_from.includes(this.fromLocation) && trip.trip_to.includes(this.toLocation) && (trip.trip_day == 'mon-fri' || trip.trip_day == 'fri');
-            } else {
-              return trip.trip_from.includes(this.fromLocation) && trip.trip_to.includes(this.toLocation) && trip.trip_day == this.getTodayDay(this.dateNow);
-            }
-          });
-        },
-      ),
-      map(trips => {
-        // group items by trip_from
-        return _.mapValues(
-          _.groupBy(trips, item => {
-            return item.trip_from;
-          }),
-        );
-      }),
-      map(filteredTrips => {
-        // group items by trip_to
-        // this.numberOfTrips++;
-        return _.forEach(filteredTrips, function(value, key) {
-          filteredTrips[key] = _.groupBy(filteredTrips[key], function(item) {
-            return item.trip_to;
-          });
-        });
-      }),
-    );
+    this.onFilter();
   }
 
-  // GET LOCATIONS
-  getLocations(refresher?) {
-    this.bus.getLocationDetails(Boolean(refresher)).subscribe(response => {
-      this.locations = response.locations;
-    });
-  }
-
-  // TOGGLE THE MENU
-  toggleFilterMenu() {
-    this.menu.toggle();
-  }
-
-  changeLocations(){
-    let temp = this.toLocation;
-    this.toLocation = this.fromLocation;
-    this.fromLocation = temp;
-  }
-
-  // FILTER TRIPS BY FROM LOCATION , TO LOCATION, TRIP DAY AND COMING TRIPS ONLY
-  filterTrips(
-    source: string,
-    destination: string,
-    day: string,
-    comingTripsOnly: string,
-  ) {
-    this.settings.set('tripFrom', source);    
-    this.settings.set('tripTo', destination);    
+  onFilter(){
+    this.settings.set('tripFrom', this.fromLocation);    
+    this.settings.set('tripTo', this.toLocation);    
     this.filteredTrip$ = this.trip$.pipe(
       map(trips => {
         this.numberOfTrips = 1; // HIDE 'THERE ARE NO TRIPS' MESSAGE
         let filteredArray = _.filter(trips, trip => {
           // FILTER TRIPS BY (FROM, TO) LOCATIONS, AND DAY
-          if (day == 'mon-fri') {
+          if (this.tripDay == 'mon-fri') {
             return (
-              trip.trip_from.includes(source) &&
-              trip.trip_to.includes(destination) &&
+              trip.trip_from.includes(this.fromLocation) &&
+              trip.trip_to.includes(this.toLocation) &&
               (trip.trip_day == 'mon-fri' || trip.trip_day == 'fri')
             );
           } else {
             return (
-              trip.trip_from.includes(source) &&
-              trip.trip_to.includes(destination) &&
-              trip.trip_day == day
+              trip.trip_from.includes(this.fromLocation) &&
+              trip.trip_to.includes(this.toLocation) &&
+              trip.trip_day == this.tripDay
             );
           }
         });
-        if (comingTripsOnly == 'true') {
+        if (this.comingTripsOnly == 'true') {
           filteredArray = _.filter(filteredArray, trip => {
             // FILTER TRIPS TO UPCOMING TRIPS ONLY
             return this.strToDate(trip.trip_time) >= this.dateNow;
@@ -164,7 +110,26 @@ export class BusTrackingPage {
         })
       }),
     );
-    this.toggleFilterMenu();
+  }
+
+  // GET LOCATIONS
+  getLocations(refresher?) {
+    this.bus.getLocationDetails(Boolean(refresher)).subscribe(response => {
+      this.locations = response.locations;
+    });
+  }
+
+  // TOGGLE THE MENU
+  toggleFilterMenu() {
+    this.menu.toggle();
+  }
+
+  // SWAP FROM AND TO LOCATIONS
+  changeLocations(){
+    let temp = this.toLocation;
+    this.toLocation = this.fromLocation;
+    this.fromLocation = temp;
+    this.onFilter();
   }
 
   // GET DAY SHORT NAME (LIKE 'SAT' FOR SATURDAY)
