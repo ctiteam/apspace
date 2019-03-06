8546511fd61fbd21c468260aab0d920de9dc8db3
Mohamad Al Ghayeb <mohamad_alghayeb@hotmail.com>
2019-03-06 17:08:03 +0800

    fix: fix the text color for status bar in light theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -62,6 +62,7 @@ export class MyApp {
             this.selectedTheme = val;
             if(this.selectedTheme === 'light-theme'){
               this.statusBar.backgroundColorByHexString('#e7e7e7');
+              this.statusBar.styleBlackOpaque();
             } else if (this.selectedTheme === 'dark-theme'){
               this.statusBar.backgroundColorByHexString('#1d1b1b');
               this.statusBar.styleLightContent();
