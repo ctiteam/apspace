d8951db9bcc57a8eaadcc204d03d786a1febb1ea
Mohamad Al Ghayeb <mohamad_alghayeb@hotmail.com>
2019-03-25 14:09:18 +0800

    feat(qualification verification): wrap items with containers for the web view and add notes to the page

--- src/pages/qualification-verification/qualification-verification.ts
+++ src/pages/qualification-verification/qualification-verification.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { WsApiProvider } from '../../providers';
 import { Observable } from 'rxjs';
 import { Qualification } from '../../interfaces';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 @IonicPage()
 @Component({
@@ -14,18 +15,27 @@ export class QualificationVerificationPage {
   searchKeyword = '';
   userSearched = false;
   resultKeyWord = '';
-  constructor(public navCtrl: NavController, public navParams: NavParams, private ws: WsApiProvider) {
+  formsURL = 'http://forms.sites.apiit.edu.my/certificate-email-form/';
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private ws: WsApiProvider,
+    private iab: InAppBrowser    
+    ) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad QualificationVerificationPage');
   }
 
   searchForGraduaters(){
-    console.log('hioi');
     this.userSearched = true;
     this.resultKeyWord = this.searchKeyword;
     this.graduater$ = this.ws.get<Qualification[]>(`/alumni/validate?criterion=${this.searchKeyword}`, true, {auth: false});
+  }
+
+  openForms(){
+    this.iab.create(`${this.formsURL}`, '_blank', 'location=true');
   }
 
 }
