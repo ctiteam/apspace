9dff9ff18192d1aba0c33511382b4d488e6121ad
Mohamad Al Ghayeb <mohamad_alghayeb@hotmail.com>
2019-04-10 13:55:59 +0800

    fix(student consent form): fix error messages and add skeleton items for loading

--- src/pages/student-consent-form/student-consent-form.module.ts
+++ src/pages/student-consent-form/student-consent-form.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { StudentConsentFormlPage } from './student-consent-form';
+import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { StudentConsentFormlPage } from './student-consent-form';
   ],
   imports: [
     IonicPageModule.forChild(StudentConsentFormlPage),
-  ],
+    ComponentsModule
+  ]
 })
 export class StudentConsentFormlPageModule { }
