291763458408e1b8eb777af7433ec7e19948e874
Mohamad Al Ghayeb <mohamad_alghayeb@hotmail.com>
2019-04-15 12:18:09 +0800

    (password recovery): add confirmation & toast messages when sending the request

--- src/pages/password-recovery/password-recovery.ts
+++ src/pages/password-recovery/password-recovery.ts
@@ -1,6 +1,6 @@
 import { HttpParams } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { IonicPage } from 'ionic-angular';
+import { IonicPage, ToastController, AlertController } from 'ionic-angular';
 
 import { finalize } from 'rxjs/operators';
 
@@ -20,18 +20,18 @@ export class PasswordRecoveryPage {
   // THIS REGULAR EXPERSSION FOLLOWS THE RFC 2822 STANDARD
   emailValidationRegularExp = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 
-  constructor(private ws: WsApiProvider) { }
+  constructor(private ws: WsApiProvider, private toastCtrl: ToastController, public alertCtrl: AlertController) { }
 
   ionViewDidLoad() {
     this.getConfig();
   }
 
-  onchangeEmail(){
+  onchangeEmail() {
     console.log("changing");
     let responseOfRegEx = this.qa.secondaryEmail.match(this.emailValidationRegularExp);
-    if(!responseOfRegEx){
+    if (!responseOfRegEx) {
       this.showEmailNotValidMsg = true;
-    } else{
+    } else {
       this.showEmailNotValidMsg = false;
     }
   }
@@ -44,9 +44,46 @@ export class PasswordRecoveryPage {
   }
 
   setConfig(v: Sqa) {
-    const body = new HttpParams({ fromObject: { ...v }}).toString();
-    const headers = { 'Content-Type': 'application/x-www-form-urlencoded' };
-    this.ws.post('/sqa/', { body, headers }).subscribe();
+
+    const confirm = this.alertCtrl.create({
+      title: 'Update Security Questions',
+      message: 'You are about to update your security questios and your secondary email which are used to recover your password in case you forget it. Do you want to continue?',
+      buttons: [
+        {
+          text: 'No',
+          handler: () => {
+          }
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            const body = new HttpParams({ fromObject: { ...v } }).toString();
+            const headers = { 'Content-Type': 'application/x-www-form-urlencoded' };
+            this.ws.post('/sqa/', { body, headers }).subscribe(
+              _ => { },
+              _ => { 
+                this.toast("Something went wrong and we couldn't complete your request. Please try again or contact us via the feedback page");
+              },
+              () => {
+                this.toast("Your security question and secondary email have been updated successfully.")
+              }
+            );
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+
+  toast(msg: string) {
+    this.toastCtrl
+      .create({
+        message: msg,
+        duration: 7000,
+        position: "bottom",
+        showCloseButton: true
+      })
+      .present();
   }
 
 }
