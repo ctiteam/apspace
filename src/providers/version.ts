c8c99fe1e3b15240f4d7f1706a4b0a223aec018b
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2019-05-07 08:50:34 +0800

    chore: bump version to 1.1.3

--- src/providers/version.ts
+++ src/providers/version.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class VersionProvider {
 
-  readonly version = '1.1.2';
+  readonly version = '1.1.3';
 
   /** Application version name. */
   get name(): string {
