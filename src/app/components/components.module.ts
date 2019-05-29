4b172c2c63c149755fba15462b841353c50a0e1b
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2019-05-29 17:01:20 +0800

    fix(loading spinner component): fixing import

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -6,12 +6,15 @@ import { IonicModule } from '@ionic/angular';
 import { LecturerTimetableComponent } from './lecturer-timetable/lecturer-timetable.component';
 import { SkeletonItemComponent } from './skeleton-item/skeleton-item.component';
 import { SearchModalComponent } from './search-modal/search-modal.component';
+import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner';
 
 @NgModule({
   declarations: [
     LecturerTimetableComponent,
     SkeletonItemComponent,
-    SearchModalComponent
+    SearchModalComponent,
+    LoadingSpinnerComponent
+
   ],
   imports: [
     CommonModule,
@@ -22,7 +25,8 @@ import { SearchModalComponent } from './search-modal/search-modal.component';
   exports: [
     LecturerTimetableComponent,
     SkeletonItemComponent,
-    SearchModalComponent
+    SearchModalComponent,
+    LoadingSpinnerComponent
   ],
   entryComponents: [
     SearchModalComponent
--- src/app/components/loading-spinner/loading-spinner.ts
+++ src/app/components/loading-spinner/loading-spinner.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'loading-spinner',
   templateUrl: 'loading-spinner.html',
+  styleUrls: ['loading-spinner.css'],
 })
 export class LoadingSpinnerComponent {
   ngOnInit() { }
