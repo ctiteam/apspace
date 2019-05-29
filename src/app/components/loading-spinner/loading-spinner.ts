6c07010702d9be2a5da51f5dc98f28056d42eda9
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2019-05-29 14:32:57 +0800

    feat(loading spinner component): addig the componenet to the app

---
+++ src/app/components/loading-spinner/loading-spinner.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'loading-spinner',
+  templateUrl: 'loading-spinner.html',
+})
+export class LoadingSpinnerComponent {
+  ngOnInit() { }
+}
