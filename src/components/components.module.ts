07694148e1559757eb17d87b8a693d58da8e4e92
Peu Wai Thian <peu.waithian@staffemail.apu.edu.my>
2019-05-31 19:26:57 +0800

    feat: implement seasonal firework effect

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -4,18 +4,21 @@ import { IonicModule } from 'ionic-angular';
 import { LecturerTimetableComponent } from './lecturer-timetable/lecturer-timetable';
 import { SkeletonItemComponent } from './skeleton-item/skeleton-item';
 import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner';
+import { FireworksComponent } from './fireworks/fireworks';
 
 @NgModule({
   declarations: [
     LecturerTimetableComponent,
     SkeletonItemComponent,
     LoadingSpinnerComponent,
+    FireworksComponent
   ],
   imports: [IonicModule],
   exports: [
     LecturerTimetableComponent,
     SkeletonItemComponent,
-    LoadingSpinnerComponent
+    LoadingSpinnerComponent,
+    FireworksComponent
   ],
 })
 export class ComponentsModule { }
