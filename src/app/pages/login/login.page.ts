c5e167cb5dfc682dc9480930a4629550d563b379
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2019-06-10 11:54:43 +0800

    feat(login): add animation to the login button

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -3,7 +3,7 @@ import { Router } from '@angular/router';
 import { Events, Platform, ToastController } from '@ionic/angular';
 import { Network } from '@ionic-native/network/ngx';
 
-import { EMPTY, Subscription } from 'rxjs';
+import { EMPTY, Subscription, throwError } from 'rxjs';
 import { catchError, switchMap, tap, timeout } from 'rxjs/operators';
 
 import { Role } from '../../interfaces';
@@ -48,36 +48,49 @@ export class LoginPage implements OnInit {
     }).then(toast => toast.present());
   }
 
-  onLoginClicked() {
-    if (this.plt.is('cordova') && this.network.type === 'none') {
-      return this.toast('You are now offline.');
-    }
+  onLoginBtnClicked() {
     this.userDidLogin = true;
     this.loginProcessLoading = true;
-    this.cas.getTGT(this.apkey, this.password).pipe(
-      catchError(e => (this.toast(e), EMPTY)),
-      switchMap(tgt => this.cas.getST(this.cas.casUrl, tgt)),
-      catchError(_ => (this.toast('Fail to get service ticket.'), EMPTY)),
-      switchMap(st => this.cas.validate(st)),
-      catchError(_ => (this.toast('You are not authorized to use APSpace'), EMPTY)),
-      tap(role => this.cacheApi(role)),
-      timeout(15000),
-      tap(_ => this.events.publish('user:login')),
-    ).subscribe(
-      _ => { },
-      _ => {
-        this.loginProcessLoading = false;
-        this.userUnauthenticated = true;
-      },
-      () => {
-        this.loginProcessLoading = false;
-        this.userAuthenticated = true;
-        setTimeout(() => {
-          // Show the success message for 700 ms after completing the request
-          this.router.navigate(['/']);
-        }, 700);
+    if (!this.apkey || !this.password) {
+      this.loginProcessLoading = false;
+      this.userDidLogin = false;
+      this.toast("Please, fill up username and password");
+    } else{
+      if (this.plt.is('cordova') && this.network.type === 'none') {
+        return this.toast('You are now offline.');
       }
-    );
+      this.cas.getTGT(this.apkey, this.password).pipe(
+        catchError(e => (this.toast('Invalid Username or Password'), throwError('Invalid Username or Password'))),
+        switchMap(tgt => this.cas.getST(this.cas.casUrl, tgt).pipe(
+          catchError(e => (this.toast('Fail to get service ticket.'), throwError('Fail to get service ticket')))
+        )),
+        switchMap(st => this.cas.validate(st).pipe(
+          catchError(e => (this.toast('You are not authorized to use APSpace'), throwError('unauthorized')))
+        )),
+        tap(role => this.cacheApi(role)),
+        timeout(15000),
+        tap(_ => this.events.publish('user:login')),
+      ).subscribe(
+        _ => {},
+        _ => {
+          this.loginProcessLoading = false;
+          this.userUnauthenticated = true;
+          setTimeout(() => {
+            // Hide the error message after 700 ms
+            this.userUnauthenticated = false;
+            this.userDidLogin = false;
+          }, 900);
+        },
+        () => {
+          this.loginProcessLoading = false;
+          this.userAuthenticated = true;
+          setTimeout(() => {
+            // Show the success message for 700 ms after completing the request
+            this.router.navigate(['/student-timetable']);
+          }, 900);
+        }
+      );
+    }
   }
 
   cacheApi(role: Role) {
