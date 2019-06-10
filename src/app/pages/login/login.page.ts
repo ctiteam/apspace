773009e9abb440d8fb35d984c4fd8a297954a07f
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2019-06-10 12:17:28 +0800

    feat(login): handle CAS password expired error

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Events, Platform, ToastController } from '@ionic/angular';
+import { Events, Platform, ToastController, AlertController } from '@ionic/angular';
 import { Network } from '@ionic-native/network/ngx';
 
-import { EMPTY, Subscription, throwError } from 'rxjs';
+import { throwError } from 'rxjs';
 import { catchError, switchMap, tap, timeout } from 'rxjs/operators';
 
 import { Role } from '../../interfaces';
-import { CasTicketService, WsApiService, SettingsService } from '../../services';
+import { CasTicketService, WsApiService } from '../../services';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-login',
@@ -32,20 +33,13 @@ export class LoginPage implements OnInit {
     private network: Network,
     private plt: Platform,
     private router: Router,
-    private settings: SettingsService,
     private toastCtrl: ToastController,
     private ws: WsApiService,
+    public alertCtrl: AlertController,
+    public iab: InAppBrowser
   ) { }
 
   ngOnInit() {
-  }
-
-  toast(message: string) {
-    this.toastCtrl.create({
-      message,
-      duration: 3000,
-      position: 'top'
-    }).then(toast => toast.present());
   }
 
   onLoginBtnClicked() {
@@ -54,18 +48,28 @@ export class LoginPage implements OnInit {
     if (!this.apkey || !this.password) {
       this.loginProcessLoading = false;
       this.userDidLogin = false;
-      this.toast("Please, fill up username and password");
+      this.showToastMessage("Please, fill up username and password");
     } else{
       if (this.plt.is('cordova') && this.network.type === 'none') {
-        return this.toast('You are now offline.');
+        return this.showToastMessage('You are now offline.');
       }
       this.cas.getTGT(this.apkey, this.password).pipe(
-        catchError(e => (this.toast('Invalid Username or Password'), throwError('Invalid Username or Password'))),
+        catchError(err => {
+          if(err.includes('AccountPasswordMustChangeException')){
+            this.showConfirmationMessage();
+            this.showToastMessage("Your password has expired!")
+            return throwError('Your password has expired!');
+          }
+          else{
+            this.showToastMessage("Invalid username or password")
+            return throwError('Invalid Username or Password');
+          }
+        }),
         switchMap(tgt => this.cas.getST(this.cas.casUrl, tgt).pipe(
-          catchError(e => (this.toast('Fail to get service ticket.'), throwError('Fail to get service ticket')))
+          catchError(e => (this.showToastMessage('Fail to get service ticket.'), throwError('Fail to get service ticket')))
         )),
         switchMap(st => this.cas.validate(st).pipe(
-          catchError(e => (this.toast('You are not authorized to use APSpace'), throwError('unauthorized')))
+          catchError(e => (this.showToastMessage('You are not authorized to use APSpace'), throwError('unauthorized')))
         )),
         tap(role => this.cacheApi(role)),
         timeout(15000),
@@ -91,6 +95,35 @@ export class LoginPage implements OnInit {
         }
       );
     }
+  }
+
+  showToastMessage(message: string) {
+    this.toastCtrl.create({
+      message,
+      duration: 3000,
+      position: 'top'
+    }).then(toast => toast.present());
+  }
+
+  showConfirmationMessage() {
+    const confirm = this.alertCtrl.create({
+      header: 'Your password has expired..',
+      message: 'You are required to change your password to be able to login to the APSpace and other applications. The following documentation provides the steps to do that.',
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Open The documentation',
+          handler: () => {
+            this.iab.create('http://kb.sites.apiit.edu.my/question/apkey-troubleshooting/', '_blank', 'location=true');
+          }
+        }
+      ]
+    }).then(confirm => confirm.present());
   }
 
   cacheApi(role: Role) {
