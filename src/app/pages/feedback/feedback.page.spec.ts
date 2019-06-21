9ff4c65587350d3e3a71c6ee4c8e0de839d038e0
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2019-06-21 10:43:04 +0800

    fix(feedback): import statements

--- src/app/pages/feedback/feedback.page.spec.ts
+++ src/app/pages/feedback/feedback.page.spec.ts
@@ -2,7 +2,7 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FeedbackPage } from './feedback.page';
-import { FeedbackService, SettingsService, VersionService } from '../services';
+import { FeedbackService, SettingsService, VersionService } from '../../services';
 
 describe('FeedbackPage', () => {
   let component: FeedbackPage;
