8154864108bb576204c842570a171ebc3b6a4c2d
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2019-06-23 19:54:02 +0800

    chore: modify event component's interface

--- src/app/interfaces/event-component.ts
+++ src/app/interfaces/event-component.ts
@@ -1,9 +1,27 @@
 export interface EventComponent {
     title?: string,
+    color: string,
+    pass: boolean,
+    type: string,
     primaryDateTime?: string,
     secondaryDateTime?: string,
     quaternaryDateTime?: string,
     firstDescription?: string,
     secondDescription?: string,
-    thirdDescription3?: string
+    thirdDescription?: string
 }
+
+/*
+EVENT TYPE COULD BE:
+  1. events-with-time-only:
+    LEFT ITEM HAS TIME NOT DATE
+
+  2. events-with-date-only:
+    LEFT ITEM HAS DATE
+
+  3. events-with-time-and-hyperlink:
+    LEFT ITEM HAS TIME AND THE SECOND DESCRIPTION IS WRAPPED WITH <a>
+
+  4. events-with-date-and-hyperlink:
+    LEFT ITEM HAS DATE AND THE SECOND DESCRIPTION IS WRAPPED WITH <a>
+*/
