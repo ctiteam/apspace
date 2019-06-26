113af3b5b5f65818f7c40c1e494c6073734596fb
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2019-06-26 18:08:32 +0800

    chore: disable tracing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -54,7 +54,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
