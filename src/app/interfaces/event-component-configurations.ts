4e687793cf31af4f07d943e08e1c120f9d127936
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2019-07-05 13:38:41 +0800

    feat(event list compoenent): allow users to specify color for past events

--- src/app/interfaces/event-component-configurations.ts
+++ src/app/interfaces/event-component-configurations.ts
@@ -2,6 +2,7 @@ export interface EventComponentConfigurations {
     title?: string,
     color: string,
     pass: boolean,
+    passColor: string,
     outputFormat: 'event-with-time-only' | 'event-with-date-only' | 'event-with-time-and-hyperlink' | 'event-with-date-and-hyperlink',
     type: 'class' | 'exam' | 'holiday' | 'iconsult',
     dateOrTime: string,   // TIME FORMAT 'HH mm A', DATE FORMAT: 'DAY MONTH YEAR'
