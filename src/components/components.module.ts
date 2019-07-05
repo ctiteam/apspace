1809f0f58e4383421982febccd8113185de2f312
Ivan Tham <pickfire@riseup.net>
2019-07-05 08:57:22 +0800

    Revert "feat: implement seasonal firework effect"
    This reverts commit 07694148e1559757eb17d87b8a693d58da8e4e92.

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -4,21 +4,18 @@ import { IonicModule } from 'ionic-angular';
 import { LecturerTimetableComponent } from './lecturer-timetable/lecturer-timetable';
 import { SkeletonItemComponent } from './skeleton-item/skeleton-item';
 import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner';
-import { FireworksComponent } from './fireworks/fireworks';
 
 @NgModule({
   declarations: [
     LecturerTimetableComponent,
     SkeletonItemComponent,
     LoadingSpinnerComponent,
-    FireworksComponent
   ],
   imports: [IonicModule],
   exports: [
     LecturerTimetableComponent,
     SkeletonItemComponent,
-    LoadingSpinnerComponent,
-    FireworksComponent
+    LoadingSpinnerComponent
   ],
 })
 export class ComponentsModule { }
