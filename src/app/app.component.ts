5dea0602a76329d6a9cb561dce4cc0ec040d378b
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2019-07-28 20:04:19 +0800

    chore(app componenet): implement dark theme classes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,85 @@
 import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Storage } from '@ionic/storage';
 
-import { SettingsService } from './services';
+import { SettingsService, UserSettingsService } from './services';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  darkThemeActivated = false;
+  pureDarkThemeActivated = false;
+  selectedAccentColor = 'blue-accent-color';
 
   constructor(
     private platform: Platform,
     private statusBar: StatusBar,
+    public storage: Storage,
     private settings: SettingsService,
+    private userSettings: UserSettingsService
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
-    this.settings.ready();
+    // this.settings.ready();
 
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-    });
+    // this.platform.ready().then(() => {
+    //   this.statusBar.styleDefault();
+    // });
+    // platform required to be ready before everything else
+    this.platform
+      .ready()
+      .then(() => Promise.all([this.settings.ready(), this.storage.get('tgt')]))
+      .then(([, tgt]) => {
+        this.userSettings.getUserSettingsFromStorage();
+        this.userSettings
+          .darkThemeActivated()
+          .subscribe((val) => {
+            this.darkThemeActivated = val;
+            this.userSettings.changeStatusBarColor(val);
+          });
+        this.userSettings
+          .PureDarkThemeActivated()
+          .subscribe((val) => {
+            this.pureDarkThemeActivated = val;
+          });
+        this.userSettings
+          .getAccentColor()
+          .subscribe(val => (this.selectedAccentColor = val));
+
+        // if (tgt) {
+        //   // check if the user is logged in
+        //   this.events.subscribe('user:logout', () => this.onLogout());
+        //   this.navCtrl.setRoot('TabsPage');
+        //   if (this.platform.is('cordova')) {
+        //     this.checkNewNotification();
+        //     this.notificationService.getMessage().subscribe();
+        //   }
+        // } else {
+        //   this.events.subscribe('user:login', () => this.onLogin());
+        //   if (this.platform.is('cordova')) {
+        //     this.checkNewNotification();
+        //   }
+        //   this.navCtrl.setRoot('LoginPage');
+        // }
+        if (this.platform.is('cordova')) {
+          if (this.platform.is('ios')) {
+            this.statusBar.overlaysWebView(false);
+          }
+          // if (this.network.type === 'none') {
+          //   this.toastCtrl
+          //     .create({
+          //       message: 'You are now offline.',
+          //       duration: 3000,
+          //       position: 'top',
+          //     })
+          //     .present();
+          // }
+        }
+      });
   }
 }
