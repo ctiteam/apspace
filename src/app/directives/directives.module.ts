bee00aad2e46c61d2da66d1ea8f0f8a189c1307b
Ivan Tham <pickfire@riseup.net>
2019-08-01 10:19:17 +0800

    fix: bump ionic to 4.7.1 for change detection

--- src/app/directives/directives.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { AutosizeDirective } from './autoSize/auto-size';
-
-@NgModule({
-  declarations: [AutosizeDirective],
-  imports: [],
-  exports: [AutosizeDirective],
-})
-export class DirectivesModule { }
