fb010e4b479e990af3e6533a99285c36e022b185
Mohamad Al Ghayeb@Home <mohamad_alghayeb@hotmail.com>
2019-08-04 18:40:04 +0800

    chore: re-design more page

--- src/app/pages/more/more.page.ts
+++ src/app/pages/more/more.page.ts
@@ -13,7 +13,7 @@ import { MenuItem, MenuGroup } from './menu.interface';
   styleUrls: ['./more.page.scss'],
 })
 export class MorePage {
-
+  menuUI: 'lists' | 'cards' = 'cards';
   term = '';
 
   /* tslint:disable:no-bitwise */
@@ -21,197 +21,153 @@ export class MorePage {
     {
       title: 'Main',
       items: [
-        {
-          title: 'Profile',
-          url: '',
-          size: 'small',
-          color: 'purple',
-          icon: 'contact',
-          role: Role.Student | Role.Lecturer | Role.Admin,
-          tags: ['mentor']
-        },
+        // {
+        //   title: 'Profile',
+        //   url: 'profile',
+        //   img: 'assets/img/profile.png',
+        //   role: Role.Student | Role.Lecturer | Role.Admin,
+        //   tags: ['mentor']
+        // },
         {
           title: 'Fees',
-          url: '',
-          size: 'small',
-          color: 'dark-orange',
-          icon: 'cash',
+          url: '/fees',
+          img: 'assets/img/fees.svg',
           role: Role.Student,
           tags: []
         },
         {
-          title: 'Bus Tracking',
-          url: '',
-          size: 'medium',
-          color: 'orange',
-          icon: 'bus',
+          title: 'Bus Shuttle Services',
+          url: 'bus-shuttle-services',
+          img: 'assets/img/bus-shuttle-services.png',
           role: Role.Student | Role.Lecturer | Role.Admin,
           tags: []
         },
         {
           title: 'Holidays',
-          url: '',
-          size: 'small',
-          color: 'orange',
-          icon: 'bicycle',
+          url: 'holidays',
+          img: 'assets/img/holidays.svg',
           role: Role.Student | Role.Lecturer | Role.Admin,
           tags: []
         },
         {
           title: 'News',
-          url: '',
-          size: 'small',
-          color: 'green',
-          icon: 'paper',
+          url: 'news',
+          img: 'assets/img/news.png',
           role: Role.Student ,
           tags: []
         },
         {
           title: 'Knowledge Base',
           url: 'http://kb.sites.apiit.edu.my/home/',
-          size: 'medium',
-          color: 'dark-orange',
-          icon: 'paper',
+          img: 'assets/img/kb.png',
           role: Role.Student | Role.Lecturer | Role.Admin,
           tags: []
         },
         {
           title: 'Forms & Application',
           url: 'http://forms.sites.apiit.edu.my/home/',
-          size: 'medium',
-          color: 'blue',
-          icon: 'clipboard',
+          img: 'assets/img/forms-and-applications.png',
           role: Role.Student | Role.Lecturer | Role.Admin,
           tags: []
         },
         {
           title: 'APCard',
-          url: '',
-          size: 'small',
-          color: 'red',
-          icon: 'card',
+          url: 'apcard',
+          img: 'assets/img/apcard.png',
           role: Role.Student | Role.Lecturer | Role.Admin,
           tags: []
         },
         {
           title: 'Webmail',
+          img: 'assets/img/webmail.png',
           url: 'https://outlook.office.com/owa/?realm=mail.apu.edu.my',
-          size: 'small',
-          color: 'purple',
-          icon: 'mail',
           role: Role.Student,
           tags: []
         },
-        {
-          title: 'APLC Progress Report',
-          url: '',
-          size: 'large',
-          color: 'purple',
-          icon: 'card',
-          role: Role.Lecturer | Role.Admin,
-          tags: []
-        },
+        // {
+        //   title: 'APLC Progress Report',
+        //   url: '',
+        //   img: 'assets/img/aplc-progress-report.png',
+        //   role: Role.Lecturer | Role.Admin,
+        //   tags: []
+        // },
       ],
     },
     {
       title: 'Course Related',
       items: [
-        {
-          title: 'Results',
-          url: '',
-          size: 'small',
-          color: 'red',
-          icon: 'checkbox',
-          role: Role.Student,
-          tags: []
-        },
-        {
-          title: 'iConsult',
-          url: '',
-          size: 'small',
-          color: 'green',
-          icon: 'clipboard',
-          role: Role.Lecturer | Role.Admin,
-          tags: []
-        },
         // {
-        //   title: 'Student Consent Form',
+        //   title: 'Results',
         //   url: '',
-        //   size: 'large',
-        //   color: 'orange',
-        //   icon: 'create',
+        //   img: 'assets/img/results.png',
+        //   role: Role.Student,
+        //   tags: []
+        // },
+        // {
+        //   title: 'iConsult',
+        //   img: 'assets/img/iconsult.png',
+        //   url: '',
         //   role: Role.Lecturer | Role.Admin,
         //   tags: []
         // },
-        {
-          title: 'iConsult',
-          url: '',
-          size: 'medium',
-          color: 'green',
-          icon: 'clipboard',
-          role: Role.Student,
-          tags: []
-        },
+        // {
+        //   title: 'Student Consent Form',
+        //   url: '',
+        //   role: Role.Lecturer | Role.Admin,
+        //   tags: []
+        // },
+        // {
+        //   title: 'iConsult',
+        //   url: '',
+        //   img: 'assets/img/iconsult.png',
+        //   role: Role.Student,
+        //   tags: []
+        // },
         {
           title: 'Timetable',
-          url: '',
-          size: 'small',
-          color: 'orange',
-          icon: 'clipboard',
+          url: 'student-timetable',
+          img: 'assets/img/timetable.png',
           role: Role.Student,
           tags: []
         },
         {
           title: 'Exam Schedule',
-          url: '',
-          size: 'medium',
-          color: 'purple',
-          icon: 'book',
+          url: 'exam-schedule',
+          img: 'assets/img/exam-schedule.png',
           role: Role.Student | Role.Lecturer | Role.Admin,
           tags: []
         },
         {
           title: 'My Library',
           url: 'http://opac.apiit.edu.my/cgi-bin/koha/opac-user.pl',
-          size: 'medium',
-          color: 'blue',
-          icon: 'bookmarks',
+          img: 'assets/img/my-library.png',
           role: Role.Student | Role.Lecturer | Role.Admin,
           tags: []
         },
         {
           title: 'Student Timetable',
-          url: '/student-timetable',
-          size: 'medium',
-          color: 'red',
-          icon: 'calendar',
+          url: 'student-timetable',
+          img: 'assets/img/timetable.png',
           role: Role.Lecturer | Role.Admin,
           tags: []
         },
         {
           title: 'Moodle (Course Material)',
           url: 'https://lms2.apiit.edu.my/login/index.php',
-          size: 'large',
-          color: 'dark-orange',
-          icon: 'open',
+          img: 'assets/img/moodle.png',
           role: Role.Student | Role.Lecturer | Role.Admin,
           tags: []
         },
         {
           title: 'Attendance',
-          url: '',
-          size: 'small',
-          color: 'orange',
-          icon: 'alarm',
+          url: 'attendance',
+          img: 'assets/img/attendance.png',
           role: Role.Student,
           tags: []
         },
         // {
         //   title: 'Student Survey',
         //   url: '',
-        //   size: 'medium',
-        //   color: 'green',
-        //   icon: 'alarm',
         //   role: Role.Student,
         //   tags: []
         // },
@@ -220,96 +176,75 @@ export class MorePage {
     {
       title: 'Others',
       items: [
-        {
-          title: 'Upcoming Graduations',
-          url: '',
-          size: 'xlarge',
-          color: ' orange',
-          icon: 'school',
-          role: Role.Student | Role.Lecturer | Role.Admin,
-          tags: []
-        },
+        // {
+        //   title: 'Upcoming Graduations',
+        //   url: '',
+        //   img: 'assets/img/upcoming-graduations.png',
+        //   role: Role.Student | Role.Lecturer | Role.Admin,
+        //   tags: []
+        // },
         {
           title: 'Graduate Verification Service',
-          url: '',
-          size: 'xlarge',
-          color: ' blue',
-          icon: 'document',
+          url: 'graduate-verification-service',
+          img: 'assets/img/graduate-verification-service.png',
           role: Role.Student | Role.Lecturer | Role.Admin,
           tags: []
         },
         // {
         //   title: 'Visa Renewal Status',
         //   url: '',
-        //   size: 'small',
-        //   color: ' red',
-        //   icon: 'md-globe',
         //   role: Role.Student,
         //   tags: []
         // },
         {
           title: 'Staff Directory',
           url: '',
-          size: 'medium',
-          color: 'dark-orange',
-          icon: 'people',
+          img: 'assets/img/staff-directory.png',
           role: Role.Student | Role.Lecturer | Role.Admin,
           tags: ['lecturer', 'academic']
         },
         {
           title: 'Classroom Finder',
-          url: '/classroom-finder',
-          size: 'medium',
-          color: 'purple',
-          icon: 'search',
+          url: 'classroom-finder',
+          img: 'assets/img/classroom-finder.png',
           role: Role.Student | Role.Lecturer | Role.Admin,
           tags: []
         },
-        {
-          title: 'Operation Hours',
-          url: '',
-          size: 'medium',
-          color: 'green',
-          icon: 'information-circle',
-          role: Role.Student | Role.Lecturer | Role.Admin,
-          tags: []
-        },
+        // {
+        //   title: 'Operation Hours',
+        //   url: '',
+        //   img: 'assets/img/operation-hours.png',
+        //   role: Role.Student | Role.Lecturer | Role.Admin,
+        //   tags: []
+        // },
         {
           title: 'Notification',
-          url: '',
-          size: 'small',
-          color: 'orange',
-          icon: 'notifications',
+          url: 'notifications',
+          img: 'assets/img/notifications.png',
           role: Role.Student | Role.Lecturer | Role.Admin,
           tags: []
         },
         {
           title: 'Feedback',
-          url: '',
-          size: 'small',
-          color: 'green',
-          icon: 'at',
+          url: 'feedback',
+          img: 'assets/img/feedback.png',
           role: Role.Student | Role.Lecturer | Role.Admin,
           tags: []
         },
         {
           title: 'Settings',
-          url: '',
-          icon: 'settings',
-          size: 'small',
-          color: 'blue',
+          url: 'settings',
+          img: 'assets/img/settings.png',
           role: Role.Student | Role.Lecturer | Role.Admin,
           tags: []
         },
-        {
-          title: 'Logout',
-          url: '',
-          icon: 'log-out',
-          size: 'large',
-          color: 'red',
-          role: Role.Student | Role.Lecturer | Role.Admin,
-          tags: []
-        },
+        // {
+        //   title: 'Logout',
+        //   url: 'logout',
+        //   img: 'assets/img/logout.png',
+        //   role: Role.Student | Role.Lecturer | Role.Admin,
+        //   tags: []
+        // },
       ],
     },
   ];
