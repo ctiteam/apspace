7f9ee894b68e34a58d12adfc7cfb5e1e53c84cba
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2019-08-06 00:34:45 +0800

    style(profile): re-design the page

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, NgZone, AfterViewInit, } from '@angular/core';
+import { Component, OnInit, NgZone, AfterViewInit, OnDestroy, } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { Role, StaffProfile, StudentPhoto, StudentProfile } from '../../interfaces';
-import { SettingsService, WsApiService } from '../../services';
+import { SettingsService, WsApiService, UserSettingsService } from '../../services';
 import * as am4core from '@amcharts/amcharts4/core';
 import * as am4maps from '@amcharts/amcharts4/maps';
 import am4geodata_worldLow from '@amcharts/amcharts4-geodata/worldLow';
@@ -13,11 +13,13 @@ import am4geodata_worldLow from '@amcharts/amcharts4-geodata/worldLow';
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
 })
-export class ProfilePage implements OnInit, AfterViewInit {
+export class ProfilePage implements OnInit, AfterViewInit, OnDestroy {
+  chart: am4maps.MapChart;
   photo$: Observable<StudentPhoto[]>;
   profile$: Observable<StudentProfile>;
   staffProfile$: Observable<StaffProfile[]>;
   visa$: Observable<any>;
+  activeAccentColor = '';
   contriesList: {
     alpha2Code: string,
     name: string,
@@ -32,8 +34,11 @@ export class ProfilePage implements OnInit, AfterViewInit {
     private ws: WsApiService,
     private settings: SettingsService,
     private navCtrl: NavController,
-    private zone: NgZone
-  ) { }
+    private zone: NgZone,
+    private userSettings: UserSettingsService,
+  ) {
+    this.activeAccentColor = this.userSettings.getAccentColorRgbaValue();
+  }
 
   ngOnInit() {
     this.settings.ready().then(() => {
@@ -42,11 +47,35 @@ export class ProfilePage implements OnInit, AfterViewInit {
         this.studentRole = true;
         this.photo$ = this.ws.get<StudentPhoto[]>('/student/photo', true);
         this.profile$ = this.ws.get<StudentProfile>('/student/profile', true);
-        this.getProfile();
       } else if (role && (Role.Lecturer || Role.Admin)) {
         this.staffProfile$ = this.ws.get<StaffProfile[]>('/staff/profile', true);
       }
-      this.setUserCountry();
+      this.getProfile();
+    });
+  }
+
+  ngAfterViewInit() {
+    this.ws.get<{ alpha2Code: string, name: string, demonym: string }[]>('', false,
+      {
+        auth: false,
+        url: 'https://restcountries.eu/rest/v2/all?fields=name;alpha2Code;demonym'
+      }
+    ).subscribe(
+
+      {
+        next: response => this.contriesList = response,
+        error: err => console.log(err),
+        complete: () => this.createMap()
+      }
+
+    );
+  }
+
+  ngOnDestroy() {
+    this.zone.runOutsideAngular(() => {
+      if (this.chart) {
+        this.chart.dispose();
+      }
     });
   }
 
@@ -54,13 +83,19 @@ export class ProfilePage implements OnInit, AfterViewInit {
     this.navCtrl.navigateRoot('/student-timetable');
   }
 
-  setUserCountry() {
+  getProfile() {
     const role = this.settings.get('role');
     if (role && Role.Student) {
       this.ws.get<StudentProfile>('/student/profile').pipe(
         tap(p => {
           this.countryName = p.COUNTRY;
           this.countryType = 'country';
+          if (p.COUNTRY === 'Malaysia') {
+            this.local = true;
+          } else {
+            this.local = false;
+            this.visa$ = this.getVisaStatus();
+          }
         }),
       ).subscribe();
     } else {
@@ -71,19 +106,6 @@ export class ProfilePage implements OnInit, AfterViewInit {
         }),
       ).subscribe();
     }
-  }
-
-  getProfile() {
-    this.ws.get<StudentProfile>('/student/profile').pipe(
-      tap(p => {
-        if (p.COUNTRY === 'Malaysia') {
-          this.local = true;
-        } else {
-          this.local = false;
-          this.visa$ = this.getVisaStatus();
-        }
-      }),
-    ).subscribe();
   }
 
   getCountryData(countryName: string, type: string) {
@@ -102,80 +124,55 @@ export class ProfilePage implements OnInit, AfterViewInit {
   getVisaStatus() {
     return this.ws.get<any>('/student/visa_status');
   }
-  ngAfterViewInit() {
 
-    this.ws.get<{alpha2Code: string, name: string, demonym: string}[]>('', false,
-      {
-        auth: false,
-        url: 'https://restcountries.eu/rest/v2/all?fields=name;alpha2Code;demonym'
-      }
-    ).subscribe(
-      (response) => {
-        this.contriesList = response;
-      },
-      (error) => {
-        console.log(error);
-      },
-      () => {
+  createMap() {
+    am4core.ready(() => {
+      this.zone.runOutsideAngular(() => {
+        /* Chart code */
 
-        am4core.ready(() => {
-          this.zone.runOutsideAngular(() => {
-            /* Chart code */
+        // Create map instance
+        const chart = am4core.create('map', am4maps.MapChart);
 
-            // Create map instance
-            const chart = am4core.create('map', am4maps.MapChart);
+        // Set map definition
+        chart.geodata = am4geodata_worldLow;
 
-            // Set map definition
-            chart.geodata = am4geodata_worldLow;
+        // Set projection
+        chart.projection = new am4maps.projections.NaturalEarth1();
 
-            // Set projection
-            chart.projection = new am4maps.projections.NaturalEarth1();
+        // Create map polygon series
+        const polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+        // polygonSeries.mapPolygons.template.strokeWidth = 0.5;
 
-            // Create map polygon series
-            const polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
-            // polygonSeries.mapPolygons.template.strokeWidth = 0.5;
+        // Exclude Antartica
+        polygonSeries.exclude = ['AQ'];
 
-            // Exclude Antartica
-            polygonSeries.exclude = ['AQ'];
+        // Make map load polygon (like country names) data from GeoJSON
+        polygonSeries.useGeodata = true;
 
-            // Make map load polygon (like country names) data from GeoJSON
-            polygonSeries.useGeodata = true;
+        // Configure series
+        const polygonTemplate = polygonSeries.mapPolygons.template;
+        polygonTemplate.tooltipText = '{name}';
+        polygonTemplate.properties.fill = am4core.color('rgba(' + this.activeAccentColor + ', 0.2)');
 
-            // Configure series
-            const polygonTemplate = polygonSeries.mapPolygons.template;
-            polygonTemplate.tooltipText = '{name}';
-            polygonTemplate.fill = chart.colors.getIndex(0);
+        // Create hover state and set alternative fill color
+        const hs = polygonTemplate.states.create('hover');
+        hs.properties.fill = am4core.color('rgba(' + this.activeAccentColor + ', 1)');
 
-            // Create hover state and set alternative fill color
-            const hs = polygonTemplate.states.create('hover');
-            hs.properties.fill = chart.colors.getIndex(2);
+        // Create active state
+        const activeState = polygonTemplate.states.create('active');
+        activeState.properties.fill = am4core.color('rgba(' + this.activeAccentColor + ', 1)');
 
-            // Create active state
-            const activeState = polygonTemplate.states.create('active');
-            activeState.properties.fill = chart.colors.getIndex(4);
+        chart.events.on('ready', (ev) => {
+          const countryName = polygonSeries.getPolygonById(this.getCountryData(this.countryName, this.countryType)[0].alpha2Code);
+          countryName.isActive = true;
 
-            // Create an event to toggle "active" state
-            // polygonTemplate.events.on("hit", function(ev) {
-            //   ev.target.isActive = !ev.target.isActive;
-            // })
-
-            chart.events.on('ready', (ev) => {
-              // const countryMapper = new Countries();
-              const countryName = polygonSeries.getPolygonById(this.getCountryData(this.countryName, this.countryType)[0].alpha2Code);
-              countryName.isActive = true;
-
-              setTimeout(() => {
-                chart.zoomToMapObject(countryName);
-                countryName.isActive = true;
-              }, 1500);
-            });
-
-            const graticuleSeries = chart.series.push(new am4maps.GraticuleSeries());
-          });
+          setTimeout(() => {
+            chart.zoomToMapObject(countryName, 7);
+            countryName.isActive = true;
+          }, 1500);
         });
-
-
-      }
-    );
+        this.chart = chart;
+      });
+    });
   }
 }
