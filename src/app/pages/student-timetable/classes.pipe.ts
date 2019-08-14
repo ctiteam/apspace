92216edf7b8f2d4f8d7bae7336a2b0b3c9fcbe11
Ivan Tham <pickfire@riseup.net>
2019-08-14 17:05:17 +0800

    feat(classroom-finder): add link to timetable
    - remove mulitple subscribe to async pipe
    - refactored deplicates with template outlet
    - filter by room for student-timetable
    - student timetable use manual change detection

--- src/app/pages/student-timetable/classes.pipe.ts
+++ src/app/pages/student-timetable/classes.pipe.ts
@@ -13,14 +13,22 @@ export class ClassesPipe implements PipeTransform {
   /**
    * Filter timetable by intake.
    *
-   * @param studentTimetable Array of timetable
+   * @param timetables Array of timetable
    * @param intake Filter by intake if not null
+   * @param room Filter by room if not null
+   * @returns filtered Filtered timetable
    */
-  transform(studentTimetable: StudentTimetable[] | null, intake: string): StudentTimetable[] {
-    if (intake && Array.isArray(studentTimetable)) {
-      return studentTimetable.filter(t => intake === t.INTAKE);
-    } else {
-      return []; // No timetable for staff
+  transform(timetables: StudentTimetable[], intake: string, room: string): StudentTimetable[] {
+    if (!Array.isArray(timetables)) {
+      return [];
+    } else if (intake && room) {
+      return timetables.filter(t => intake === t.INTAKE && room === t.ROOM);
+    } else if (intake) {
+      return timetables.filter(t => intake === t.INTAKE);
+    } else if (room) {
+      return timetables.filter(t => room === t.ROOM);
+    } else { // no filter provided
+      return [];
     }
   }
 
--- src/app/pages/student-timetable/student-timetable.module.ts
+++ src/app/pages/student-timetable/student-timetable.module.ts
@@ -36,7 +36,7 @@ const routes: Routes = [
     StudentTimetablePage,
     ClassesPipe,
     ThedayPipe,
-    TheWeekPipe
+    TheWeekPipe,
   ],
   entryComponents: []
 })
