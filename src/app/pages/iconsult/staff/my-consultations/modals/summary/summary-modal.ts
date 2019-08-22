d895086cf5393b23730b5ea86902dd3ff16c94f0
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2019-08-22 21:21:47 +0800

    style(summary modal): update chart colors

---
+++ src/app/pages/iconsult/staff/my-consultations/modals/summary/summary-modal.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+@Component({
+  selector: 'page-summary-modal',
+  templateUrl: 'summary-modal.html',
+  styleUrls: ['summary-modal.scss']
+})
+export class ConsultationsSummaryModalPage implements OnInit {
+  summaryDetails: any; // Coming from the page
+
+  chartData = { // Chart configurations
+    type: 'pie',
+    options: {
+      legend: {
+        display: true,
+        position: 'top',
+      },
+    },
+    data: {
+      labels: [
+        'Available',
+        'Booked',
+        'Opened',
+        'Unavailable'
+      ],
+      datasets: []
+    }
+  };
+
+
+  constructor(private modalCtrl: ModalController) { }
+
+  ngOnInit() {
+    this.chartData.data.datasets.push( // Pushing the data to the chart
+      {
+        data: [
+          +this.summaryDetails.totalBookedSlots,
+          +this.summaryDetails.totalAvailableSlots,
+          +this.summaryDetails.totalOpenedSlots,
+          +this.summaryDetails.totalUnavailalbeSlots
+        ],
+        backgroundColor: [
+          '#e54d42', '#49b571', '#3a99d9', '#a1a1a1'
+        ]
+      }
+    );
+  }
+
+  dismiss() {
+    this.modalCtrl.dismiss();
+  }
+}
