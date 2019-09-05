b74dd552d8b75fe99a6693ec36fcc57dd1e33420
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2019-09-05 20:18:23 +0800

    chore: design & create view progress report page

--- src/app/pages/aplc-progress-report/view-progress-report/view-progress-report.page.ts
+++ src/app/pages/aplc-progress-report/view-progress-report/view-progress-report.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { WsApiService } from 'src/app/services';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-view-progress-report',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-progress-report.page.scss'],
 })
 export class ViewProgressReportPage implements OnInit {
+  stagingUrl = 'https://kh1rvo4ilf.execute-api.ap-southeast-1.amazonaws.com/dev/aplc';
 
-  constructor() { }
+  subjects$: Observable<any>; // to create interface
+  classes$: Observable<any>; // to create interface
+  scoreLegend$: Observable<any>; // to create interface
+  descriptionLegend$: Observable<any>; // to create interface
+  classDescription$: Observable<any>;
+  studentsBehaviour$: Observable<any>;
+
+
+  subjectCode: string;
+  classCode: string;
+  showClassCodeLoading: false;
+
+  constructor(
+    private ws: WsApiService,
+  ) { }
 
   ngOnInit() {
+    this.initData();
+  }
+
+  initData() { // changed with refresher
+    this.subjects$ = this.ws.get<any>(`/subjects`, true, { url: this.stagingUrl });
+    this.scoreLegend$ = this.ws.get<any[]>(`/score-legend`, false, { url: this.stagingUrl });
+    this.descriptionLegend$ = this.ws.get<any[]>(`/description-legend`, false, { url: this.stagingUrl });
+  }
+
+  onSubjectCodeChange() {
+    this.classes$ = this.ws.get<any>(`/classes?subject_code=${this.subjectCode}`, true, { url: this.stagingUrl }).pipe(
+      tap(_ => this.classCode = '')
+    );
+  }
+
+  onClassCodeChange() {
+    this.classDescription$ = this.ws.get<any>(`/class-description?class_code=${this.classCode}`, true, { url: this.stagingUrl });
+    this.studentsBehaviour$ = this.ws.get<any>(`/student-behavior?class_code=${this.classCode}`, true, { url: this.stagingUrl });
   }
 
 }
