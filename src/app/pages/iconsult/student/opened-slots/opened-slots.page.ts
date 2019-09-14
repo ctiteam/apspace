8b8427f2645a36ad08dd45f8959b6089a2c57010
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2019-09-15 02:05:54 +0800

    fix(opened-slots): filter slots
    - enhance the UI/UX
    - fix issue with filter by date
    - use calendar modal for filter
    - add warning to the user when data is filtered

--- src/app/pages/iconsult/student/opened-slots/opened-slots.page.ts
+++ src/app/pages/iconsult/student/opened-slots/opened-slots.page.ts
@@ -1,59 +1,104 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { StaffDirectory, ConsultationSlot } from 'src/app/interfaces';
-// import { ActivatedRoute } from '@angular/router';
-import { MenuController, ModalController } from '@ionic/angular';
+import { ModalController } from '@ionic/angular';
 import { WsApiService } from 'src/app/services';
 import { map, tap } from 'rxjs/operators';
 import { BookSlotModalPage } from './book-slot-modal';
 import { ActivatedRoute } from 'src/testing';
-import { Router } from '@angular/router';
+import { DayConfig, CalendarComponentOptions } from 'ion2-calendar';
 
+import * as moment from 'moment';
+import { CalendarFilterModalPage } from './calendar-filter-modal/calendar-filter-modal';
 @Component({
   selector: 'app-opened-slots',
   templateUrl: './opened-slots.page.html',
   styleUrls: ['./opened-slots.page.scss'],
 })
 export class OpenedSlotsPage implements OnInit {
-  showDaysFilter = false;
   staffCasId: string; // NEED TO BE GLOBAL TO USE IT IN MANY FUNCTIONS
   staff$: Observable<StaffDirectory>;
   staff: StaffDirectory;
-  slot$: Observable<ConsultationSlot[]>; // RESPONSE STORED HERE
-  filteredSlots$: Observable<any>;  // SLOTS AFTER BEING FILTERED AND GROUPED USED FOR THE LIST OF SLOTS
-  slotsUsedForDayFilterSection$: Observable<any>; // USED FOR GETTING THE DAYS IN THE FILTER SECTION
-  slotsUsedForMonthFilterSection$: Observable<any>; // USED FOR GETTING THE MONTHS IN THE FILTER SECTION
+  slots$: Observable<{}>; // no type because of the grouping
+
+  daysConfigrations: DayConfig[] = []; // ion-calendar plugin
+  options: CalendarComponentOptions = {
+    from: new Date(),
+    to: null, // null to disable all calendar button. Days configurations will enable only dates with slots
+    daysConfig: this.daysConfigrations
+  };
+
+  dateToFilter = ''; // used in the filte pipe to filter the slots by specific date
+
   totalAvailableSlots = -1;
   totalOpenedSlots = -1;
-  filterObject = {
-    monthYear: '',
-    day: ''
-  };
+
   skeletons = ['80%', '30%', '50%', '10%', '70%'];
+
   constructor(
     private route: ActivatedRoute,
-    private router: Router,
     private ws: WsApiService,
-    private menu: MenuController,
     private modalCtrl: ModalController,
   ) { }
 
   ngOnInit() {
     this.staffCasId = this.route.snapshot.params.id;
     this.staff$ = this.getStaffProfile();
-
     this.getData();
   }
 
-  getData() { // To be changed with refresher
-    this.filteredSlots$ = this.getSlots().pipe(
-      tap(_ => this.groupSlots())
-    );
-    this.slotsUsedForDayFilterSection$ = this.getSlots().pipe(
-      tap(_ => this.prepareDayFilterData())
-    );
-    this.slotsUsedForMonthFilterSection$ = this.getSlots().pipe(
-      tap(_ => this.prepareMonthFilterData())
+  getData() { // to be changed with refresher
+    this.options = {
+      from: new Date(),
+      to: null, // null to disable all calendar button. Days configurations will enable only dates with slots
+      daysConfig: this.daysConfigrations
+    };
+    let totalAvailableSlots = 0;
+    let totalOpenedSlots = 0;
+    this.slots$ = this.ws.get<ConsultationSlot[]>('/iconsult/get_slots/' + this.staffCasId, true).pipe(
+      map(
+        slots => slots.reduce((r, a) => { // Grouping the slots daily
+          const consultationsYearMonth = a.date.split('-')[0] + '-' + a.date.split('-')[1];
+          this.totalOpenedSlots = ++totalOpenedSlots; // get the total number of opened slots
+          this.totalAvailableSlots = a.status === 'Available' ? ++totalAvailableSlots : totalAvailableSlots;
+          const consultationsDay = a.date;
+          r[consultationsYearMonth] = r[consultationsYearMonth] || {};
+          r[consultationsYearMonth][consultationsDay] = r[consultationsYearMonth][consultationsDay] || {};
+          r[consultationsYearMonth][consultationsDay].items = r[consultationsYearMonth][consultationsDay].items || [];
+          r[consultationsYearMonth][consultationsDay].items.push(a);
+          return r;
+        }, {})
+      ),
+      tap(dates => { // add css classes for slot type (used in the calendar modal)
+        Object.keys(dates).forEach(
+          monthYear => {
+            Object.keys(dates[monthYear]).forEach(
+              day => {
+                const items = dates[monthYear][day].items;
+                const numberOfAvailableAndBookedSlots =
+                  items.filter(item => item.status === 'Available' || item.status === 'Booked').length;
+                const numberOfBookedSlots = items.filter(item => item.status === 'Booked').length;
+                const cssClass = numberOfAvailableAndBookedSlots === numberOfBookedSlots && numberOfBookedSlots > 0
+                  ? `booked`
+                  : numberOfBookedSlots > 0
+                    ? `partially-booked`
+                    : numberOfBookedSlots === 0 && numberOfAvailableAndBookedSlots !== 0
+                      ? `available`
+                      : 'unavailable';
+
+                this.daysConfigrations.push({
+                  date: moment(day, 'YYYY-MM-DD').toDate(),
+                  subTitle: '.',
+                  cssClass: cssClass + ' colored',
+                  disable: false
+                });
+              }
+            );
+          }
+
+        );
+        return dates;
+      }),
     );
   }
 
@@ -61,118 +106,6 @@ export class OpenedSlotsPage implements OnInit {
     return this.ws.get<StaffDirectory[]>('/staff/listing').pipe(
       map(listOfStaff => listOfStaff.find(staff => staff.ID === this.staffCasId)),
       tap(staff => this.staff = staff),
-    );
-  }
-
-  getSlots() {
-    return this.slot$ = this.ws.get<ConsultationSlot[]>('/iconsult/get_slots/' + this.staffCasId, true);
-  }
-
-  openMenu() {
-    this.menu.enable(true, 'consultations-filter-menu');
-    this.menu.open('consultations-filter-menu');
-  }
-
-  closeMenu() {
-    this.menu.close('consultations-filter-menu');
-  }
-
-  clearFilter() {
-    this.showDaysFilter = false;
-    this.filterObject = {
-      monthYear: '',
-      day: ''
-    };
-    this.onFilter();
-  }
-
-  onFilter() {
-    const month = this.filterObject.monthYear.split(' ')[0] || '';
-    const year = this.filterObject.monthYear.split(' ')[1] || '';
-    this.filteredSlots$ = this.slot$.pipe(
-      map(slots => {
-        return slots.filter(
-          slot => slot.datetime.includes(this.filterObject.day) &&
-            slot.datetime.includes(month) &&
-            slot.date.includes(year)
-        );
-      }),
-      map((slots: ConsultationSlot[]) => {
-        return slots.reduce((r, a) => {
-          const consultationsMonth = a.datetime.split(' ')[2];
-          const consultationsYear = a.date.split('-')[0];
-          const consultationsDay = a.datetime;
-
-          r[consultationsYear] = r[consultationsYear] || {};
-          r[consultationsYear][consultationsMonth] = r[consultationsYear][consultationsMonth] || {};
-          r[consultationsYear][consultationsMonth][consultationsDay] = r[consultationsYear][consultationsMonth][consultationsDay] || [];
-          r[consultationsYear][consultationsMonth][consultationsDay].push(a);
-          return r;
-        }, {});
-      }),
-    );
-  }
-
-  groupSlots() {
-    this.filteredSlots$ = this.slot$.pipe(
-      map((slots: ConsultationSlot[]) => {
-        let totalAvailableSlots = 0;
-        let totalOpenedSlots = 0;
-        return slots.reduce((r, a) => {
-          const consultationsMonth = a.datetime.split(' ')[2];
-          const consultationsYear = a.date.split('-')[0];
-          const consultationsDay = a.datetime;
-          this.totalOpenedSlots = ++totalOpenedSlots;
-          this.totalAvailableSlots = a.status === 'Available' ? ++totalAvailableSlots : totalAvailableSlots;
-
-          r[consultationsYear] = r[consultationsYear] || {};
-          r[consultationsYear][consultationsMonth] = r[consultationsYear][consultationsMonth] || {};
-          r[consultationsYear][consultationsMonth][consultationsDay] = r[consultationsYear][consultationsMonth][consultationsDay] || [];
-          r[consultationsYear][consultationsMonth][consultationsDay].push(a);
-          return r;
-        }, {});
-      }),
-    );
-  }
-
-  prepareMonthFilterData() { // TO MODIFY
-    this.showDaysFilter = false;
-    this.slotsUsedForMonthFilterSection$ = this.slot$.pipe(
-      map((slots: ConsultationSlot[]) => {
-        return slots.reduce((r, a) => {
-          const consultationsMonth = a.datetime.split(' ')[2];
-          const consultationsYear = a.date.split('-')[0];
-          r[consultationsYear] = r[consultationsYear] || {};
-          r[consultationsYear][consultationsMonth] = r[consultationsYear][consultationsMonth] || [];
-          r[consultationsYear][consultationsMonth].push(a);
-          return r;
-        }, {});
-      }),
-    );
-  }
-
-  prepareDayFilterData() { // TO MODIFY
-    this.filterObject.day = '';
-    this.showDaysFilter = true;
-    this.slotsUsedForDayFilterSection$ = this.slot$.pipe(
-      map(slots => {
-        return slots.filter(
-          slot => slot.datetime.includes(this.filterObject.monthYear.split(' ')[0]) &&
-            slot.date.includes(this.filterObject.monthYear.split(' ')[1])
-        );
-      }),
-      map((slots: ConsultationSlot[]) => {
-        return slots.reduce((r, a) => {
-          const consultationsMonth = a.datetime.split(' ')[2];
-          const consultationsYear = a.date.split('-')[0];
-          const consultationsDay = a.datetime;
-          r[consultationsYear] = r[consultationsYear] || {};
-          r[consultationsYear][consultationsMonth] = r[consultationsYear][consultationsMonth] || {};
-          r[consultationsYear][consultationsMonth][consultationsDay] = r[consultationsYear][consultationsMonth][consultationsDay] || [];
-          r[consultationsYear][consultationsMonth][consultationsDay].push(a);
-          return r;
-        }, {});
-      }),
     );
   }
 
@@ -190,10 +123,24 @@ export class OpenedSlotsPage implements OnInit {
     await modal.onDidDismiss().then(
       data => {
         if (data.data === 'booked') {
+          this.dateToFilter = ''; // remove the filter
+          this.daysConfigrations = []; // empty days configurations used in the calendar modal and then in get slots re-set it again
           this.getData();
         }
       }
     );
+  }
+
+  async openCalendarModal() {
+    const myCalendar = await this.modalCtrl.create({
+      component: CalendarFilterModalPage,
+      componentProps: { options: this.options }
+    });
+    myCalendar.present();
+    const event: any = await myCalendar.onDidDismiss();
+    if (event.data) { // assign the data to the dateToFilter only if there is a date returned from the modal
+      this.dateToFilter = event.data;
+    }
   }
 
 }
