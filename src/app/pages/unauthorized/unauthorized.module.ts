b33bf3dc0cef6b9795fb8caf74c06f082633d8a6
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2019-10-11 01:44:56 +0800

    feat: create unauthorized & 404 pages

---
+++ src/app/pages/unauthorized/unauthorized.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { UnauthorizedPage } from './unauthorized.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: UnauthorizedPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [UnauthorizedPage]
+})
+export class UnauthorizedPageModule {}
---
+++ src/app/pages/unauthorized/unauthorized.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-unauthorized',
+  templateUrl: './unauthorized.page.html',
+  styleUrls: ['./unauthorized.page.scss'],
+})
+export class UnauthorizedPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
