a83d4ee1cc650f847cfef415e58c0616e6540f2d
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2019-10-10 15:53:19 +0800

    chore: remove unused service

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -11,7 +11,6 @@ export { DataCollectorService } from './data-collector.service';
 export { FeedbackService } from './feedback.service';
 export { NewsService } from './news.service';
 export { NotificationService } from './notification.service';
-export { OperationHoursService } from './operation-hours.service';
 export { SettingsService } from './settings.service';
 export { StudentTimetableService } from './student-timetable.service';
 export { VersionService } from './version.service';
