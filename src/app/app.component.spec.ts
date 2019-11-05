7c9f3d88371216290826e684807b5c42871c92dd
Ivan Tham <pickfire@riseup.net>
2019-11-05 23:05:27 +0800

    test(app): add mobile and web tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,43 +1,114 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, TestBed } from '@angular/core/testing';
+// import { APP_BASE_HREF, PathLocationStrategy, Location, LocationStrategy } from '@angular/common';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { Platform } from '@ionic/angular';
+import { FCM } from '@ionic-native/fcm/ngx';
+import { Network } from '@ionic-native/network/ngx';
+import { ModalController, Platform, PopoverController } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
+
+import { NEVER } from 'rxjs';
 
 import { AppComponent } from './app.component';
+import { NotificationService, UserSettingsService, VersionService } from './services';
 
 describe('AppComponent', () => {
-
-  let statusBarSpy, platformReadySpy, platformSpy;
+  // let routerSpy: { url: jasmine.Spy };
+  let fcmSpy: { onNotification: jasmine.Spy };
+  let networkSpy: { type: jasmine.Spy };
+  let userSettingsServiceSpy: {
+    getUserSettingsFromStorage: jasmine.Spy;
+    darkThemeActivated: jasmine.Spy;
+    PureDarkThemeActivated: jasmine.Spy;
+    getAccentColor: jasmine.Spy;
+  };
+  let versionServiceSpy: { checkForUpdate: jasmine.Spy };
 
   beforeEach(async(() => {
-    statusBarSpy = jasmine.createSpyObj('StatusBar', ['styleDefault']);
-    platformReadySpy = Promise.resolve();
-    platformSpy = jasmine.createSpyObj('Platform', { ready: platformReadySpy });
+    // routerSpy = jasmine.createSpyObj('Router', ['url']);
+    fcmSpy = jasmine.createSpyObj('FCM', ['onNotification']);
+    networkSpy = jasmine.createSpyObj('Network', ['type']);
+    userSettingsServiceSpy = jasmine.createSpyObj('UserSettingsService',
+      ['getUserSettingsFromStorage', 'darkThemeActivated', 'PureDarkThemeActivated', 'getAccentColor']);
+    versionServiceSpy = jasmine.createSpyObj('VersionService', ['checkForUpdate']);
 
     TestBed.configureTestingModule({
       declarations: [AppComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       providers: [
-        { provide: StatusBar, useValue: statusBarSpy },
-        { provide: Platform, useValue: platformSpy },
+        // Location,
+        // { provide: LocationStrategy, useValue: PathLocationStrategy },
+        // { provide: APP_BASE_HREF, useValue: '/' },
+        // { provide: Router, useValue: routerSpy },
+        Platform,
+        { provide: FCM, useValue: fcmSpy },
+        { provide: ModalController, useValue: {} },
+        { provide: Network, useValue: networkSpy },
+        { provide: NotificationService, useValue: {} },
+        { provide: PopoverController, useValue: {} },
+        { provide: Storage, useValue: {} },
+        { provide: UserSettingsService, useValue: userSettingsServiceSpy },
+        { provide: VersionService, useValue: versionServiceSpy },
+      ],
+      imports: [
+        RouterTestingModule.withRoutes([])
       ],
     }).compileComponents();
   }));
 
   it('should create the app', () => {
+    fcmSpy.onNotification.and.returnValue(NEVER);
+    networkSpy.type.and.returnValue('wifi');
+    userSettingsServiceSpy.darkThemeActivated.and.callFake(() => NEVER);
+    userSettingsServiceSpy.PureDarkThemeActivated.and.callFake(() => NEVER);
+    userSettingsServiceSpy.getAccentColor.and.callFake(() => NEVER);
+    versionServiceSpy.checkForUpdate.and.callFake(() => NEVER);
+
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it('should initialize the app', async () => {
-    TestBed.createComponent(AppComponent);
-    expect(platformSpy.ready).toHaveBeenCalled();
-    await platformReadySpy;
-    expect(statusBarSpy.styleDefault).toHaveBeenCalled();
+  it('should initialize the app (mobile)', () => {
+    // routerSpy.url.and.returnValue('/tabs');
+    fcmSpy.onNotification.and.returnValue(NEVER);
+    networkSpy.type.and.returnValue('wifi');
+    userSettingsServiceSpy.darkThemeActivated.and.callFake(() => NEVER);
+    userSettingsServiceSpy.PureDarkThemeActivated.and.callFake(() => NEVER);
+    userSettingsServiceSpy.getAccentColor.and.callFake(() => NEVER);
+    versionServiceSpy.checkForUpdate.and.callFake(() => NEVER);
+
+    const platform = TestBed.get(Platform);
+    spyOn(platform, 'is').and.callFake(() => 'cordova');
+    const fixture = TestBed.createComponent(AppComponent);
+    const router = TestBed.get(Router);
+    const navigateSpy = spyOn(router, 'navigate');
+
+    fixture.detectChanges();
+    expect(fcmSpy.onNotification).toHaveBeenCalled();
+    expect(navigateSpy).not.toHaveBeenCalled();
+    expect(platform.is).toHaveBeenCalledTimes(1);
+    expect(versionServiceSpy.checkForUpdate).toHaveBeenCalledTimes(1);
   });
 
-  // TODO: add more tests!
+  it('should initialize the app (web)', () => {
+    // routerSpy.url.and.returnValue('/tabs');
+    fcmSpy.onNotification.and.returnValue(NEVER);
+    userSettingsServiceSpy.darkThemeActivated.and.callFake(() => NEVER);
+    userSettingsServiceSpy.PureDarkThemeActivated.and.callFake(() => NEVER);
+    userSettingsServiceSpy.getAccentColor.and.callFake(() => NEVER);
+    versionServiceSpy.checkForUpdate.and.callFake(() => NEVER);
+
+    const fixture = TestBed.createComponent(AppComponent);
+    const router = TestBed.get(Router);
+    const navigateSpy = spyOn(router, 'navigate');
+
+    fixture.detectChanges();
+    expect(fcmSpy.onNotification).not.toHaveBeenCalled();
+    expect(navigateSpy).not.toHaveBeenCalled();
+    expect(versionServiceSpy.checkForUpdate).toHaveBeenCalledTimes(1);
+  });
 
 });
