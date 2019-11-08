7d912a76ddcf5d888894fed274bdea5d9672a9ed
Ivan Tham <pickfire@riseup.net>
2019-11-08 16:40:04 +0800

    test(app-launcher): fix test

--- src/app/services/app-launcher.service.spec.ts
+++ src/app/services/app-launcher.service.spec.ts
@@ -1,9 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
+import { AppAvailability } from '@ionic-native/app-availability/ngx';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+
 import { AppLauncherService } from './app-launcher.service';
 
 describe('AppLauncherService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: AppAvailability, useValue: {} },
+        { provide: InAppBrowser, useValue: {} },
+      ]
+    });
+  });
 
   it('should be created', () => {
     const service: AppLauncherService = TestBed.get(AppLauncherService);
