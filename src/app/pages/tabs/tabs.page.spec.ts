ab8be15368134afb0eb4202ee838243fcd96b19a
Ivan Tham <pickfire@riseup.net>
2019-11-08 17:10:45 +0800

    test(tabs): fix test

--- src/app/pages/tabs/tabs.page.spec.ts
+++ src/app/pages/tabs/tabs.page.spec.ts
@@ -1,18 +1,26 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { SettingsService } from '../../services';
 import { TabsPage } from './tabs.page';
 
 describe('TabsPage', () => {
   let component: TabsPage;
   let fixture: ComponentFixture<TabsPage>;
+  let settings: { get: jasmine.Spy };
 
   beforeEach(async(() => {
+    settings = jasmine.createSpyObj('SettingsService', ['get']);
+
     TestBed.configureTestingModule({
       declarations: [TabsPage],
+      providers: [
+        { provide: SettingsService, useValue: settings },
+      ],
+      imports: [RouterTestingModule],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    })
-      .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,6 +30,7 @@ describe('TabsPage', () => {
   });
 
   it('should create', () => {
+    settings.get.and.returnValue(null);
     expect(component).toBeTruthy();
   });
 });
