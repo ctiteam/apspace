1249f468b4f2c1aa3771880d17d9403438b7c0dc
Ivan Tham <pickfire@riseup.net>
2019-11-08 14:44:46 +0800

    test(version): fix test

--- src/app/services/version.service.spec.ts
+++ src/app/services/version.service.spec.ts
@@ -1,9 +1,24 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { Network } from '@ionic-native/network/ngx';
+import { Storage } from '@ionic/storage';
 
 import { VersionService } from './version.service';
+import { WsApiService } from './ws-api.service';
 
 describe('VersionService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: WsApiService, useValue: {} },
+        { provide: InAppBrowser, useValue: {} },
+        { provide: Network, useValue: {} },
+        { provide: Storage, useValue: {} },
+      ],
+      imports: [RouterTestingModule]
+    });
+  });
 
   it('should be created', () => {
     const service: VersionService = TestBed.get(VersionService);
