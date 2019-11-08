9f2a0c9af5acfc845027a969f3268d3d174c3f72
Ivan Tham <pickfire@riseup.net>
2019-11-08 14:49:32 +0800

    test(change-password): fix test

--- src/app/services/change-password.service.spec.ts
+++ src/app/services/change-password.service.spec.ts
@@ -1,9 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ChangePasswordService } from './change-password.service';
+import { WsApiService } from './ws-api.service';
 
 describe('ChangePasswordService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: WsApiService, useValue: {} },
+      ]
+    });
+  });
 
   it('should be created', () => {
     const service: ChangePasswordService = TestBed.get(ChangePasswordService);
