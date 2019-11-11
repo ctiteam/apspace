82c78b567a21b0a3d6d8f57107bbf64bdfc5ba14
Ivan Tham <pickfire@riseup.net>
2019-11-11 14:50:37 +0800

    test(deauth): add and fix tests

--- src/app/guards/deauth.guard.spec.ts
+++ src/app/guards/deauth.guard.spec.ts
@@ -1,28 +1,38 @@
 import { inject, TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { CasTicketService } from '../services';
 import { DeauthGuard } from './deauth.guard';
 
 describe('DeauthGuard', () => {
+  let casTicketServiceSpy: { isAuthenticated: jasmine.Spy };
+
   beforeEach(() => {
-    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
-    const casTicketServiceSpy = jasmine.createSpyObj('CasTicketService', {
-      isAuthenticated: Promise.resolve(true)
-    });
+    casTicketServiceSpy = jasmine.createSpyObj('CasTicketService', ['isAuthenticated']);
 
     TestBed.configureTestingModule({
       providers: [
         DeauthGuard,
         { provide: CasTicketService, useValue: casTicketServiceSpy },
-        { provide: Router, useValue: routerSpy }
-      ]
+      ],
+      imports: [RouterTestingModule]
     });
   });
 
-  it('should redirect if not logged in', inject([Router, DeauthGuard], async (router, guard) => {
-    expect(await guard.canActivate()).toBeFalsy();
-    expect(router.navigate.calls.count()).toBe(1);
-    expect(router.navigate.calls.first().args[0]).toEqual(['/']);
+  it('should redirect if logged in', inject([Router, DeauthGuard], async (router, guard) => {
+    spyOn(router, 'createUrlTree').and.callThrough();
+    casTicketServiceSpy.isAuthenticated.and.returnValue(Promise.resolve(true));
+
+    const expected = router.parseUrl('/');
+    expected.fragment = undefined;
+    expect(await guard.canActivate()).toEqual(expected);
+  }));
+
+  it('should not redirect if not logged in', inject([Router, DeauthGuard], async (router, guard) => {
+    spyOn(router, 'createUrlTree').and.callThrough();
+    casTicketServiceSpy.isAuthenticated.and.returnValue(Promise.resolve(false));
+
+    expect(await guard.canActivate()).toEqual(true);
   }));
 });
