6665b1c66781da29a0fd5b40aa6f3afc59a14860
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2019-11-12 11:23:17 +0800

    refactor(tabs): remove animation

--- src/app/pages/tabs/tabs.page.ts
+++ src/app/pages/tabs/tabs.page.ts
@@ -9,24 +9,7 @@ import { TabItem } from './tab-item.interface';
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.page.html',
-  styleUrls: ['./tabs.page.scss'],
-  animations: [
-    trigger('swing_me', [
-      transition('false => true', [
-        style({
-          'transform-origin': 'top center',
-          'animation-fill-mode': 'both'
-        }),
-        animate('1s', keyframes([
-          style({ transform: 'rotate3d(0, 0, 1, 15deg)', offset: 0.2 }),
-          style({ transform: 'rotate3d(0, 0, 1, -10deg)', offset: 0.4 }),
-          style({ transform: 'rotate3d(0, 0, 1, 5deg)', offset: 0.6 }),
-          style({ transform: 'rotate3d(0, 0, 1, -5deg)', offset: 0.8 }),
-          style({ transform: 'rotate3d(0, 0, 1, 0deg)', offset: 1 }),
-        ]))
-      ])
-    ])
-  ]
+  styleUrls: ['./tabs.page.scss']
 })
 export class TabsPage implements OnInit {
   selectedTab: string;
