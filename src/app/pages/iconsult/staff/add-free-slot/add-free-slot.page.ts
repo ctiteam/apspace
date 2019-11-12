3ff2223bcce2a13bdf16e5ee79895e11f76cb1b9
Louis We <we.yuan@staffemail.apu.edu.my>
2019-11-12 14:01:53 +0800

    chore(add-free-slot): rearrange code to adapt new Iconsult API

--- src/app/pages/iconsult/staff/add-free-slot/add-free-slot.page.ts
+++ src/app/pages/iconsult/staff/add-free-slot/add-free-slot.page.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NavigationExtras, Router } from '@angular/router';
-import { AlertController, LoadingController, ToastController } from '@ionic/angular';
+import {
+  AlertController,
+  LoadingController,
+  ToastController
+} from '@ionic/angular';
 import { Observable } from 'rxjs';
 
 // import { toastMessageEnterAnimation } from 'src/app/animations/toast-message-animation/enter';
@@ -15,7 +19,7 @@ import * as moment from 'moment';
 @Component({
   selector: 'app-add-free-slot',
   templateUrl: './add-free-slot.page.html',
-  styleUrls: ['./add-free-slot.page.scss'],
+  styleUrls: ['./add-free-slot.page.scss']
 })
 export class AddFreeSlotPage implements OnInit {
   venues$: Observable<Venue[]>;
@@ -27,15 +31,25 @@ export class AddFreeSlotPage implements OnInit {
 
   // Options for the ion-calendar (start date)
   mainDateOptions: CalendarComponentOptions = {
-    from: moment(this.todaysDate).add(1, 'day').toDate(),
-    to: moment(this.todaysDate).add(1, 'day').add(12, 'month').toDate(),
+    from: moment(this.todaysDate)
+      .add(1, 'day')
+      .toDate(),
+    to: moment(this.todaysDate)
+      .add(1, 'day')
+      .add(12, 'month')
+      .toDate(),
     disableWeeks: [0]
   };
 
   // Options for the ion-calendar (end date)
   repeatUntilDateOptions: CalendarComponentOptions = {
-    from: moment(this.todaysDate).add(2, 'day').toDate(),
-    to: moment(this.todaysDate).add(1, 'day').add(12, 'month').toDate()
+    from: moment(this.todaysDate)
+      .add(2, 'day')
+      .toDate(),
+    to: moment(this.todaysDate)
+      .add(1, 'day')
+      .add(12, 'month')
+      .toDate()
   };
 
   consultationTypeOptions = [
@@ -50,13 +64,10 @@ export class AddFreeSlotPage implements OnInit {
     { name: 'Wednesday', value: 'Wed' },
     { name: 'Thursday', value: 'Thu' },
     { name: 'Friday', value: 'Fri' },
-    { name: 'Saturday', value: 'Sat' },
+    { name: 'Saturday', value: 'Sat' }
   ];
 
-  locationOptions = [
-    'New Campus',
-    'TPM',
-  ];
+  locationOptions = ['New Campus', 'TPM'];
 
   constructor(
     private ws: WsApiService,
@@ -66,29 +77,39 @@ export class AddFreeSlotPage implements OnInit {
     private alertCtrl: AlertController,
     private loadingController: LoadingController,
     private settings: SettingsService
-  ) { }
+  ) {}
 
   ngOnInit() {
     if (this.settings.get('defaultCampus')) {
-      this.venues$ = this.ws.get<Venue[]>(`/iconsult/getvenues/${this.settings.get('defaultCampus')}`);
+      this.venues$ = this.ws.get<Venue[]>(
+        `/iconsult/locations?venue=${this.settings.get('defaultCampus')}`,
+        {
+          url: 'https://x8w3m20p69.execute-api.ap-southeast-1.amazonaws.com/dev'
+        }
+      );
     }
     this.addFreeSlotForm = this.formBuilder.group({
       slotType: [this.consultationTypeOptions[0].value, Validators.required], // alwayes required
-      startDate: [moment(this.todaysDate).add(1, 'day').format('YYYY-MM-DD'), Validators.required], // always required
+      startDate: [
+        moment(this.todaysDate)
+          .add(1, 'day')
+          .format('YYYY-MM-DD'),
+        Validators.required
+      ], // always required
       repeatOn: [[]],
       noOfWeeks: [null],
       endDate: [''],
       location: [this.settings.get('defaultCampus') || '', Validators.required], // always required
       venue: [this.settings.get('defaultVenue') || '', Validators.required], // alwayes required
-      time: this.formBuilder.array([
-        this.initTimeSlots(),
-      ]),
+      time: this.formBuilder.array([this.initTimeSlots()])
     });
+
+    this.addFreeSlotForm.valueChanges.subscribe(value => console.log(value));
   }
 
   initTimeSlots(): FormGroup {
     return this.formBuilder.group({
-      slotsTime: ['', Validators.required], // alwayes required
+      slotsTime: ['', Validators.required] // alwayes required
     });
   }
 
@@ -112,25 +133,27 @@ export class AddFreeSlotPage implements OnInit {
   }
 
   showDefaultLocationWarningAlert(newCampus: string, newVenue: string) {
-    this.alertCtrl.create({
-      header: 'Updating Your Default Location?',
-      subHeader:
-      // tslint:disable-next-line: max-line-length
-      'We noticed that you have entered a new location for your consultation hour. Do you want to use the new location as your default iConsult location?',
-      buttons: [
-        {
-          text: 'No',
-          handler: () => { }
-        },
-        {
-          text: 'Yes',
-          handler: () => {
-            this.settings.set('defaultCampus', newCampus);
-            this.settings.set('defaultVenue', newVenue);
+    this.alertCtrl
+      .create({
+        header: 'Updating Your Default Location?',
+        subHeader:
+          // tslint:disable-next-line: max-line-length
+          'We noticed that you have entered a new location for your consultation hour. Do you want to use the new location as your default iConsult location?',
+        buttons: [
+          {
+            text: 'No',
+            handler: () => {}
+          },
+          {
+            text: 'Yes',
+            handler: () => {
+              this.settings.set('defaultCampus', newCampus);
+              this.settings.set('defaultVenue', newVenue);
+            }
           }
-        }
-      ]
-    }).then(confirm => confirm.present());
+        ]
+      })
+      .then(confirm => confirm.present());
   }
 
   submit() {
@@ -139,96 +162,124 @@ export class AddFreeSlotPage implements OnInit {
     if (this.addFreeSlotForm.invalid) {
       return;
     }
-    const body = {
-      date: '',
-      end_date: '',
-      entry_datetime: '', // always empty from backend
-      location_id: this.addFreeSlotForm.value.venue,
-      repeat: [],
-      rule_status: '', // always empty from backend
-      start_date: '',
-      start_time: this.addFreeSlotForm.value.time.map(el => moment(el.slotsTime).format('kk:mm')),
-      user_id: '' // alwayes empty from backend
-    };
+    const body = [
+      {
+        location_id: this.addFreeSlotForm.value.venue,
+        datetime:
+          this.addFreeSlotForm.value.startDate +
+          ' ' +
+          this.addFreeSlotForm.value.time.map(el =>
+            moment(el.slotsTime).format('HH:mm:00')
+          )
+      }
+    ];
+
+    console.log(body);
 
     // Temp solution until the backend change the methods
     // Only adding the needed data from the form
-    if (this.addFreeSlotForm.value.slotType === this.consultationTypeOptions[0].value) {
-      // Single Slot
-      body.date = this.addFreeSlotForm.value.startDate;
-    } else if (this.addFreeSlotForm.value.slotType === this.consultationTypeOptions[1].value) {
-      // Repeat weekly
-      body.start_date = this.addFreeSlotForm.value.startDate;
-      body.end_date = moment(this.addFreeSlotForm.value.startDate, 'YYYY-MM-DD')
-        .add((+this.addFreeSlotForm.value.noOfWeeks * 7) - 1, 'days')
-        .format('YYYY-MM-DD');
-      body.repeat = this.addFreeSlotForm.value.repeatOn;
-    } else if (this.addFreeSlotForm.value.slotType === this.consultationTypeOptions[2].value) {
-      // Repeat Until a specific Date
-      body.start_date = this.addFreeSlotForm.value.startDate;
-      body.end_date = this.addFreeSlotForm.value.endDate;
-      body.repeat = this.addFreeSlotForm.value.repeatOn;
-    }
-    this.alertCtrl.create({
-      header: 'Adding new slot(s)',
-      subHeader: 'Are you sure you want to add new slot(s) with the following details:',
-      message: `<p><strong>Slot Date: </strong> ${this.addFreeSlotForm.value.startDate}</p>
-                <p><strong>Slot End Date: </strong> ${this.addFreeSlotForm.value.endDate || 'N/A'}</p>
-                <p><strong>Slot Time: </strong> ${body.start_time.toString()}</p>
-                <p><strong>Slot Location: </strong> ${this.addFreeSlotForm.value.location}</p>
-                <p><strong>Slot Venue: </strong> ${this.addFreeSlotForm.value.venue} </p>`,
-      buttons: [
-        {
-          text: 'No',
-          handler: () => { }
-        },
-        {
-          text: 'Yes',
-          handler: () => {
-            this.presentLoading();
-            this.ws.post<any>('/iconsult/lecaddfreeslots', { body }).subscribe({
-              next: () => {
-                this.showToastMessage('Slot(s) added successfully!', 'success');
-              },
-              error: () => {
-                this.dismissLoading();
-                this.showToastMessage('Something went wrong! please try again or contact us via the feedback page', 'danger');
-              },
-              complete: () => {
-                if (this.addFreeSlotForm.value.venue !== this.settings.get('defaultVenue')) {
-                  this.showDefaultLocationWarningAlert(this.addFreeSlotForm.value.location, this.addFreeSlotForm.value.venue);
-                }
-                this.dismissLoading();
-                const navigationExtras: NavigationExtras = {
-                  state: { reload: true }
-                };
-                this.router.navigateByUrl('iconsult/my-consultations', navigationExtras);
-              }
-            });
+    // if (this.addFreeSlotForm.value.slotType === this.consultationTypeOptions[0].value) {
+    //   // Single Slot
+    //   body.date = this.addFreeSlotForm.value.startDate;
+    // } else if (this.addFreeSlotForm.value.slotType === this.consultationTypeOptions[1].value) {
+    //   // Repeat weekly
+    //   body.start_date = this.addFreeSlotForm.value.startDate;
+    //   body.end_date = moment(this.addFreeSlotForm.value.startDate, 'YYYY-MM-DD')
+    //     .add((+this.addFreeSlotForm.value.noOfWeeks * 7) - 1, 'days')
+    //     .format('YYYY-MM-DD');
+    //   body.repeat = this.addFreeSlotForm.value.repeatOn;
+    // } else if (this.addFreeSlotForm.value.slotType === this.consultationTypeOptions[2].value) {
+    //   // Repeat Until a specific Date
+    //   body.start_date = this.addFreeSlotForm.value.startDate;
+    //   body.end_date = this.addFreeSlotForm.value.endDate;
+    //   body.repeat = this.addFreeSlotForm.value.repeatOn;
+    // }
+    this.alertCtrl
+      .create({
+        header: 'Adding new slot(s)',
+        subHeader:
+          'Are you sure you want to add new slot(s) with the following details:',
+        // message: `<p><strong>Slot Date: </strong> ${this.addFreeSlotForm.value.startDate}</p>
+        //           <p><strong>Slot End Date: </strong> ${this.addFreeSlotForm.value.endDate || 'N/A'}</p>
+        //           <p><strong>Slot Time: </strong> ${body.datetime.toString()}</p>
+        //           <p><strong>Slot Location: </strong> ${this.addFreeSlotForm.value.location}</p>
+        //           <p><strong>Slot Venue: </strong> ${this.addFreeSlotForm.value.venue} </p>`,
+        buttons: [
+          {
+            text: 'No',
+            handler: () => {}
+          },
+          {
+            text: 'Yes',
+            handler: () => {
+              this.presentLoading();
+              this.ws
+                .post<any>('/iconsult/slot?', {
+                  body,
+                  url:
+                    'https://x8w3m20p69.execute-api.ap-southeast-1.amazonaws.com/dev'
+                })
+                .subscribe({
+                  next: () => {
+                    this.showToastMessage(
+                      'Slot(s) added successfully!',
+                      'success'
+                    );
+                  },
+                  error: () => {
+                    this.dismissLoading();
+                    this.showToastMessage(
+                      'Something went wrong! please try again or contact us via the feedback page',
+                      'danger'
+                    );
+                  },
+                  complete: () => {
+                    if (
+                      this.addFreeSlotForm.value.venue !==
+                      this.settings.get('defaultVenue')
+                    ) {
+                      this.showDefaultLocationWarningAlert(
+                        this.addFreeSlotForm.value.location,
+                        this.addFreeSlotForm.value.venue
+                      );
+                    }
+                    this.dismissLoading();
+                    const navigationExtras: NavigationExtras = {
+                      state: { reload: true }
+                    };
+                    this.router.navigateByUrl(
+                      'iconsult/my-consultations',
+                      navigationExtras
+                    );
+                  }
+                });
+            }
           }
-        }
-      ]
-    }).then(confirm => confirm.present());
+        ]
+      })
+      .then(confirm => confirm.present());
   }
 
   showToastMessage(message: string, color: 'danger' | 'success') {
-    this.toastCtrl.create({
-      message,
-      duration: 5000,
-      position: 'top',
-      color,
-      showCloseButton: true,
-      animated: true,
-      // enterAnimation: toastMessageEnterAnimation,
-      // leaveAnimation: toastMessageLeaveAnimation
-    }).then(toast => toast.present());
+    this.toastCtrl
+      .create({
+        message,
+        duration: 5000,
+        position: 'top',
+        color,
+        showCloseButton: true,
+        animated: true
+        // enterAnimation: toastMessageEnterAnimation,
+        // leaveAnimation: toastMessageLeaveAnimation
+      })
+      .then(toast => toast.present());
   }
   async presentLoading() {
     this.loading = await this.loadingController.create({
       spinner: 'dots',
       duration: 5000,
       message: 'Please wait...',
-      translucent: true,
+      translucent: true
     });
     return await this.loading.present();
   }
@@ -278,45 +329,65 @@ export class AddFreeSlotPage implements OnInit {
       // remove validation from number of weeks when type is date range
       this.formFields.noOfWeeks.setValidators(null);
       this.formFields.noOfWeeks.updateValueAndValidity();
-
     }
   }
 
   locationChanged(event) {
     // When the user changes the location, get the list of venues again
+    console.log(event);
     this.formFields.venue.setValue('');
     // this.venueFieldDisabled = true;
-    this.venues$ = this.ws.get<Venue[]>(`/iconsult/getvenues/${event.detail.value}`);
+    this.venues$ = this.ws.get<Venue[]>(
+      `/iconsult/locations?venue=${event.detail.value}`,
+      {
+        url: 'https://x8w3m20p69.execute-api.ap-southeast-1.amazonaws.com/dev'
+      }
+    );
   }
 
   mainDateChanged(event) {
     // Calculate the range of date the user can select when the start date is changed
     // applicable only when the slot type is range
-    if (this.formFields.slotType.value === this.consultationTypeOptions[2].value) {
+    if (
+      this.formFields.slotType.value === this.consultationTypeOptions[2].value
+    ) {
       this.repeatUntilDateOptions = {
-        from: moment(event, 'YYYY-MM-DD').add(1, 'day').toDate(),
-        to: moment(this.todaysDate).add(1, 'day').add(12, 'month').toDate(),
+        from: moment(event, 'YYYY-MM-DD')
+          .add(1, 'day')
+          .toDate(),
+        to: moment(this.todaysDate)
+          .add(1, 'day')
+          .add(12, 'month')
+          .toDate()
       };
       // Set the end date to the first available day
-      this.formFields.endDate.setValue(moment(event, 'YYYY-MM-DD').add(1, 'day').format('YYYY-MM-DD'));
+      this.formFields.endDate.setValue(
+        moment(event, 'YYYY-MM-DD')
+          .add(1, 'day')
+          .format('YYYY-MM-DD')
+      );
     }
   }
 
   // Checking for duplication in slots time and remove it when the user enters the data
   timeChanged(index: number) {
     const control = this.formFields.time as FormArray;
-    this.formFields.time.value.map(
-      el => moment(el.slotsTime).format('kk:mm')
-    ).filter((v, i) => {
-      if (this.formFields.time.value.map(
-        el => moment(el.slotsTime).format('kk:mm')
-      ).indexOf(v) !== i) {
-        control.removeAt(index);
-      }
-      return this.formFields.time.value.map(
-        el => moment(el.slotsTime).format('kk:mm')
-      ).indexOf(v) === i;
-    });
+    console.log('timechanged ', index, control);
+    this.formFields.time.value
+      .map(el => moment(el.slotsTime).format('kk:mm'))
+      .filter((v, i) => {
+        if (
+          this.formFields.time.value
+            .map(el => moment(el.slotsTime).format('kk:mm'))
+            .indexOf(v) !== i
+        ) {
+          control.removeAt(index);
+        }
+        return (
+          this.formFields.time.value
+            .map(el => moment(el.slotsTime).format('kk:mm'))
+            .indexOf(v) === i
+        );
+      });
   }
-
 }
