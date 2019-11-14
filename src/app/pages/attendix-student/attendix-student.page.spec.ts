25295a66e72c366c05a148680e553650e8405979
Ivan Tham <pickfire@riseup.net>
2019-11-14 19:07:18 +0800

    test(attendix-student): fix test

--- src/app/pages/attendix-student/attendix-student.page.spec.ts
+++ src/app/pages/attendix-student/attendix-student.page.spec.ts
@@ -1,24 +1,40 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { QRScanner } from '@ionic-native/qr-scanner/ngx';
+import {
+  ApolloTestingController, ApolloTestingModule
+} from 'apollo-angular/testing';
 
+import { SettingsService } from '../../services';
 import { AttendixStudentPage } from './attendix-student.page';
 
 describe('AttendixStudentPage', () => {
   let component: AttendixStudentPage;
   let fixture: ComponentFixture<AttendixStudentPage>;
+  let apollo: ApolloTestingController;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AttendixStudentPage ],
+      declarations: [AttendixStudentPage],
+      providers: [
+        { provide: QRScanner, useValue: {} },
+        { provide: SettingsService, useValue: {} },
+      ],
+      imports: [ApolloTestingModule],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    })
-    .compileComponents();
+    }).compileComponents();
+
+    apollo = TestBed.get(ApolloTestingController);
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AttendixStudentPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    apollo.verify();
   });
 
   it('should create', () => {
