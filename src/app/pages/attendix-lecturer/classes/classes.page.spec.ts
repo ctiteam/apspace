5c64ed8e886a77257799f499cb7f4b742e473773
Ivan Tham <pickfire@riseup.net>
2019-11-14 17:55:39 +0800

    test(attendix-lecturer): fix test

--- src/app/pages/attendix-lecturer/classes/classes.page.spec.ts
+++ src/app/pages/attendix-lecturer/classes/classes.page.spec.ts
@@ -1,27 +1,44 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonSelect, ModalController } from '@ionic/angular';
+import { NEVER } from 'rxjs';
 
+import { StudentTimetableService, WsApiService } from '../../../services';
 import { ClassesPage } from './classes.page';
 
 describe('ClassesPage', () => {
   let component: ClassesPage;
   let fixture: ComponentFixture<ClassesPage>;
+  let ttSpy: jasmine.SpyObj<StudentTimetableService>;
+  let wsSpy: jasmine.SpyObj<WsApiService>;
 
   beforeEach(async(() => {
+    ttSpy = jasmine.createSpyObj('StudentTimetableService', ['get']);
+    wsSpy = jasmine.createSpyObj('WsApiService', ['get']);
+
     TestBed.configureTestingModule({
-      declarations: [ClassesPage],
+      declarations: [ClassesPage, IonSelect],
+      providers: [
+        { provide: ModalController, useValue: {} },
+        { provide: StudentTimetableService, useValue: ttSpy },
+        { provide: WsApiService, useValue: wsSpy },
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    })
-      .compileComponents();
+    }).compileComponents();
   }));
 
-  beforeEach(() => {
+  it('should create', () => {
+    ttSpy.get.and.returnValue(NEVER);
+    wsSpy.get.and.returnValue(NEVER);
+
     fixture = TestBed.createComponent(ClassesPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
 
-  it('should create', () => {
     expect(component).toBeTruthy();
+    expect(ttSpy.get).toHaveBeenCalledTimes(1);
+    expect(wsSpy.get).toHaveBeenCalledTimes(2);
+    expect(wsSpy.get).toHaveBeenCalledWith('/staff/profile', { caching: 'cache-only' });
+    expect(wsSpy.get).toHaveBeenCalledWith('/attendix/classcodes');
   });
 });
