0c987a667994e09b3758bffcce4226539ab43c14
Ivan Tham <pickfire@riseup.net>
2019-11-15 16:53:34 +0800

    test(more): fix test

--- src/app/pages/more/more.page.spec.ts
+++ src/app/pages/more/more.page.spec.ts
@@ -1,31 +1,40 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
-import { Router } from '@angular/router';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
-import { CasTicketService } from '../../services';
+import { Network } from '@ionic-native/network/ngx';
+import { NavController } from '@ionic/angular';
 
 import { asyncData } from '../../../testing';
+import { CasTicketService, SettingsService, UserSettingsService } from '../../services';
 import { FuseModule } from '../../shared/fuse/fuse.module';
+import { ByGroupPipe } from './by-group.pipe';
 import { MorePage } from './more.page';
 
 describe('MorePage', () => {
   let component: MorePage;
   let fixture: ComponentFixture<MorePage>;
-  let routerSpy: jasmine.Spy;
+  let navCtrlSpy: jasmine.Spy;
+  let userSettingsServiceSpy: jasmine.SpyObj<UserSettingsService>;
+  let settingsServiceSpy: jasmine.SpyObj<SettingsService>;
 
   beforeEach(async(() => {
-    const router = jasmine.createSpyObj('Router', ['navigate']);
-    routerSpy = router.navigate.and.returnValue(true);
+    const navCtrl = jasmine.createSpyObj('NavController', ['navigateForward']);
+    navCtrlSpy = navCtrl.navigateForward.and.returnValue(true);
     const cas = jasmine.createSpyObj('CasTicketService', ['getST']);
     cas.getST.and.returnValue(asyncData('ticket'));
+    userSettingsServiceSpy = jasmine.createSpyObj('UserSettingsService', ['getMenuUI']);
+    settingsServiceSpy = jasmine.createSpyObj('SettingsService', ['get']);
 
     TestBed.configureTestingModule({
-      declarations: [MorePage],
+      declarations: [MorePage, ByGroupPipe],
       imports: [FuseModule],
       providers: [
         { provide: CasTicketService, useValue: cas },
         { provide: InAppBrowser, useValue: {} },
-        { provide: Router, useValue: router },
+        { provide: NavController, useValue: navCtrl },
+        { provide: Network, useValue: {} },
+        { provide: UserSettingsService, useValue: userSettingsServiceSpy },
+        { provide: SettingsService, useValue: settingsServiceSpy },
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
@@ -44,11 +53,11 @@ describe('MorePage', () => {
 
   it('should route to local url', () => {
     component.openPage('/hello');
-    expect(routerSpy).toHaveBeenCalledWith(['/hello']);
+    expect(navCtrlSpy).toHaveBeenCalledWith(['/hello']);
   });
 
   it('should not route to external url', () => {
     component.openPage('https://external/hello');
-    expect(routerSpy).not.toHaveBeenCalled();
+    expect(navCtrlSpy).not.toHaveBeenCalled();
   });
 });
