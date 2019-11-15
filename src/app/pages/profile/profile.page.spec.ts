ebb16c5dfd79b90b16f6da5b34284227fcf93314
Ivan Tham <pickfire@riseup.net>
2019-11-15 17:40:41 +0800

    test(profile): fix test

--- src/app/pages/profile/profile.page.spec.ts
+++ src/app/pages/profile/profile.page.spec.ts
@@ -1,6 +1,8 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { Router } from '@angular/router';
 
+import { SettingsService, WsApiService } from '../../services';
 import { ProfilePage } from './profile.page';
 
 describe('ProfilePage', () => {
@@ -9,7 +11,12 @@ describe('ProfilePage', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProfilePage ],
+      declarations: [ProfilePage],
+      providers: [
+        { provide: Router, useValue: { url: '/' } },
+        { provide: SettingsService, useValue: {} },
+        { provide: WsApiService, useValue: {} },
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
     .compileComponents();
