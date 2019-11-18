1e699c7cb6b161b7a4e7eb9468013584e2d2c07f
Ivan Tham <pickfire@riseup.net>
2019-11-18 14:02:06 +0800

    test(change-password): fix test

--- src/app/pages/settings/change-password/change-password.page.spec.ts
+++ src/app/pages/settings/change-password/change-password.page.spec.ts
@@ -1,27 +1,43 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { IonicModule } from '@ionic/angular';
+import { NEVER } from 'rxjs';
 
+import {
+  CasTicketService, ChangePasswordService, SettingsService, WsApiService
+} from '../../../services';
 import { ChangePasswordPage } from './change-password.page';
 
-describe('ChangePasswordStudentPage', () => {
+describe('ChangePasswordPage', () => {
   let component: ChangePasswordPage;
   let fixture: ComponentFixture<ChangePasswordPage>;
+  let settingsSpy: jasmine.SpyObj<SettingsService>;
+  let wsSpy: jasmine.SpyObj<WsApiService>;
 
   beforeEach(async(() => {
+    settingsSpy = jasmine.createSpyObj('SettingsService', ['get']);
+    wsSpy = jasmine.createSpyObj('WsApiService', ['get']);
+
     TestBed.configureTestingModule({
       declarations: [ChangePasswordPage],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    })
-      .compileComponents();
+      providers: [
+        { provide: ChangePasswordService, useValue: {} },
+        { provide: CasTicketService, useValue: {} },
+        { provide: SettingsService, useValue: settingsSpy },
+        { provide: WsApiService, useValue: wsSpy },
+      ],
+      imports: [FormsModule, ReactiveFormsModule, RouterTestingModule, IonicModule],
+    }).compileComponents();
   }));
 
-  beforeEach(() => {
+  it('should create', () => {
+    wsSpy.get.and.returnValue(NEVER);
+
     fixture = TestBed.createComponent(ChangePasswordPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
 
-  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
