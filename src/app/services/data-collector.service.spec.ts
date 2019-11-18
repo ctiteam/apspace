81e4da4fa3b4cec504261d5e1ebdf3df578902de
Ivan Tham <pickfire@riseup.net>
2019-11-18 17:24:13 +0800

    test(data-collector): add test

---
+++ src/app/services/data-collector.service.spec.ts
@@ -0,0 +1,25 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Device } from '@ionic-native/device/ngx';
+
+import { DataCollectorService } from './data-collector.service';
+import { VersionService } from './version.service';
+import { WsApiService } from './ws-api.service';
+
+describe('DataCollectorService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: Device, useValue: {} },
+        { provide: VersionService, useValue: {} },
+        { provide: WsApiService, useValue: {} },
+      ],
+      imports: [RouterTestingModule]
+    });
+  });
+
+  it('should be created', () => {
+    const service: DataCollectorService = TestBed.get(DataCollectorService);
+    expect(service).toBeTruthy();
+  });
+});
