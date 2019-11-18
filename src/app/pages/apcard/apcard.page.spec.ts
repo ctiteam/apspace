e66b185e6f12ec922b3018bde87200c0b3bf9151
Ivan Tham <pickfire@riseup.net>
2019-11-18 15:04:32 +0800

    test(apcard): fix test

--- src/app/pages/apcard/apcard.page.spec.ts
+++ src/app/pages/apcard/apcard.page.spec.ts
@@ -1,5 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { Router } from '@angular/router';
 
 import { WsApiService } from 'src/app/services';
 import { ApcardPage } from './apcard.page';
@@ -9,11 +10,12 @@ describe('ApcardPage', () => {
   let fixture: ComponentFixture<ApcardPage>;
 
   beforeEach(async(() => {
-    const ws = jasmine.createSpyObj('WsApiService', []);
+    const ws = jasmine.createSpyObj('WsApiService', ['get']);
 
     TestBed.configureTestingModule({
       declarations: [ApcardPage],
       providers: [
+        { provide: Router, useValue: { url: '/' } },
         { provide: WsApiService, useValue: ws }
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
