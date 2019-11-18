9d92fa6ed14f39e57441105dec1fe7f6a08a524e
Ivan Tham <pickfire@riseup.net>
2019-11-18 14:50:45 +0800

    test(login): fix test

--- src/app/pages/login/login.page.spec.ts
+++ src/app/pages/login/login.page.spec.ts
@@ -1,33 +1,45 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
-
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { FCM } from '@ionic-native/fcm/ngx';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 import { Network } from '@ionic-native/network/ngx';
+import { ModalController } from '@ionic/angular';
+import { of } from 'rxjs';
 
-
-import { CasTicketService, SettingsService, WsApiService } from '../../services';
+import {
+  CasTicketService, DataCollectorService, NotificationService, SettingsService,
+  UserSettingsService,
+  WsApiService
+} from '../../services';
 import { LoginPage } from './login.page';
 
 describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
-  let getSpy: jasmine.Spy;
+  let casSpy: jasmine.SpyObj<CasTicketService>;
+  let snapshotStub: Partial<ActivatedRouteSnapshot>;
 
   beforeEach(async(() => {
-    const settingsService = jasmine.createSpyObj('SettingsService', ['get']);
-    getSpy = settingsService.get.and.returnValue(Promise.resolve(null));
+    casSpy = jasmine.createSpyObj('SettingsService', ['getTGT']);
 
     TestBed.configureTestingModule({
       imports: [FormsModule],
       declarations: [LoginPage],
       providers: [
-        { provide: CasTicketService, useValue: {} },
-        { provide: WsApiService, useValue: {} },
+        { provide: ActivatedRoute, useValue: { snapshot: snapshotStub } },
+        { provide: CasTicketService, useValue: casSpy },
+        { provide: DataCollectorService, useValue: {} },
+        { provide: FCM, useValue: {} },
+        { provide: InAppBrowser, useValue: {} },
+        { provide: ModalController, useValue: {} },
         { provide: Network, useValue: {} },
+        { provide: NotificationService, useValue: {} },
         { provide: Router, useValue: {} },
-        // { provide: Storage, useValue: storage },
-        { provide: SettingsService, useValue: settingsService },
+        { provide: SettingsService, useValue: {} },
+        { provide: UserSettingsService, useValue: {} },
+        { provide: WsApiService, useValue: {} },
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
@@ -35,6 +47,7 @@ describe('LoginPage', () => {
   }));
 
   beforeEach(() => {
+    snapshotStub = { queryParams: {} };
     fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -45,7 +58,12 @@ describe('LoginPage', () => {
   });
 
   it('should retrieve tgt', () => {
-    // TODO: add click
-    expect(getSpy).toHaveBeenCalledWith(['tgt']);
+    casSpy.getTGT.and.returnValue(of('tgt'));
+
+    component.apkey = 'TP004112';
+    component.password = 'hu99uh1';
+    component.login();
+
+    expect(casSpy.getTGT).toHaveBeenCalledTimes(1);
   });
 });
