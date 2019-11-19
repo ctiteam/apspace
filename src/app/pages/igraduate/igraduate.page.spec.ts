503ec812a13c9553fae7d3391f4706a3c77c093a
Ivan Tham <pickfire@riseup.net>
2019-11-19 16:58:52 +0800

    test(igraduate): fix test

--- src/app/pages/igraduate/igraduate.page.spec.ts
+++ src/app/pages/igraduate/igraduate.page.spec.ts
@@ -1,18 +1,28 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { ModalController } from '@ionic/angular';
 
+import { WsApiService } from '../../services';
+import { FilterPipe } from './filter.pipe';
 import { IgraduatePage } from './igraduate.page';
+import { StudentDetailsModalPage } from './student-details-modal';
 
 describe('IgraduatePage', () => {
   let component: IgraduatePage;
   let fixture: ComponentFixture<IgraduatePage>;
+  let wsSpy: jasmine.SpyObj<WsApiService>;
 
   beforeEach(async(() => {
+    wsSpy = jasmine.createSpyObj('WsApiService', ['get']);
+
     TestBed.configureTestingModule({
-      declarations: [ IgraduatePage ],
+      declarations: [IgraduatePage, FilterPipe, StudentDetailsModalPage],
+      providers: [
+        { provide: ModalController, useValue: {} },
+        { provide: WsApiService, useValue: wsSpy },
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    })
-    .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
