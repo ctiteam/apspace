eaa070142c25a46ff6c7f5ba7562cd9364f91bf9
Mohamad Al Ghayeb <mohamad.alghayeb@gmail.com>
2019-11-26 14:29:56 +0800

    fix(shared-pipes): fixed import

--- src/app/shared/shared-pipes.module.ts
+++ src/app/shared/shared-pipes.module.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { FusePipe } from '././fuse/fuse.pipe';
-import { ReversePipe } from '././reverse/reverse.pipe';
+import { FusePipe } from './fuse/fuse.pipe';
+import { ReversePipe } from './reverse/reverse.pipe';
 
 @NgModule({
     declarations: [FusePipe, ReversePipe],
