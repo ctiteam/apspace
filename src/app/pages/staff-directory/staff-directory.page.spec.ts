8b2b93894848a82b84bb041a1a19e4ae128ff96f
Ivan Tham <pickfire@riseup.net>
2019-11-28 22:31:46 +0800

    test(staff-directory): fix test

--- src/app/pages/staff-directory/staff-directory.page.spec.ts
+++ src/app/pages/staff-directory/staff-directory.page.spec.ts
@@ -2,7 +2,7 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 
 import { WsApiService } from '../../services';
-import { FuseModule } from '../../shared/fuse/fuse.module';
+import { FusePipe } from '../../shared/fuse/fuse.pipe';
 import { DepartmentPipe } from './department.pipe';
 import { StaffDirectoryPage } from './staff-directory.page';
 
@@ -12,11 +12,10 @@ describe('StaffDirectoryPage', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [DepartmentPipe, StaffDirectoryPage],
+      declarations: [DepartmentPipe, StaffDirectoryPage, FusePipe],
       providers: [
         { provide: WsApiService, useValue: {} },
       ],
-      imports: [FuseModule],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
