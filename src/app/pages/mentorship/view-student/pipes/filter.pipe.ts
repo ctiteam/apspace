dcbbaf964e775a5c3c20c16dafa75ad394a11a8e
InspectorGadget <igadget28@gmail.com>
2019-12-17 10:15:30 +0800

    fix(mentorship): fix the advanced filters

--- src/app/pages/mentorship/view-student/pipes/filter.pipe.ts
+++ src/app/pages/mentorship/view-student/pipes/filter.pipe.ts
@@ -4,9 +4,9 @@ import { Subcourse } from 'src/app/interfaces/mentorship';
 const filterMap = {
   'low-attendance': (item: Subcourse) => item.TOTAL_ATTEND_PERCENT < 80,
   // tslint:disable-next-line: object-literal-key-quotes
-  'failed': (item: Subcourse) => item.GRADE_POINT < '2.00',
+  'failed': (item: Subcourse) => +item.GRADE_POINT < 2.00 && item.GRADE_POINT !== null,
   'full-attendance': (item: Subcourse) => item.TOTAL_ATTEND_PERCENT === 100,
-  'full-cgpa': (item: Subcourse) => item.GRADE_POINT === '4.00'
+  'full-cgpa': (item: Subcourse) => +item.GRADE_POINT === 4.00
 };
 
 @Pipe({
@@ -17,10 +17,9 @@ export class FilterPipe implements PipeTransform {
   transform(result: Subcourse[] | null, shownFilters: string[]): Subcourse[] {
     if (shownFilters && shownFilters.length > 0) {
       return (result || []).filter((item: Subcourse) => {
-        return shownFilters.some(
+        return shownFilters.every(
           label =>
-            filterMap[label.toLowerCase().trim()] &&
-            filterMap[label.toLowerCase().trim()](item)
+            filterMap[label] && filterMap[label](item)
         );
       });
     }
