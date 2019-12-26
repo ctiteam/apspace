b0ca18dd00ca1a044ac0b61f1b6d96d45bb78033
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2019-12-26 17:12:07 +0800

    feat(beapu): added student name

--- src/app/pages/filing-report/filing-report.page.ts
+++ src/app/pages/filing-report/filing-report.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController, LoadingController, Platform, ToastController } from '@ionic/angular';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { WsApiService } from 'src/app/services';
 
 @Component({
@@ -9,7 +9,7 @@ import { WsApiService } from 'src/app/services';
   styleUrls: ['./filing-report.page.scss'],
 })
 export class FilingReportPage implements OnInit {
-  studentPhoto$: Observable<any[]>; // to be changed
+  studentDetails$: Observable<any[]>; // to be changed
   readPolicyCheckbox = false;
   loading: HTMLIonLoadingElement;
   stagingUrl = 'http://forms.sites-staging.apiit.edu.my/wp-json/gf/v2';
@@ -97,25 +97,22 @@ export class FilingReportPage implements OnInit {
     // for Demo. It will be removed after the backend created
     if (this.currentStepNumber === 1 && (this.studentId === 'TP037354' || this.studentId === 'TP055641')) {
       this.show = 'records';
+
+      this.studentDetails$ = this.ws.post<any[]>('/student/image', {
+        url: 'https://u1cd2ltoq6.execute-api.ap-southeast-1.amazonaws.com/dev',
+        body: {
+          id: [this.studentId]
+        }
+      });
     } else if (this.currentStepNumber === 1) {
       this.show = 'empty';
     }
-  }
-
-  showImage(studentID: string) {
-    this.studentPhoto$ = this.ws.post<any[]>('/student/image', {
-      url: 'https://u1cd2ltoq6.execute-api.ap-southeast-1.amazonaws.com/dev',
-      body: {
-        id: [studentID]
-      }
-    });
   }
 
   cancel() {
     this.studentId = 'TP';
     this.currentStepNumber = 0;
     this.description = '';
-    this.studentPhoto$ = of(); // empty the image observable
   }
 
   submitReport() {
