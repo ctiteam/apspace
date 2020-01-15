bd9370613a524bc2857b1999513e3c29c942bba5
we.yuan <we.yuan@staffemail.apu.edu.my>
2020-01-15 17:17:43 +0800

    chore(my-consultations): change function in template to pipe

---
+++ src/app/pages/iconsult/staff/my-consultations/validate-checkbox.pipe.ts
@@ -0,0 +1,15 @@
+import { DatePipe } from '@angular/common';
+import { Pipe, PipeTransform } from '@angular/core';
+import * as moment from 'moment';
+import { ConsultationSlot } from 'src/app/interfaces';
+
+@Pipe({ name: 'validateCheckbox' })
+export class ValidateCheckboxPipe implements PipeTransform {
+  constructor(private datePipe: DatePipe) {}
+
+  transform(slot: ConsultationSlot) {
+    console.log(slot);
+    return new Date(this.datePipe.transform(slot.start_time, 'medium', '+0800'))
+      > moment(new Date()).add(24, 'hours').toDate();
+  }
+}
