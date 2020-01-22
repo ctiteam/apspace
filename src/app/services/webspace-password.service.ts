0471c34919b4e6170cb9525a247f4412826085be
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2020-01-22 21:45:26 +0800

    fix(webspace-password.service): send form data via httpClient

--- src/app/services/webspace-password.service.ts
+++ src/app/services/webspace-password.service.ts
@@ -1,20 +1,43 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { WsApiService } from './ws-api.service';
+import { switchMap } from 'rxjs/operators';
+import { CasTicketService } from './cas-ticket.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebspacePasswordService {
+
   url = 'https://api.apiit.edu.my/webspace-id';
 
-  constructor(private ws: WsApiService) { }
+  constructor(private http: HttpClient, private cas: CasTicketService) { }
 
-  changePassword(newPassword): Observable<any> {
-    return this.ws.post<any>(`/change`, { body: newPassword, url: this.url });
+  changePassword(body: { current: string, new: string }): Observable<any> {
+    const formBody = new HttpParams().set('new', body.new).set('current', body.current);
+
+    return this.cas.getST(`${this.url}/change`).pipe(
+      switchMap(ticket => {
+        return this.http.post(
+          `${this.url}/change?ticket=${ticket}`,
+          formBody.toString(),
+          { headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded') }
+        );
+      })
+    );
   }
 
-  resetPassword(requestReset): Observable<any> {
-    return this.ws.post<any>(`/reset`, { body: requestReset, url: this.url });
+  resetPassword(passportNumber: string): Observable<any> {
+    const formBody = new HttpParams().set('passport', passportNumber);
+
+    return this.cas.getST(`${this.url}/change`).pipe(
+      switchMap(ticket => {
+        return this.http.post(
+          `${this.url}/change?ticket=${ticket}`,
+          formBody.toString(),
+          { headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded') }
+        );
+      })
+    );
   }
 }
