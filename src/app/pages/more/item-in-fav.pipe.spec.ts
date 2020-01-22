d1db87dceda35c9ff40968aea66e23a15a44e01b
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2020-01-22 21:20:03 +0800

    feat: added pipe to check if item is a favorite item

---
+++ src/app/pages/more/item-in-fav.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ItemInFavPipe } from './item-in-fav.pipe';
+
+describe('ItemInFavPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ItemInFavPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
