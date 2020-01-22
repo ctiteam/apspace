bfb5cf53eb67b9c112188fdd1c3b37bb6be21727
InspectorGadget™ <igadget28@gmail.com>
2020-01-23 00:25:35 +0800

    chore: borrowing student dashboard

--- src/app/pages/student-dashboard/student-dashboard.page.ts
+++ src/app/pages/student-dashboard/student-dashboard.page.ts
@@ -333,7 +333,11 @@ export class StudentDashboardPage implements OnInit, OnDestroy, AfterViewInit {
       component: ShakespearModalPage,
       cssClass: 'controlled-modal',
       componentProps: {
+<<<<<<< HEAD
         imagePath: 'https://i.imgur.com/CUi8Na8.jpg'
+=======
+        imagePath: 'https://i.imgur.com/GGeAbyD.jpg'
+>>>>>>> chore: borrowing student dashboard
       }
     });
 
