96a7f1710f50897818075225ca44fc32ee22f475
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2020-01-22 21:20:03 +0800

    feat: added pipe to check if item is a favorite item

---
+++ src/app/pages/more/item-in-fav.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { MenuItem } from './menu.interface';
+
+@Pipe({
+  name: 'itemInFav'
+})
+export class ItemInFavPipe implements PipeTransform {
+
+  transform(favMenuItems: MenuItem[], menuItem: MenuItem): any {
+    return favMenuItems.some(item => item.title === menuItem.title);
+  }
+
+}
