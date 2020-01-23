37fdf660ee0e28030cd3cbd4d2550dc6f71b60b9
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2020-01-23 16:57:04 +0800

    feat(more): filter fav menu when user searches

---
+++ src/app/pages/more/by-item.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ByItemPipe } from './by-item.pipe';
+
+describe('ByItemPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ByItemPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
