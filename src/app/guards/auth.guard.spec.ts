ca77900f2141c7ffbc862cc14780ed9068184798
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2020-02-05 09:24:04 +0800

    test(auth-guard): test user redirection to logout

--- src/app/guards/auth.guard.spec.ts
+++ src/app/guards/auth.guard.spec.ts
@@ -24,56 +24,76 @@ describe('AuthGuard', () => {
     });
   });
 
-  it('should redirect if authenticated', inject([Router, AuthGuard], async (router, guard) => {
-    spyOn(router, 'createUrlTree').and.callThrough();
-    settingsSpy.ready.and.returnValue(Promise.resolve());
-    casTicketServiceSpy.isAuthenticated.and.returnValue(Promise.resolve(true));
-    const route = { url: new UrlSegment('tabs', {}), data: {} };
-    const state = { url: route.url.toString() };
+  // canActivate and canActivateChild behaves the same
+  ['canActivate', 'canActivateChild'].forEach(test => describe(test, () => {
+    it('should redirect if authenticated', inject([Router, AuthGuard], async (router, guard) => {
+      spyOn(router, 'createUrlTree').and.callThrough();
+      settingsSpy.ready.and.returnValue(Promise.resolve());
+      casTicketServiceSpy.isAuthenticated.and.returnValue(Promise.resolve(true));
+      const route = { url: new UrlSegment('tabs', {}), data: {} };
+      const state = { url: route.url.toString() };
 
-    expect(await guard.canActivate(route, state)).toEqual(true);
-    expect(router.createUrlTree).not.toHaveBeenCalled();
-  }));
+      expect(await guard[test](route, state)).toEqual(true);
+      expect(settingsSpy.get).not.toHaveBeenCalled();
+      expect(router.createUrlTree).not.toHaveBeenCalled();
+    }));
 
-  it('should redirect if not authenticated', inject([Router, AuthGuard], async (router, guard) => {
-    spyOn(router, 'createUrlTree').and.callThrough();
-    settingsSpy.ready.and.returnValue(Promise.resolve());
-    casTicketServiceSpy.isAuthenticated.and.returnValue(Promise.resolve(false));
-    const route = { url: new UrlSegment('tabs', {}), data: {} };
-    const state = { url: route.url.toString() };
+    it('should redirect if not authenticated', inject([Router, AuthGuard], async (router, guard) => {
+      spyOn(router, 'createUrlTree').and.callThrough();
+      settingsSpy.ready.and.returnValue(Promise.resolve());
+      casTicketServiceSpy.isAuthenticated.and.returnValue(Promise.resolve(false));
+      const route = { url: new UrlSegment('tabs', {}), data: {} };
+      const state = { url: route.url.toString() };
 
-    const expected = router.parseUrl('/login');
-    expected.fragment = undefined;
-    expect(await guard.canActivate(route, state)).toEqual(expected);
-    expect(router.createUrlTree).toHaveBeenCalledTimes(1);
-  }));
+      const expected = router.parseUrl('/login');
+      expected.fragment = undefined;
+      expect(await guard[test](route, state)).toEqual(expected);
+      expect(router.createUrlTree).toHaveBeenCalledTimes(1);
+    }));
 
-  it('should redirect with state if not authenticated', inject([Router, AuthGuard], async (router, guard) => {
-    spyOn(router, 'createUrlTree').and.callThrough();
-    settingsSpy.ready.and.returnValue(Promise.resolve());
-    casTicketServiceSpy.isAuthenticated.and.returnValue(Promise.resolve(false));
-    const route = { url: new UrlSegment('hello', {}), data: {} };
-    const state = { url: route.url.toString() };
+    it('should redirect with state if not authenticated', inject([Router, AuthGuard], async (router, guard) => {
+      spyOn(router, 'createUrlTree').and.callThrough();
+      settingsSpy.ready.and.returnValue(Promise.resolve());
+      casTicketServiceSpy.isAuthenticated.and.returnValue(Promise.resolve(false));
+      const route = { url: new UrlSegment('hello', {}), data: {} };
+      const state = { url: route.url.toString() };
 
-    const expected = router.parseUrl('/login?redirect=hello');
-    expected.fragment = undefined;
-    expect(await guard.canActivate(route, state)).toEqual(expected);
-    expect(router.createUrlTree).toHaveBeenCalledTimes(1);
-  }));
+      const expected = router.parseUrl('/login?redirect=hello');
+      expected.fragment = undefined;
+      expect(await guard[test](route, state)).toEqual(expected);
+      expect(router.createUrlTree).toHaveBeenCalledTimes(1);
+    }));
 
-  it('should redirect if not authorized', inject([Router, AuthGuard], async (router, guard) => {
-    spyOn(router, 'createUrlTree').and.callThrough();
-    settingsSpy.ready.and.returnValue(Promise.resolve());
-    settingsSpy.get.and.returnValue(Role.Student);
-    casTicketServiceSpy.isAuthenticated.and.returnValue(Promise.resolve(true));
-    const route = { url: new UrlSegment('tabs', {}), data: { role: Role.Lecturer } };
-    const state = { url: route.url.toString() };
+    it('should redirect if no settings', inject([Router, AuthGuard], async (router, guard) => {
+      spyOn(router, 'createUrlTree').and.callThrough();
+      settingsSpy.ready.and.returnValue(Promise.resolve());
+      settingsSpy.get.and.returnValue(undefined);
+      casTicketServiceSpy.isAuthenticated.and.returnValue(Promise.resolve(true));
+      const route = { url: new UrlSegment('tabs', {}), data: { role: Role.Lecturer } };
+      const state = { url: route.url.toString() };
 
-    const expected = router.parseUrl('/unauthorized');
-    expected.fragment = undefined;
-    expect(await guard.canActivate(route, state)).toEqual(expected);
-    expect(router.createUrlTree).toHaveBeenCalledTimes(1);
-    expect(settingsSpy.get).toHaveBeenCalledTimes(1);
-    expect(settingsSpy.get).toHaveBeenCalledWith('role');
+      const expected = router.parseUrl('/logout');
+      expected.fragment = undefined;
+      expect(await guard[test](route, state)).toEqual(expected);
+      expect(router.createUrlTree).toHaveBeenCalledTimes(1);
+      expect(settingsSpy.get).toHaveBeenCalledTimes(1);
+      expect(settingsSpy.get).toHaveBeenCalledWith('role');
+    }));
+
+    it('should redirect if not authorized', inject([Router, AuthGuard], async (router, guard) => {
+      spyOn(router, 'createUrlTree').and.callThrough();
+      settingsSpy.ready.and.returnValue(Promise.resolve());
+      settingsSpy.get.and.returnValue(Role.Student);
+      casTicketServiceSpy.isAuthenticated.and.returnValue(Promise.resolve(true));
+      const route = { url: new UrlSegment('tabs', {}), data: { role: Role.Lecturer } };
+      const state = { url: route.url.toString() };
+
+      const expected = router.parseUrl('/unauthorized');
+      expected.fragment = undefined;
+      expect(await guard[test](route, state)).toEqual(expected);
+      expect(router.createUrlTree).toHaveBeenCalledTimes(1);
+      expect(settingsSpy.get).toHaveBeenCalledTimes(1);
+      expect(settingsSpy.get).toHaveBeenCalledWith('role');
+    }));
   }));
 });
