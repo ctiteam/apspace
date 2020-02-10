e9e5a352a9553c0ed5ede89d1720e0af2918e217
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2020-02-10 16:30:28 +0800

    feat(print-transactions): finished styling the report

--- src/app/pages/apcard/print-transactions-modal/print-transactions-modal.ts
+++ src/app/pages/apcard/print-transactions-modal/print-transactions-modal.ts
@@ -16,7 +16,6 @@ export class PrintTransactionsModalPage implements OnInit {
   now = new Date();
   max = moment(this.now).format('YYYY-MM-DD').toString();
   transactionTypes = ['All', 'Credit', 'Debit'];
-
   transactionTypeModel = '';
   yearMonthModel = '';
   pdfObj = null; // used to generate report
@@ -43,10 +42,11 @@ export class PrintTransactionsModalPage implements OnInit {
   generatePdf() {
     const yearMonthDate = new Date(this.yearMonthModel);
     let numberOfItems = 0, totalSpent = 0, totalTopup = 0;
-    // let firstItem: any;
-    // let lastItem: any;
+    // let firstTransaction: Apcard;
+    // let lastTransaction: Apcard;
     this.tableBody = [ // empty the list whenever there is an update
       [
+        { text: '#', bold: true, style: 'tableHeader' },
         { text: 'Date', bold: true, style: 'tableHeader' },
         { text: 'Time', bold: true, style: 'tableHeader', alignment: 'center' },
         { text: 'Item', bold: true, style: 'tableHeader' },
@@ -56,8 +56,9 @@ export class PrintTransactionsModalPage implements OnInit {
     this.transactions.filter(transaction => {
       const spendDateObj = new Date(transaction.SpendDate);
       return (spendDateObj.getMonth() === yearMonthDate.getMonth() && spendDateObj.getFullYear() === yearMonthDate.getFullYear());
-    }).forEach((transaction, index: number) => {
+    }).reverse().forEach((transaction, index: number) => {
       const studentData = [
+        { text: index + 1, alignment: 'center', style: transaction.ItemName === 'Top Up' ? 'greenText' : 'tableCell' },
         { text: moment(new Date(transaction.SpendDate)).format('YYYY-MM-DD'), style: transaction.ItemName === 'Top Up' ? 'greenText' : 'tableCell' },
         { text: transaction.SpendTime, alignment: 'center', style: transaction.ItemName === 'Top Up' ? 'greenText' : 'tableCell' },
         { text: transaction.ItemName, style: transaction.ItemName === 'Top Up' ? 'greenText' : 'tableCell' },
@@ -66,12 +67,12 @@ export class PrintTransactionsModalPage implements OnInit {
       numberOfItems += 1;
       totalSpent += transaction.ItemName.toLowerCase() !== 'Top Up'.toLowerCase() ? Math.abs(transaction.SpendVal) : 0;
       totalTopup += transaction.ItemName.toLowerCase() === 'Top Up'.toLowerCase() ? Math.abs(transaction.SpendVal) : 0;
-      if (index === 0) {
-        console.log('first transaction: ', transaction);
-      }
-      if (index === this.transactions.length - 1) {
-        console.log('last transaction: ', transaction);
-      }
+      // if (index === 0) { // first transaction
+      //   firstTransaction = transaction;
+      // }
+      // if (index === array.length - 1) { // last transaction
+      //   lastTransaction = transaction;
+      // }
       this.tableBody.push(studentData);
     });
 
@@ -80,53 +81,18 @@ export class PrintTransactionsModalPage implements OnInit {
       return false;
     }
 
-
-    this.summaryBody = [
-      [
-        { text: 'Student ID', bold: true, margin: [5, 5, 5, 5] },
-        { text: this.transactions[0].SNO, alignment: 'right', margin: [5, 5, 5, 5] }
-      ],
-      [
-        { text: 'Date Range', bold: true, margin: [5, 5, 5, 5] },
-        {
-          text: `${yearMonthDate.getFullYear()}/${yearMonthDate.getMonth() + 1}/1 - ${yearMonthDate.getFullYear()}/${yearMonthDate.getMonth() + 1}/${new Date(yearMonthDate.getFullYear(), yearMonthDate.getMonth() + 1, 0).getDate()}`,
-          alignment: 'right', margin: [5, 5, 5, 5]
-        }
-      ],
-      [
-        { text: 'Number of items', bold: true, margin: [5, 5, 5, 5] },
-        { text: (numberOfItems === 1 ? numberOfItems + ' Item' : numberOfItems + ' Items'), alignment: 'right', margin: [5, 5, 5, 5] }
-      ],
-      [
-        { text: 'Total spent in the month:', bold: true, margin: [5, 5, 5, 5] },
-        { text: ('RM' + totalSpent.toFixed(2)), alignment: 'right', margin: [5, 5, 5, 5] }
-      ],
-      [
-        { text: 'Total topup in the month:', bold: true, margin: [5, 5, 5, 5] },
-        { text: ('RM' + totalTopup.toFixed(2)), alignment: 'right', margin: [5, 5, 5, 5] }
-      ],
-      [
-        { text: 'Balance At the begining of the month:', bold: true, margin: [5, 5, 5, 5] },
-        { text: 'test', alignment: 'right', margin: [5, 5, 5, 5] }
-      ],
-      [
-        { text: 'Balance At the end of the month:', bold: true, margin: [5, 5, 5, 5] },
-        { text: 'test', alignment: 'right', margin: [5, 5, 5, 5] }
-      ]
-    ];
-
     const pdfTitle = `apcard_transactions_for_${yearMonthDate.getFullYear()}_${yearMonthDate.getMonth() + 1}`;
 
     const docDefinition = {
       info: {
         title: pdfTitle,
         author: 'APSpace_Reports',
-        subject: 'APCard Transactions Report',
+        subject: `APCard @ ${this.now.toString()}`,
         keywords: 'APCard APSpace Reports',
         creator: 'APSpace_Reports',
         producer: 'APSpace_Reports',
         creationDate: this.now.toDateString(),
-        modDate: this.now.toDateString(),
+        modDate: this.now.toDateString()
       },
       content: [
         {
@@ -137,7 +103,14 @@ export class PrintTransactionsModalPage implements OnInit {
         },
 
         {
-          text: 'APCard Transactions Report',
+          text: 'APCard Transactions Monthly Report',
+          style: 'subheader',
+          alignment: 'center',
+          margin: [0, 10, 0, 0]
+        },
+
+        {
+          text: `Report for ${moment(yearMonthDate.getMonth() + 1, 'M').format('MMMM').toString()}, ${yearMonthDate.getFullYear()}`,
           style: 'subheader',
           alignment: 'center',
           margin: [0, 10, 0, 0]
@@ -147,12 +120,39 @@ export class PrintTransactionsModalPage implements OnInit {
 
         {
           layout: 'noBorders',
-          fillColor: '#49b571',
-          color: '#FFFFFF',
+          fillColor: '#dbdbdb',
+          color: '#000000',
           table: {
             headerRows: 1,
             widths: ['70%', '30%'],
-            body: this.summaryBody,
+            body: [
+              [
+                { text: 'Student ID', bold: true, margin: [5, 5, 5, 5] },
+                { text: this.transactions[0].SNO, alignment: 'right', margin: [5, 5, 5, 5] }
+              ],
+              [
+                { text: 'Number of items', bold: true, margin: [5, 5, 5, 5] },
+
+                // tslint:disable-next-line: max-line-length
+                { text: (numberOfItems === 1 ? numberOfItems + ' Item' : numberOfItems + ' Items'), alignment: 'right', margin: [5, 5, 5, 5] }
+              ],
+              [
+                { text: 'Total debit in the month:', bold: true, margin: [5, 5, 5, 5] },
+                { text: ('RM' + totalSpent.toFixed(2)), alignment: 'right', margin: [5, 5, 5, 5], color: '#e54d42' }
+              ],
+              [
+                { text: 'Total credit in the month:', bold: true, margin: [5, 5, 5, 5] },
+                { text: ('RM' + totalTopup.toFixed(2)), alignment: 'right', margin: [5, 5, 5, 5], color: '#346948' }
+              ],
+              // [
+              //   { text: 'Balance At the begining of the month:', bold: true, margin: [5, 5, 5, 5] },
+              //   { text: ('RM' + firstTransaction.Balance.toFixed(2)), alignment: 'right', margin: [5, 5, 5, 5] }
+              // ],
+              // [
+              //   { text: 'Balance At the end of the month:', bold: true, margin: [5, 5, 5, 5] },
+              //   { text: ('RM' + lastTransaction.Balance.toFixed(2)), alignment: 'right', margin: [5, 5, 5, 5] }
+              // ]
+            ],
             pageBreak: 'after'
           }
         },
@@ -163,7 +163,7 @@ export class PrintTransactionsModalPage implements OnInit {
         {
           table: {
             headerRows: 1,
-            widths: ['auto', 'auto', '*', 'auto'],
+            widths: ['auto', 'auto', 'auto', '*', 'auto'],
             body: this.tableBody,
             pageBreak: 'after'
           }
@@ -232,12 +232,16 @@ export class PrintTransactionsModalPage implements OnInit {
           width: '50%',
         },
         greenText: {
-          color: '#49b571',
+          color: '#346948',
+          fillColor: '#e4f7e9',
           margin: [5, 5, 5, 5]
         },
         redText: {
           color: '#e54d42',
           margin: [5, 5, 5, 5]
+        },
+        blueText: {
+          color: '#3a99d9'
         }
       }
     };
