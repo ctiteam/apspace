38bfa07dee26eb7a9f3fba56f2fbc5446b9a570e
we.yuan <we.yuan@staffemail.apu.edu.my>
2020-02-11 17:57:41 +0800

    feat(add-exam-schedule): able to select date on range in calendar

--- src/app/pages/exam-schedule-admin/add-exam-schedule/add-exam-schedule.page.ts
+++ src/app/pages/exam-schedule-admin/add-exam-schedule/add-exam-schedule.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { CalendarComponentOptions } from 'ion2-calendar';
 
 @Component({
   selector: 'app-add-exam-schedule',
@@ -7,6 +8,11 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./add-exam-schedule.page.scss'],
 })
 export class AddExamSchedulePage implements OnInit {
+  dateRange: { from: string; to: string; };
+  type: 'string'; // 'string' | 'js-date' | 'moment' | 'time' | 'object'
+  optionsRange: CalendarComponentOptions = {
+    pickMode: 'range'
+  };
 
   constructor(public modalCtrl: ModalController) { }
 
