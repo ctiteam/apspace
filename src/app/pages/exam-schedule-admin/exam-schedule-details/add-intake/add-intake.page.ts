57b8e90e672b2e84a3db79a6b4c5969b0df29ae4
we.yuan <we.yuan@staffemail.apu.edu.my>
2020-02-11 10:40:16 +0800

    chore(add-intake): enhance input form

--- src/app/pages/exam-schedule-admin/exam-schedule-details/add-intake/add-intake.page.ts
+++ src/app/pages/exam-schedule-admin/exam-schedule-details/add-intake/add-intake.page.ts
@@ -7,14 +7,146 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./add-intake.page.scss'],
 })
 export class AddIntakePage implements OnInit {
+  searchTerm = '';
+  items: any;
+
+  tests = [
+    {
+      value: '1Lorem Ipsum'
+    },
+    {
+      value: '2Lorem Ipsum'
+    },
+    {
+      value: '3Lorem Ipsum'
+    },
+    {
+      value: '4Lorem Ipsum'
+    },
+    {
+      value: '5Lorem Ipsum'
+    },
+    {
+      value: '6Lorem Ipsum'
+    },
+    {
+      value: 'Lorem 7Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lorem 8Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lorem 9Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lorem Ip10sum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lo11rem Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    },
+    {
+      value: 'Lorem Ipsum'
+    }
+  ];
 
   constructor(public modalCtrl: ModalController) { }
 
   ngOnInit() {
+    this.setFilteredItems();
   }
 
   closeModal() {
     this.modalCtrl.dismiss();
   }
 
+  filterItems(searchTerm) {
+    return this.tests.filter(test => {
+      return test.value.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
+    });
+  }
+
+  setFilteredItems() {
+    this.items = this.filterItems(this.searchTerm);
+  }
 }
