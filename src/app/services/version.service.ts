135d7418e81eb64db42ed88077e3f2f04132e6d7
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2020-02-26 16:37:00 +0800

    chore: bump version to 2.1.2

--- src/app/services/version.service.ts
+++ src/app/services/version.service.ts
@@ -24,7 +24,7 @@ export class VersionService {
     private storage: Storage
   ) { }
 
-  readonly version = '2.1.1';
+  readonly version = '2.1.2';
 
   /** Application version name. */
   get name(): string {
