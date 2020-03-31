4da465362692a3359e8205e06a77ec242fe40e48
we.yuan <we.yuan@staffemail.apu.edu.my>
2020-03-31 11:33:21 +0800

    chore(print-payslip): test native pdf viewer

--- src/app/pages/hr/print-payslip-modal/print-payslip-modal.page.ts
+++ src/app/pages/hr/print-payslip-modal/print-payslip-modal.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { DocumentViewer } from '@ionic-native/document-viewer/ngx';
+import { FileOpener } from '@ionic-native/file-opener/ngx';
+// import { DocumentViewer } from '@ionic-native/document-viewer/ngx';
 import { FileTransfer } from '@ionic-native/file-transfer/ngx';
 import { File } from '@ionic-native/file/ngx';
 import { ModalController, Platform } from '@ionic/angular';
@@ -14,9 +15,10 @@ export class PrintPayslipModalPage implements OnInit {
   constructor(
     public modalCtrl: ModalController,
     private platform: Platform,
-    private document: DocumentViewer,
+    // private document: DocumentViewer,
     private file: File,
-    private transfer: FileTransfer
+    private transfer: FileTransfer,
+    private fileOpener: FileOpener
   ) { }
 
   ngOnInit() {
@@ -25,14 +27,17 @@ export class PrintPayslipModalPage implements OnInit {
   generatePayslipPdf() {
     const path = this.platform.is('ios') ? this.file.documentsDirectory : this.file.dataDirectory;
 
+    console.log(path);
+
     const transfer = this.transfer.create();
 
     transfer.download(
       'https://ztmu4mdu21.execute-api.ap-southeast-1.amazonaws.com/dev/staff/download_payslip/U6942.2020-02-End Month.Payslip.PDF',
       path + 'myfile.pdf'
-    ).then(entry => {
-      const url = entry.toURL();
-      this.document.viewDocument(url, 'application/pdf', {});
+    ).then(_ => {
+      // const url = entry.toURL();
+      // this.document.viewDocument(url, 'application/pdf', {});
+      this.fileOpener.open(this.file.dataDirectory + `myfile.pdf`, 'application/pdf');
     });
   }
 
