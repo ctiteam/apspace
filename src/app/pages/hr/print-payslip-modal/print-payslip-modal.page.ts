f8e3e114af1f8f54157c0bf825d2ed4f4c76a7ca
we.yuan <we.yuan@staffemail.apu.edu.my>
2020-03-31 15:17:51 +0800

    chore(print-payslip): test native view pdf

--- src/app/pages/hr/print-payslip-modal/print-payslip-modal.page.ts
+++ src/app/pages/hr/print-payslip-modal/print-payslip-modal.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import { FileOpener } from '@ionic-native/file-opener/ngx';
-import { DocumentViewer } from '@ionic-native/document-viewer/ngx';
+import { FileOpener } from '@ionic-native/file-opener/ngx';
 import { FileTransfer } from '@ionic-native/file-transfer/ngx';
 import { File } from '@ionic-native/file/ngx';
 import { ModalController, Platform } from '@ionic/angular';
@@ -15,30 +14,29 @@ export class PrintPayslipModalPage implements OnInit {
   constructor(
     public modalCtrl: ModalController,
     private platform: Platform,
-    private document: DocumentViewer,
     private file: File,
     private transfer: FileTransfer,
-    // private fileOpener: FileOpener
+    private fileOpener: FileOpener
   ) { }
 
   ngOnInit() {
   }
 
   generatePayslipPdf() {
-    const path = this.platform.is('ios') ? this.file.documentsDirectory : this.file.dataDirectory;
+    const path = this.file.dataDirectory;
+    const url = 'https://ztmu4mdu21.execute-api.ap-southeast-1.amazonaws.com/dev/staff/download_payslip/';
+    const fileName = 'U6942.2020-02-End Month.Payslip.PDF';
 
     const transfer = this.transfer.create();
 
     transfer.download(
-      'https://ztmu4mdu21.execute-api.ap-southeast-1.amazonaws.com/dev/staff/download_payslip/U6942.2020-02-End Month.Payslip.PDF',
-      path + 'myfile.pdf'
-    ).then(entry => {
-      const url = entry.toURL();
-      this.document.viewDocument(url, 'application/pdf', {});
-      // console.log('this is entry to url ', entry.toURL());
-      // this.fileOpener.open(entry.toURL(), 'application/pdf').then(() => {
-      //   console.log('file is opened');
-      // }).catch(e => console.log('ERROR ERROR ', e));
+      url + fileName,
+      path + fileName
+    ).then((entry) => {
+      console.log('this is entry to url ', entry.toURL());
+      this.fileOpener.open(entry.toURL(), 'application/pdf').then(() => {
+        console.log('file is opened');
+      }).catch(e => console.log('ERROR ERROR ', e));
     }, (error) => console.log('download error ', error));
   }
 
