74269767644386fca28dad3e284d1d10a5f53c4e
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2020-04-02 12:52:41 +0800

    refactor(time-frame): removed console.log

--- src/app/pages/attendix-lecturer/classes-new/time-frame-filter.pipe.ts
+++ src/app/pages/attendix-lecturer/classes-new/time-frame-filter.pipe.ts
@@ -9,7 +9,6 @@ export class TimeFrameFilterPipe implements PipeTransform {
 
   transform(classes: AttendixClass[], timeframe: number): any {
     const today = new Date();
-    console.log(timeframe);
     return classes.sort((a, b) =>  moment(b.DATE, 'YYYY-MM-DD').toDate().getTime() - moment(a.DATE, 'YYYY-MM-DD').toDate().getTime())
             .filter(c => moment(today).diff(moment(c.DATE, 'YYYY-MM-DD').subtract(), 'days') <= timeframe);
   }
