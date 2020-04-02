d5a6c3ecb928de856aeb53a9c2b53f416834cef4
we.yuan <we.yuan@staffemail.apu.edu.my>
2020-04-02 15:36:59 +0800

    chore(print-payslip): generate payslip in browser and mobile

--- src/app/pages/hr/print-payslip-modal/print-payslip-modal.page.ts
+++ src/app/pages/hr/print-payslip-modal/print-payslip-modal.page.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FileOpener } from '@ionic-native/file-opener/ngx';
 import { FileTransfer } from '@ionic-native/file-transfer/ngx';
 import { File } from '@ionic-native/file/ngx';
-import { ModalController, Platform } from '@ionic/angular';
+import { ModalController, Platform, ToastController } from '@ionic/angular';
+import { map, tap} from 'rxjs/operators';
+import { WsApiService } from 'src/app/services';
 
 @Component({
   selector: 'app-print-payslip-modal',
@@ -10,34 +12,57 @@ import { ModalController, Platform } from '@ionic/angular';
   styleUrls: ['./print-payslip-modal.page.scss'],
 })
 export class PrintPayslipModalPage implements OnInit {
+  payslips = [];
+  payslipsUrl = 'https://ztmu4mdu21.execute-api.ap-southeast-1.amazonaws.com';
+  payslipsEndpoint = '/dev/staff/payslips';
+
+  minDate;
+  maxDate;
+
+  selectedDate;
 
   constructor(
     public modalCtrl: ModalController,
+    public toastCtrl: ToastController,
     private platform: Platform,
     private file: File,
     private transfer: FileTransfer,
-    private fileOpener: FileOpener
+    private fileOpener: FileOpener,
+    private ws: WsApiService
   ) { }
 
   ngOnInit() {
+    this.ws.get<any>(this.payslipsEndpoint, { url: this.payslipsUrl }).pipe(
+      map(payslips => payslips.payslips),
+      tap(payslips => this.payslips = payslips),
+      tap(payslips => {
+        const dates = [...payslips.map(payslip => payslip.split('.', 2)[1].replace('-End Month', ''))];
+        this.minDate = dates[0];
+        this.maxDate = dates[dates.length - 1];
+      })
+    ).subscribe();
   }
 
   generatePayslipPdf() {
-    const path = this.file.dataDirectory;
-    const url = 'https://ztmu4mdu21.execute-api.ap-southeast-1.amazonaws.com/dev/staff/download_payslip/';
-    const fileName = 'U6942.2020-02-End Month.Payslip.PDF';
+    const downloadPayslipEndpoint = '/dev/staff/download_payslip/';
+    const filteredSelectedDate = this.selectedDate.substring(0, 7);
+    const fileName = this.payslips.find(element => element.includes(filteredSelectedDate));
 
     const transfer = this.transfer.create();
 
-    transfer.download(
-      url + fileName,
-      path + fileName
-    ).then((entry) => {
-      console.log('this is entry to url ', entry.toURL());
-      this.fileOpener.open(entry.toURL(), 'application/pdf').then(() => {
-        console.log('file is opened');
-      }).catch(e => console.log('ERROR ERROR ', e));
-    }, (error) => console.log('download error ', error));
+    if (this.platform.is('cordova')) {
+      transfer.download(
+        this.payslipsUrl + downloadPayslipEndpoint + fileName,
+        this.file.dataDirectory + fileName
+      ).then((entry) => {
+        console.log('this is entry to url ', entry.toURL());
+        this.fileOpener.open(entry.toURL(), 'application/pdf').then(() => {
+          console.log('file is opened');
+        }).catch(e => console.log('ERROR ERROR ', e));
+      }, (error) => console.log('download error ', error));
+    } else {
+      window.open(this.payslipsUrl + downloadPayslipEndpoint + fileName);
+    }
   }
 
   dismiss() {
