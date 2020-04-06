23ede53c3d248d5e696f7b02b88169e984287733
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2020-04-06 16:48:43 +0800

    refactor: removed unused pipe

--- src/app/pages/attendix-lecturer/classes-new/time-frame-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TimeFrameFilterPipe } from './time-frame-filter.pipe';
-
-describe('TimeFrameFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TimeFrameFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
