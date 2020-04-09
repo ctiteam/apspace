eb60041594e4ae715bcced5af9c1e1f334475c6f
Ivan Tham <pickfire@riseup.net>
2020-04-09 13:18:27 +0800

    perf(attendix-lecturer): use slice

--- src/app/pages/attendix-lecturer/classes/classcodes-filter.pipe.ts
+++ src/app/pages/attendix-lecturer/classes/classcodes-filter.pipe.ts
@@ -8,7 +8,7 @@ import { Classcode } from 'src/app/interfaces';
 export class ClasscodesFilterPipe implements PipeTransform {
 
   transform(classcodes: Classcode[], keyword: string, timeframe: number) {
-    const filteredClasscodes: Classcode[] = JSON.parse(JSON.stringify(classcodes)); // to prevent updating the original object
+    const filteredClasscodes: Classcode[] = classcodes.slice(); // to prevent updating the original object
     if (keyword) { // do this only if the user enters data in search box
       keyword = keyword.toLowerCase().replace(/%/g, '.*')
         .replace(/\(/g, '\\(').replace(/\)/g, '\\)') // escape parenthesis
@@ -17,13 +17,13 @@ export class ClasscodesFilterPipe implements PipeTransform {
     }
     const searchRegExp = new RegExp(keyword, 'i');
     const today = new Date();
-    return filteredClasscodes.filter(classcodeObj => {
-      if (searchRegExp.test(classcodeObj.CLASS_CODE.toLowerCase())) { // filter classcode by keyword (using regex)
-        const classes = classcodeObj.CLASSES
+    return filteredClasscodes.filter(classcode => {
+      if (searchRegExp.test(classcode.CLASS_CODE.toLowerCase())) { // filter classcode by keyword (using regex)
+        const classes = classcode.CLASSES.slice()
           .sort((a, b) => moment(b.DATE, 'YYYY-MM-DD').toDate().getTime() - moment(a.DATE, 'YYYY-MM-DD').toDate().getTime())
           .filter(c => moment(today).diff(moment(c.DATE, 'YYYY-MM-DD').subtract(), 'days') <= timeframe);
         if (classes.length > 0) { // filter classes by time frame (last 7, 30, or 98 days).
-          classcodeObj.CLASSES = classes;
+          classcode.CLASSES = classes;
           return true;
         }
       }
