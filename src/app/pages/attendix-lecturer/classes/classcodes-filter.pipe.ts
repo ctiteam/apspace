42aa3e81d88f3c2b4df80413429a8a4997876d07
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2020-04-10 23:14:53 +0800

    fix(classcodes-filter-pipe): prevent updating original data
    - last changes broke the pipe

--- src/app/pages/attendix-lecturer/classes/classcodes-filter.pipe.ts
+++ src/app/pages/attendix-lecturer/classes/classcodes-filter.pipe.ts
@@ -8,7 +8,8 @@ import { Classcode } from 'src/app/interfaces';
 export class ClasscodesFilterPipe implements PipeTransform {
 
   transform(classcodes: Classcode[], keyword: string, timeframe: number) {
-    const filteredClasscodes: Classcode[] = classcodes.slice(); // to prevent updating the original object
+    console.log(classcodes);
+    const filteredClasscodes: Classcode[] = JSON.parse(JSON.stringify(classcodes)); // to prevent updating the original object
     if (keyword) { // do this only if the user enters data in search box
       keyword = keyword.toLowerCase().replace(/%/g, '.*')
         .replace(/\(/g, '\\(').replace(/\)/g, '\\)') // escape parenthesis
@@ -19,7 +20,7 @@ export class ClasscodesFilterPipe implements PipeTransform {
     const today = new Date();
     return filteredClasscodes.filter(classcode => {
       if (searchRegExp.test(classcode.CLASS_CODE.toLowerCase())) { // filter classcode by keyword (using regex)
-        const classes = classcode.CLASSES.slice()
+        const classes = classcode.CLASSES
           .filter(c => moment(today).diff(moment(c.DATE, 'YYYY-MM-DD').subtract(), 'days') <= timeframe);
         if (classes.length > 0) { // filter classes by time frame (last 7, 30, or 98 days).
           classcode.CLASSES = classes;
