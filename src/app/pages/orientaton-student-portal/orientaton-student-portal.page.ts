0f7db8118d352062da59d320522c0794dcde7f34
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2020-05-08 12:35:42 +0800

    feat(orientation-student-portal): added see profile update button

--- src/app/pages/orientaton-student-portal/orientaton-student-portal.page.ts
+++ src/app/pages/orientaton-student-portal/orientaton-student-portal.page.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { Network } from '@ionic-native/network/ngx';
 import { ModalController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { OrientationStudentsList } from 'src/app/interfaces';
-import { WsApiService } from 'src/app/services';
+import { CasTicketService, WsApiService } from 'src/app/services';
 import { ViewStudentProfileModalPage } from './view-student-profile/view-student-profile-modal';
 
 @Component({
@@ -16,7 +18,10 @@ export class OrientatonStudentPortalPage implements OnInit {
 
   constructor(
     private ws: WsApiService,
-    private modalCtrl: ModalController
+    private modalCtrl: ModalController,
+    private network: Network,
+    private cas: CasTicketService,
+    private iab: InAppBrowser
   ) { }
 
   ngOnInit() {
@@ -24,7 +29,15 @@ export class OrientatonStudentPortalPage implements OnInit {
     this.studentsList$ = this.ws.get<OrientationStudentsList[]>('orientation/student_list', { url: 'https://gv8ap4lfw5.execute-api.ap-southeast-1.amazonaws.com/dev/' });
   }
 
-
+  openForms() {
+    // external pages does not use relative or absolute link
+    const url = 'http://forms.sites.apiit.edu.my/my-inbox/';
+    if (this.network.type !== 'none') {
+      this.cas.getST(url).subscribe(st => {
+        this.iab.create(`${url}?ticket=${st}`, '_system', 'location=true');
+      });
+    }
+  }
 
   async viewProfile(studentID: string) {
     const modal = await this.modalCtrl.create({
