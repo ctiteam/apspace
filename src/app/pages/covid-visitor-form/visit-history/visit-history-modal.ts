4584ed14dd7594ee58b98572319c4d33c0a05a26
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2020-06-28 00:55:30 +0800

    feat(visit-history-modal): used api for the general info

--- src/app/pages/covid-visitor-form/visit-history/visit-history-modal.ts
+++ src/app/pages/covid-visitor-form/visit-history/visit-history-modal.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
 
 import { WsApiService } from 'src/app/services';
+
 @Component({
   selector: 'page-visit-history-modal',
   templateUrl: 'visit-history-modal.html',
@@ -14,6 +14,7 @@ export class VisitHistoryModalPage implements OnInit {
   show: 'history' | 'symptoms';
   declarationId: number | any;
   roomsList$: Observable<any>;
+  generalInformation$: Observable<any>;
   skeletons = new Array(7);
   constructor(
     private modalCtrl: ModalController,
@@ -21,9 +22,8 @@ export class VisitHistoryModalPage implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.roomsList$ = this.ws.get(`/covid/room_attendance_log?declaration_id=${this.declarationId}`).pipe(
-      tap(res => console.log(res))
-    );
+    this.roomsList$ = this.ws.get(`/covid/room_attendance_log?declaration_id=${this.declarationId}`);
+    this.generalInformation$ = this.ws.get('/covid/general_information');
   }
 
   dismiss() {
