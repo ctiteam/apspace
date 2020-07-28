659ddba3afaff30949f977b0478cbcbd49ecad5f
jiraichi <shansp@protonmail.ch>
2020-07-28 18:47:58 +0800

    feat(koha): changed to prod api and styling

--- src/app/pages/koha/koha.page.ts
+++ src/app/pages/koha/koha.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+
 import { Checkouts, Fines, History, LatestAdditions } from 'src/app/interfaces/koha';
 import { WsApiService } from 'src/app/services';
 
@@ -21,7 +21,6 @@ export class KohaPage implements OnInit {
 
   selectedSegment: 'checkouts' | 'history' | 'latest-additions' = 'checkouts';
 
-  url = 'https://ousb3s0l8k.execute-api.ap-southeast-1.amazonaws.com/dev/koha';
 
   constructor(
     private ws: WsApiService
@@ -30,50 +29,18 @@ export class KohaPage implements OnInit {
   ngOnInit() {
     this.getKohaData();
     this.getKohaFine();
-    this.getKohaDataTest();
- // this.getKohaDataTestProd();
   }
 
   getKohaData() {
-    this.checkouts$ = this.ws.get<Checkouts>('/checkouts', { url: this.url });
-    this.historyList$ = this.ws.get<History>('/history', { url: this.url });
-    this.recentAdditions$ = this.ws.get<LatestAdditions>('/latestadditions', { url: this.url });
+    this.checkouts$ = this.ws.get<Checkouts>('/koha/checkouts');
+    this.historyList$ = this.ws.get<History>('/koha/history');
+    this.recentAdditions$ = this.ws.get<LatestAdditions>('/koha/latestadditions');
   }
 
   getKohaFine() {
-    this.ws.get<Fines>('/fines', { url: this.url }).subscribe((res) => {
+    this.ws.get<Fines>('/koha/fines').subscribe((res) => {
       this.fine = Number(res.fine);
       this.fineLoading = true;
     });
-  }
-
-  getKohaDataTest() {
-    this.ws.get<Checkouts>('/checkouts', { url: this.url }).pipe(
-      tap(res => console.log(res))
-    ).subscribe();
-    this.ws.get<History>('/history', { url: this.url }).pipe(
-      tap(res => console.log(res))
-    ).subscribe();
-    this.ws.get<Fines>('/fines', { url: this.url }).pipe(
-      tap(res => console.log(Number(res.fine)))
-    ).subscribe();
-    this.ws.get<LatestAdditions>('/latestadditions', { url: this.url }).pipe(
-      tap(res => console.log(res))
-    ).subscribe();
-  }
-
-  getKohaDataTestProd() {
-    this.ws.get<Checkouts>('/koha/checkouts').pipe(
-      tap(res => console.log(res))
-    ).subscribe();
-    this.ws.get<History>('/koha/history').pipe(
-      tap(res => console.log(res))
-    ).subscribe();
-    this.ws.get<Fines>('/koha/fines').pipe(
-      tap(res => console.log(Number(res.fine)))
-    ).subscribe();
-    this.ws.get<LatestAdditions>('/koha/latestadditions').pipe(
-      tap(res => console.log(res))
-    ).subscribe();
   }
 }
