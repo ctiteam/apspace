98320aadff195e5596d778ac229252e5002c1e7f
Muhammad Faris <muhammad.farisfaisal03@gmail.com>
2020-08-28 11:12:49 +0800

    feat(holidays): redesign holidays page

--- src/app/pages/holidays/holidays.module.ts
+++ src/app/pages/holidays/holidays.module.ts
@@ -1,8 +1,9 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule , DatePipe} from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { CalendarModule } from 'ion2-calendar';
 
 import { ComponentsModule } from 'src/app/components/components.module';
 import { HolidaysPage } from './holidays.page';
@@ -21,7 +22,9 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes),
     ComponentsModule,
+    CalendarModule
   ],
+  providers: [DatePipe],
   declarations: [HolidaysPage]
 })
 export class HolidaysPageModule {}
