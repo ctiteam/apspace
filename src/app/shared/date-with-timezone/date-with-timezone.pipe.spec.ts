f05eae15940e23560f50be79a16a42d12d9b8c6d
Ivan Tham <pickfire@riseup.net>
2020-08-28 15:33:48 +0800

    test: fix datewithtimezone dependency

--- src/app/shared/date-with-timezone/date-with-timezone.pipe.spec.ts
+++ src/app/shared/date-with-timezone/date-with-timezone.pipe.spec.ts
@@ -1,8 +1,11 @@
+import { inject } from '@angular/core/testing';
+
+import { SettingsService } from '../../services';
 import { DateWithTimezonePipe } from './date-with-timezone.pipe';
 
 describe('DateWithTimezonePipe', () => {
-  it('create an instance', () => {
-    const pipe = new DateWithTimezonePipe();
+  it('create an instance', inject([SettingsService], (settings: SettingsService) => {
+    const pipe = new DateWithTimezonePipe(settings);
     expect(pipe).toBeTruthy();
-  });
+  }));
 });
