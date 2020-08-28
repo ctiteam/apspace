edf5d9417d79be4a6693b9f325a5b77e046b2684
Ivan Tham <pickfire@riseup.net>
2020-08-28 15:35:21 +0800

    test(student-tiemtable): test outdated details

--- src/app/services/student-timetable.service.spec.ts
+++ src/app/services/student-timetable.service.spec.ts
@@ -1,21 +1,14 @@
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { Network } from '@ionic-native/network/ngx';
 
 import { StudentTimetableService } from './student-timetable.service';
 
 describe('StudentTimetableService', () => {
   let httpTestingController: HttpTestingController;
-  let networkSpy: jasmine.SpyObj<Network>;
 
   beforeEach(() => {
-    networkSpy = { type: 'wifi' } as jasmine.SpyObj<Network>;
-
     TestBed.configureTestingModule({
-      providers: [
-        { provide: Network, useValue: networkSpy },
-      ],
       imports: [RouterTestingModule, HttpClientTestingModule]
     });
 
@@ -31,7 +24,7 @@ describe('StudentTimetableService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should save to cache', () => {
+  it('should refresh when empty', () => {
     const testData = [];
     const service: StudentTimetableService = TestBed.inject(StudentTimetableService);
 
@@ -40,19 +33,20 @@ describe('StudentTimetableService', () => {
       fail
     );
 
-    // first requset is expected to be cached
+    // first request is expected to be cached
     const req1 = httpTestingController.expectOne(service.timetableUrl);
     expect(req1.request.method).toEqual('GET');
-    expect(req1.request.headers.has('x-refresh')).toBeFalsy();
+    expect(req1.request.headers.has('x-refresh')).toBeFalse();
     req1.flush(testData);
 
     const req2 = httpTestingController.expectOne(service.timetableUrl);
     expect(req2.request.method).toEqual('GET');
-    expect(req2.request.headers.has('x-refresh')).toBeTruthy();
+    expect(req2.request.headers.has('x-refresh')).toBeTrue();
     req2.flush(testData);
   });
 
-  it('should calculate outdated correctly', () => {
+  it('should considered outdated since current Sunday', () => {
+    jasmine.clock().mockDate(new Date('2020-08-30'));
     const testData = [
       {
         INTAKE: '',
@@ -65,27 +59,58 @@ describe('StudentTimetableService', () => {
         NAME: '',
         SAMACCOUNTNAME: '',
         DATESTAMP: '',
-        DATESTAMP_ISO: '2019-01-01',
+        DATESTAMP_ISO: '2020-08-28',
         TIME_FROM: '',
         TIME_TO: '',
       }
     ];
     const service: StudentTimetableService = TestBed.inject(StudentTimetableService);
-    networkSpy.type = 'none';
 
     service.get(false).subscribe(
       data => expect(data).toEqual(testData),
     );
 
-    // first requset is expected to be cached
+    // first request is expected to be cached
     const req1 = httpTestingController.expectOne(service.timetableUrl);
     expect(req1.request.method).toEqual('GET');
-    expect(req1.request.headers.has('x-refresh')).toBeFalsy();
+    expect(req1.request.headers.has('x-refresh')).toBeFalse();
     req1.flush(testData);
 
     const req2 = httpTestingController.expectOne(service.timetableUrl);
     expect(req2.request.method).toEqual('GET');
-    expect(req2.request.headers.has('x-refresh')).toBeTruthy();
+    expect(req2.request.headers.has('x-refresh')).toBeTrue();
     req2.flush(testData);
+  });
+
+  it('should considered fresh while still Saturday', () => {
+    jasmine.clock().mockDate(new Date('2020-08-29'));
+    const testData = [
+      {
+        INTAKE: '',
+        MODID: '',
+        DAY: '',
+        GROUPING: '',
+        LOCATION: '',
+        ROOM: '',
+        LECTID: '',
+        NAME: '',
+        SAMACCOUNTNAME: '',
+        DATESTAMP: '',
+        DATESTAMP_ISO: '2020-08-28',
+        TIME_FROM: '',
+        TIME_TO: '',
+      }
+    ];
+    const service: StudentTimetableService = TestBed.inject(StudentTimetableService);
+
+    service.get(false).subscribe(
+      data => expect(data).toEqual(testData),
+    );
+
+    // first request is expected to be cached
+    const req1 = httpTestingController.expectOne(service.timetableUrl);
+    expect(req1.request.method).toEqual('GET');
+    expect(req1.request.headers.has('x-refresh')).toBeFalse();
+    req1.flush(testData);
   });
 });
