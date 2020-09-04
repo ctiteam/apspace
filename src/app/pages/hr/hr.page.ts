ad6bdacd83244c96f11e69c7e111540ad70ddb35
Mohamad Al Ghayeb <mohamad.alghayeb@staffemail.apu.edu.my>
2020-09-04 09:44:47 +0800

    refactor(hr): implemented new changes
    - removed commented code
    - changed page style
    - cleaned classes & styles
    - used native ionic elements

--- src/app/pages/hr/hr.page.ts
+++ src/app/pages/hr/hr.page.ts
@@ -37,103 +37,25 @@ export class HrPage implements OnInit {
   pendingApproval$: Observable<PendingApproval[]>;
   skeletons = new Array(4);
   staffsOnLeave = []; // IDs of all staff on leave
-  // summaryChart = {
-  //   type: 'bar',
-  //   options: {
-  //     legend: {
-  //       position: 'right', // place legend on the right side of chart
-  //       display: false
-  //     },
-  //     scales: {
-  //       xAxes: [{
-  //         stacked: true
-  //       }],
-  //       yAxes: [{
-  //         stacked: true
-  //       }]
-  //     }
-  //   },
-  //   data: {}
-  // };
-  // highestRatedSliderOpts = {
-  //   initialSlide: 0,
-  //   speed: 400,
-  //   slidesPerView: 2.3,
-  //   centeredContent: true,
-  //   spaceBetween: 8
-  // };
   constructor(public modalCtrl: ModalController, private ws: WsApiService, private iab: InAppBrowser) { }
 
   ngOnInit() {
     this.ws.get<StaffProfile[]>('/staff/profile', { caching: 'cache-only' }).pipe(
       tap(profile => this.showPaySlip = this.chosenOnes.includes(profile[0].ID))
     ).subscribe();
-    // commented until the backend is fixed
-    // this.leaves$ = this.ws.get<LeaveBalance[]>('/staff/leave_balance').pipe(
-    //   tap(leaves => {
-    //     const labels = [];
-    //     const datasets = [
-    //       {
-    //         label: ['Taken'],
-    //         data: [],
-    //         backgroundColor: '#EBCCD1'
-    //       },
-    //       {
-    //         label: ['Pending'],
-    //         data: [],
-    //         backgroundColor: '#FAEBCC'
-    //       },
-    //       {
-    //         label: ['Available'],
-    //         data: [],
-    //         backgroundColor: '#D6E9C6'
-
-    //       }
-    //     ];
-    //     leaves.forEach(leave => {
-    //       const matches = leave.LEAVE_TYPE.match(/\b(\w)/g); // Get first letter from each word. Example: ['A', 'L'] for Annual Leave
-    //       const acronym = matches.join('').slice(0, 2); // Join letters together and take first two only. Example: AL
-    //       if (!labels.includes(acronym)) { // TEMP UNTIL THE BACKEND IS FIXED
-    //         labels.push(acronym);
-    //         datasets[0].data.push(+leave.TAKEN); // adding data for testing
-    //         datasets[1].data.push(+leave.PENDING); // adding data for testing
-    //         datasets[2].data.push(+leave.AVAILABLE); // adding data for testing
-    //       }
-    //       leave.LEAVE_ACRONYM = acronym; // add the acronym to the list
-    //       leave.LEAVE_TYPE_COLOR = this.strToColor(leave.LEAVE_TYPE); // add color to the list
-    //     });
-
-    //     this.summaryChart.data = {
-    //       labels,
-    //       datasets
-    //     };
-    //   })
-    // );
     this.history$ = this.getHistory();
     this.leaveInCluster$ = this.getOnLeaveInMyCluster();
     this.pendingApproval$ = this.getPendingMyApproval();
-  }
-
-  /** Convert string to color with djb2 hash function. */
-  strToColor(str: string): string {
-    let hash = 5381;
-    /* tslint:disable:no-bitwise */
-    for (let i = 0; i < str.length; i++) {
-      hash = ((hash << 5) + hash) + str.charCodeAt(i); /* hash * 33 + c */
-    }
-    return '#' + [16, 8, 0].map(i => ('0' + (hash >> i & 0xFF).toString(16))
-      .substr(-2)).join('');
-    /* tslint:enable:no-bitwise */
   }
 
   getHistory() {
     return this.ws.get('/staff/leave_status').pipe(
       map((res: []) => {
         const results = this.sortArrayOfDateKey(res, 'LEAVE_DATE', 'desc').reduce((previous: any, current: any) => {
-          if (!previous[format(current.LEAVE_DATE, 'MMMM yyyy')]) {
-            previous[format(current.LEAVE_DATE, 'MMMM yyyy')] = [current];
+          if (!previous[format(new Date(current.LEAVE_DATE), 'MMMM yyyy')]) {
+            previous[format(new Date(current.LEAVE_DATE), 'MMMM yyyy')] = [current];
           } else {
-            previous[format(current.LEAVE_DATE, 'MMMM yyyy')].push(current);
+            previous[format(new Date(current.LEAVE_DATE), 'MMMM yyyy')].push(current);
           }
           return previous;
         }, {});
