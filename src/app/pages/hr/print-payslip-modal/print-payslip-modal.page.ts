edd3f9e3ef720642612ac3481d6cf31ef3484652
Louis We <we.yuan@staffemail.apu.edu.my>
2020-09-10 08:17:29 +0800

    feat(payslip): allow HR to search employees files

--- src/app/pages/hr/print-payslip-modal/print-payslip-modal.page.ts
+++ src/app/pages/hr/print-payslip-modal/print-payslip-modal.page.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { FileOpener } from '@ionic-native/file-opener/ngx';
 import { File } from '@ionic-native/file/ngx';
-import { ModalController, Platform, ToastController } from '@ionic/angular';
+import { AlertController, LoadingController, ModalController, Platform, ToastController } from '@ionic/angular';
 import { Observable, of } from 'rxjs';
-import { finalize, map } from 'rxjs/operators';
+import { catchError, finalize, map } from 'rxjs/operators';
 
 import { CasTicketService, WsApiService } from 'src/app/services';
 
@@ -17,12 +17,11 @@ export class PrintPayslipModalPage {
   files$: Observable<any[]>;
   ePayslipUrl = 'https://t16rz80rg7.execute-api.ap-southeast-1.amazonaws.com/staging';
   // payslipsUrl = 'https://api.apiit.edu.my';
-  ePayslipEndpoint = '/epayslip/list';
 
   dateToFilter;
   fileToFilter;
   term;
-  isHumanResourceAdmin = false;
+  isHumanResourceAdmin = true;
   search = false;
   whileFirstSearch = false;
   segmentValue = 'myFiles';
@@ -30,6 +29,8 @@ export class PrintPayslipModalPage {
   constructor(
     public modalCtrl: ModalController,
     public toastCtrl: ToastController,
+    public alertCtrl: AlertController,
+    public loadingCtrl: LoadingController,
     private platform: Platform,
     private file: File,
     private fileOpener: FileOpener,
@@ -42,7 +43,7 @@ export class PrintPayslipModalPage {
   }
 
   doRefresh(refresher?) {
-    this.files$ = this.ws.get<any>(this.ePayslipEndpoint, {url: this.ePayslipUrl}).pipe(
+    this.files$ = this.ws.get<any>('/epayslip/list', { url: this.ePayslipUrl }).pipe(
       map(files => [...files.ea_form, ...files.payslips]),
       map(files => files.sort((a, b) => 0 - (a > b ? 1 : -1))),
       finalize(() => refresher && refresher.target.complete())
@@ -50,12 +51,12 @@ export class PrintPayslipModalPage {
   }
 
   downloadPayslipPdf(payslip) {
-    const downloadPayslipEndpoint = '/staff/download_payslip/';
+    const downloadPayslipEndpoint = '/epayslip/download/';
     const link = this.ePayslipUrl + downloadPayslipEndpoint + payslip;
 
     this.cas.getST(link).subscribe(st => {
       fetch(link + `?ticket=${st}`).then(result => result.blob()).then(blob => {
-        const pdfBlob = new Blob([blob], {type: 'application/pdf'});
+        const pdfBlob = new Blob([blob], { type: 'application/pdf' });
 
         if (this.platform.is('cordova')) {
           // Save the PDF to the data Directory of our App
@@ -87,14 +88,17 @@ export class PrintPayslipModalPage {
   }
 
   segmentChanged(event: any) {
+    this.dateToFilter = '';
+    this.fileToFilter = '';
+
     if (event.detail.value === 'myFiles') {
       this.doRefresh();
       this.search = false;
       this.whileFirstSearch = false;
     } else {
+      this.files$ = of([]);
       this.search = true;
       this.whileFirstSearch = true;
-      this.files$ = of([]);
     }
   }
 
@@ -102,6 +106,63 @@ export class PrintPayslipModalPage {
     if (this.whileFirstSearch) {
       this.whileFirstSearch = false;
     }
+
+    this.files$ = this.ws.get<any>(`/epayslip/find?staff_id=${this.term}`, { url: this.ePayslipUrl }).pipe(
+      map(files => [...files.ea_form, ...files.payslips]),
+      map(files => files.sort((a, b) => 0 - (a > b ? 1 : -1))),
+      catchError(error => of(error))
+    );
+  }
+
+  syncFiles() {
+    this.alertCtrl.create({
+      header: 'Perform synchronize?',
+      buttons: [
+        {
+          text: 'Yes',
+          handler: _ => {
+            this.presentLoading();
+            this.ws.get<any>('/epayslip/sync', { url: this.ePayslipUrl }).subscribe({
+              next: () => {
+                this.presentAlert('Success!', 'Synchronized', 'The synchronize is done.', 'success-alert');
+              },
+              error: () => {
+                this.dismissLoading();
+                this.presentAlert('Alert!', 'Synchronize Failed.', 'The synchronize is failed.', 'danger-alert');
+              },
+              complete: () => {
+                this.dismissLoading();
+              }
+            });
+          }
+        }, {
+          text: 'No',
+          role: 'cancel'
+        }
+      ]
+    }).then(alert => alert.present());
+  }
+
+  presentAlert(header: string, subHeader: string, message: string, cssClass) {
+    this.alertCtrl.create({
+      cssClass,
+      header,
+      subHeader,
+      message,
+      buttons: ['OK']
+    }).then(alert => alert.present());
+  }
+
+  presentLoading() {
+    this.loadingCtrl.create({
+      spinner: 'dots',
+      message: 'Please wait...',
+      translucent: true,
+    }).then(loading => loading.present());
+  }
+
+  dismissLoading() {
+    this.loadingCtrl.dismiss();
   }
 
   dismiss() {
